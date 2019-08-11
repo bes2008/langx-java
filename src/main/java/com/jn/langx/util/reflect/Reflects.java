0e1b7c8655029e30d5adae3de084803d13d404d9
fangjinuo <fs1194361820@163.com>
2019-08-11 18:52:08 +0800

    * supports field access using reflect

--- src/main/java/com/jn/langx/util/reflect/Reflects.java
+++ src/main/java/com/jn/langx/util/reflect/Reflects.java
@@ -1,5 +1,7 @@
 package com.jn.langx.util.reflect;
 
+import com.jn.langx.exception.ExceptionMessage;
+
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
@@ -27,16 +29,16 @@ public class Reflects {
         return isAnonymous(clazz) || isLocal(clazz);
     }
 
-    public static boolean isAnonymous(Class clazz){
-        return !Enum.class.isAssignableFrom(clazz) && clazz.isAnonymousClass() ;
+    public static boolean isAnonymous(Class clazz) {
+        return !Enum.class.isAssignableFrom(clazz) && clazz.isAnonymousClass();
     }
 
 
-    public static boolean isLocal(Class clazz){
-        return !Enum.class.isAssignableFrom(clazz) && clazz.isLocalClass() ;
+    public static boolean isLocal(Class clazz) {
+        return !Enum.class.isAssignableFrom(clazz) && clazz.isLocalClass();
     }
 
-    public static Field getField(Class clazz, String fieldName) {
+    public static Field getPublicField(Class clazz, String fieldName) {
         Field field = null;
         try {
             field = clazz.getField(fieldName);
@@ -56,16 +58,95 @@ public class Reflects {
         return field;
     }
 
-    public static <T> T getDeclaredFieldValue(Object object, String fieldName) {
-        Field field = Reflects.getDeclaredField(object.getClass(), fieldName);
-        if (field != null) {
-            field.setAccessible(true);
-            try {
-                return (T) field.get(object);
-            } catch (IllegalAccessException e) {
-                // ignore it
+    public static Field getAnyField(Class clazz, String fieldName) {
+        Field field = getDeclaredField(clazz, fieldName);
+        if (field == null) {
+            Class parent = clazz.getSuperclass();
+            if (parent != null) {
+                return getAnyField(parent, fieldName);
             }
+            return null;
         }
-        return null;
+        return field;
+    }
+
+    public static <V> V getPublicFieldValueForcedIfPresent(Object object, String fieldName){
+        try {
+            return getPublicFieldValue(object, fieldName, false);
+        }catch (Throwable ex){
+            return null;
+        }
+    }
+
+    public static <V> V getPublicFieldValue(Object object, String fieldName, boolean throwException) throws NoSuchFieldException, IllegalAccessException {
+        Field field = getPublicField(object.getClass(), fieldName);
+        if (field == null) {
+            if(throwException) {
+                throw new NoSuchFieldException(new ExceptionMessage("Can't find public field {0} in the class {1}", fieldName, object.getClass().getCanonicalName()).getMessage());
+            }
+            return null;
+        }else {
+            return (V) field.get(object);
+        }
+    }
+
+    public static <V> V getDeclaredFieldValueForcedIfPresent(Object object, String fieldName){
+        try {
+            return getDeclaredFieldValue(object, fieldName, true, false);
+        }catch (Throwable ex){
+            return null;
+        }
+    }
+
+    public static <V> V getDeclaredFieldValue(Object object, String fieldName, boolean force, boolean throwException) throws NoSuchFieldException, IllegalAccessException {
+        Field field = getDeclaredField(object.getClass(), fieldName);
+        if (field == null) {
+            if(throwException) {
+                throw new NoSuchFieldException(new ExceptionMessage("Can't find a declared field {0} in the class {1}", fieldName, object.getClass().getCanonicalName()).getMessage());
+            }
+            return null;
+        }else {
+            return getFieldValue(object, field, force, throwException);
+        }
+    }
+
+    public static <V> V getAnyFieldValueForcedIfPresent(Object object, String fieldName){
+        try{
+            return getAnyFieldValue(object, fieldName, true, false);
+        }catch (Throwable ex){
+            return null;
+        }
+    }
+
+    public static <V> V getAnyFieldValue(Object object, String fieldName, boolean force, boolean throwException)  throws NoSuchFieldException, IllegalAccessException {
+        Field field = getAnyField(object.getClass(), fieldName);
+        if(field==null){
+            if(throwException){
+                throw new NoSuchFieldException(new ExceptionMessage("Can't find a declared field {0} in the class {1} and its all super class", fieldName, object.getClass().getCanonicalName()).getMessage());
+            }
+            return null;
+        }else{
+            return getFieldValue(object, field, force, throwException);
+        }
+    }
+
+    private static <V> V getFieldValue(Object object, Field field, boolean force, boolean throwException) throws IllegalAccessException{
+        if (!force && !field.isAccessible()) {
+            if(throwException) {
+                throw new IllegalAccessException();
+            }
+            return null;
+        }
+        // accessible
+        if (field.isAccessible()) {
+            return (V) field.get(object);
+        }
+
+        // unaccessible && force
+        field.setAccessible(true);
+        V v = (V) field.get(object);
+        field.setAccessible(false);
+        return v;
+
     }
 }
