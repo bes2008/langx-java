8185df29e5ab5abaafebbc31e4b15acd4eaeef48
fangjinuo <fs1194361820@163.com>
2019-08-11 18:52:40 +0800

    * refactor accessor

---
+++ src/main/java/com/jn/langx/util/Accessor.java
@@ -0,0 +1,41 @@
+package com.jn.langx.util;
+
+/**
+ * Get value from T object;
+ * <p>
+ * getXxx(String key): get the Xxx value association to specified key from target
+ * getXxx(String key, Xxx default): get the Xxx value association to specified key from target, if can't find the key, return the specified default value
+ *
+ * @param <T> the target
+ */
+public interface Accessor<T> {
+    void setTarget(T target);
+
+    String getString(String key);
+
+    String getString(String key, String defaultValue);
+
+    Integer getInteger(String key);
+
+    Integer getInteger(String key, Integer defaultValue);
+
+    Short getShort(String key);
+
+    Short getShort(String key, Short defaultValue);
+
+    Double getDouble(String key);
+
+    Double getDouble(String key, Double defaultValue);
+
+    Float getFloat(String key);
+
+    Float getFloat(String key, Float defaultValue);
+
+    Long getLong(String key);
+
+    Long getLong(String key, Long defaultValue);
+
+    Boolean getBoolean(String key);
+
+    Boolean getBoolean(String key, Boolean defaultValue);
+}
--- src/main/java/com/jn/langx/util/PropertiesAccessor.java
+++ src/main/java/com/jn/langx/util/PropertiesAccessor.java
@@ -1,26 +1,12 @@
-/*
- * Copyright 2019 the original author or authors.
- *
- * Licensed under the LGPL, Version 3.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at  http://www.gnu.org/licenses/lgpl-3.0.html
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.jn.langx.util;
 
 import java.util.Properties;
 
-public class PropertiesAccessor {
+public class PropertiesAccessor implements Accessor<Properties> {
     private Properties props;
 
     public PropertiesAccessor(Properties properties) {
-        this.props = properties;
+        setTarget(properties);
     }
 
     public String getString(String key) {
@@ -35,7 +21,7 @@ public class PropertiesAccessor {
         return getInteger(key, 0);
     }
 
-    public Integer getInteger(String key, int defaultValue) {
+    public Integer getInteger(String key, Integer defaultValue) {
         return Integer.parseInt(props.getProperty(key, "" + defaultValue));
     }
 
@@ -43,7 +29,7 @@ public class PropertiesAccessor {
         return getShort(key, Short.valueOf("" + 0));
     }
 
-    public Short getShort(String key, short defaultValue) {
+    public Short getShort(String key, Short defaultValue) {
         return Short.parseShort(props.getProperty(key, "" + defaultValue));
     }
 
@@ -51,7 +37,7 @@ public class PropertiesAccessor {
         return getDouble(key, 0.0d);
     }
 
-    public Double getDouble(String key, double defaultValue) {
+    public Double getDouble(String key, Double defaultValue) {
         return Double.parseDouble(props.getProperty(key, "" + defaultValue));
     }
 
@@ -59,15 +45,15 @@ public class PropertiesAccessor {
         return getFloat(key, 0.0f);
     }
 
-    public Float getFloat(String key, float defaultValue) {
+    public Float getFloat(String key, Float defaultValue) {
         return Float.parseFloat(props.getProperty(key, "" + defaultValue));
     }
 
     public Long getLong(String key) {
-        return getLong(key, "0");
+        return getLong(key, 0L);
     }
 
-    public Long getLong(String key, String defaultValue) {
+    public Long getLong(String key, Long defaultValue) {
         return Long.parseLong(props.getProperty(key, "" + defaultValue));
     }
 
@@ -75,7 +61,12 @@ public class PropertiesAccessor {
         return getBoolean(key, false);
     }
 
-    public Boolean getBoolean(String key, boolean defaultValue) {
+    public Boolean getBoolean(String key, Boolean defaultValue) {
         return Boolean.parseBoolean(props.getProperty(key, "" + defaultValue));
+    }
+
+    @Override
+    public void setTarget(Properties target) {
+        this.props = target;
     }
 }
