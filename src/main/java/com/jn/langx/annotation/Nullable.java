1ca706c38a9addbb82d8f1490200431e038cce28
fangjinuo <fs1194361820@163.com>
2019-08-12 08:12:52 +0800

    * update mark annotations

--- src/main/java/com/jn/langx/annotation/Nullable.java
+++ src/main/java/com/jn/langx/annotation/Nullable.java
@@ -1,4 +1,14 @@
 package com.jn.langx.annotation;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Target(value = {TYPE, FIELD, METHOD, PARAMETER, CONSTRUCTOR, LOCAL_VARIABLE })
 public @interface Nullable {
 }
