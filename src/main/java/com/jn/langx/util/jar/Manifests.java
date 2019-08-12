254397feeb53d9ec704ac2ef0c163120cff79dba
fjn <fs1194361820@163.com>
2019-08-12 11:16:00 +0800

    + add manifest loader

---
+++ src/main/java/com/jn/langx/util/jar/Manifests.java
@@ -0,0 +1,53 @@
+package com.jn.langx.util.jar;
+
+import com.jn.langx.util.Emptys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.URL;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
+
+public class Manifests {
+    private static final Logger logger = LoggerFactory.getLogger(Manifests.class);
+
+    public static Manifest loadManifest() {
+        URL url = Manifests.class.getProtectionDomain().getCodeSource().getLocation();
+        if (url.getPath().endsWith("target/classes/")) {
+            return null;
+        }
+        String jarPath = url.getPath();
+        if (jarPath.startsWith("file:")) {
+            jarPath = jarPath.replaceFirst("file:", "");
+        }
+        return loadManifest(jarPath);
+    }
+
+
+    public static Manifest loadManifest(String jarPath) {
+        if (jarPath == null) {
+            return null;
+        }
+
+        String path0 = jarPath;
+
+        String[] pathSegments = jarPath.split("!\\/");
+        if (!Emptys.isEmpty(pathSegments)) {
+            for (String segment : pathSegments) {
+                if (!Emptys.isEmpty(segment)) {
+                    jarPath = segment;
+                    break;
+                }
+            }
+        }
+
+        JarFile jarFile = null;
+        try {
+            jarFile = new JarFile(jarPath);
+            return jarFile.getManifest();
+        } catch (Throwable ex) {
+            logger.warn("Can't find the jar : {}", path0);
+            return null;
+        }
+    }
+}
