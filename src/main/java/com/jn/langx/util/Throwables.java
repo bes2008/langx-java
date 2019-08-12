be80c50c5753d01ac1b1a5b8f5b3d84ef62e04f2
fjn <fs1194361820@163.com>
2019-08-12 11:28:27 +0800

    + add throwable utility

---
+++ src/main/java/com/jn/langx/util/Throwables.java
@@ -0,0 +1,17 @@
+package com.jn.langx.util;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+public class Throwables {
+    public static String getStackTraceAsString(Throwable throwable){
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(stringWriter);
+        try {
+            throwable.printStackTrace(printWriter);
+            return stringWriter.toString();
+        }finally {
+            IOs.close(printWriter);
+        }
+    }
+}
