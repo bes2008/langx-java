8796121940d2d8081f7a93391df76ff78d19c7fe
fjn <fs1194361820@163.com>
2019-08-13 15:21:21 +0800

    * supports format base order

--- src/main/java/com/jn/langx/text/StringTemplates.java
+++ src/main/java/com/jn/langx/text/StringTemplates.java
@@ -1,20 +1,65 @@
 package com.jn.langx.text;
 
+import com.jn.langx.util.Emptys;
 import com.jn.langx.util.collect.function.Function2;
 
 import java.util.regex.Pattern;
 
 public class StringTemplates {
 
+    private static final Pattern orderPattern = Pattern.compile("\\{\\}");
+
+    /**
+     * format based order
+     * @param template the string template
+     * @return formatted string
+     */
+    public static String formatBasedOrder(String template, Object[] args){
+        return format(template, orderPattern, new Function2<String, Object[], String>() {
+            int index = -1;
+            @Override
+            public String apply(String matched, Object[] args) {
+                index++;
+                Object value = args[index];
+                return Emptys.isNull(value) ? "" : value.toString();
+            }
+        }, args);
+    }
+
+    /**
+     * format based index
+     * @param template the string template
+     * @param args args
+     * @return formatted string
+     */
     public static String format(String template, Object[] args) {
         return format(template, "", null, args);
     }
 
+
+    /**
+     * custom formatter
+     * @param template the string template
+     * @param variablePattern variable pattern in template
+     * @param valueGetter variable's value getter, will get value from args
+     * @param args args
+     * @return formatted string
+     */
     public static String format(String template, String variablePattern, Function2<String, Object[], String> valueGetter, Object[] args) {
         return new StringTemplate().variablePattern(variablePattern).using(template).with(valueGetter).format(args);
     }
 
+    /**
+     * custom formatter
+     * @param template the string template
+     * @param variablePattern variable pattern in template
+     * @param valueGetter variable's value getter, will get value from args
+     * @param args args
+     * @return formatted string
+     */
     public static String format(String template, Pattern variablePattern, Function2<String, Object[], String> valueGetter, Object[] args) {
         return new StringTemplate().variablePattern(variablePattern).using(template).with(valueGetter).format(args);
     }
+
+
 }
--- src/test/java/com/jn/langx/test/test/StringTemplateTests.java
+++ src/test/java/com/jn/langx/test/test/StringTemplateTests.java
@@ -4,7 +4,8 @@ import com.jn.langx.text.StringTemplates;
 import org.junit.Test;
 
 public class StringTemplateTests {
-    private void test(Object[] args) {
+
+    private void testFormatBasedIndex(Object[] args) {
         System.out.println(StringTemplates.format("", args));
         System.out.println(StringTemplates.format(" \t ", args));
         System.out.println(StringTemplates.format(" a, b, c ", args));
@@ -13,9 +14,19 @@ public class StringTemplateTests {
         System.out.println(StringTemplates.format(" a{0}, b{1}, c{2} ", args));
     }
 
+    private void testFormatBasedOrder(Object[] args) {
+        System.out.println(StringTemplates.formatBasedOrder("", args));
+        System.out.println(StringTemplates.formatBasedOrder(" \t ", args));
+        System.out.println(StringTemplates.formatBasedOrder(" a, b, c ", args));
+        System.out.println(StringTemplates.formatBasedOrder(" a{}, b{1}, c{2} ", args));
+        System.out.println(StringTemplates.formatBasedOrder(" a{1}, b{}, c{3} ", args));
+        System.out.println(StringTemplates.formatBasedOrder(" a{0}, b{}, c{} ", args));
+    }
+
     @Test
     public void test2(){
-        test(new Object[]{1,"hello","world","build"});
+        testFormatBasedIndex(new Object[]{1,"hello","world","build"});
+        testFormatBasedOrder(new Object[]{1,"hello","world","build"});
     }
 
 }
