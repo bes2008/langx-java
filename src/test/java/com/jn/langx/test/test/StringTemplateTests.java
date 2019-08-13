ee50d52b3684e314c7b49052d34761ef6186bfbb
fjn <fs1194361820@163.com>
2019-08-13 14:19:42 +0800

    + add string template test cases

---
+++ src/test/java/com/jn/langx/test/test/StringTemplateTests.java
@@ -0,0 +1,29 @@
+package com.jn.langx.test.test;
+
+import com.jn.langx.text.StringTemplates;
+import org.junit.Test;
+
+public class StringTemplateTests {
+    private void test(Object[] args) {
+        System.out.println(StringTemplates.format("", args));
+        System.out.println(StringTemplates.format(" \t ", args));
+        System.out.println(StringTemplates.format(" a, b, c ", args));
+        System.out.println(StringTemplates.format(" a{}, b{1}, c{2} ", args));
+        System.out.println(StringTemplates.format(" a{1}, b{2}, c{3} ", args));
+        System.out.println(StringTemplates.format(" a{0}, b{1}, c{2} ", args));
+    }
+
+    @Test
+    public void test0(){
+        test(new Object[0]);
+    }
+
+    public void test1(){
+        test(new Object[]{1,"hello"});
+    }
+
+    public void test2(){
+        test(new Object[]{1,"hello","world","build"});
+    }
+
+}
