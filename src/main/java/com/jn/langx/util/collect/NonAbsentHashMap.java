837f1c96beb7c41de1f0b26026ce84c94a146f9f
fjn <fs1194361820@163.com>
2019-08-13 11:59:58 +0800

    * supports non absent map

---
+++ src/main/java/com/jn/langx/util/collect/NonAbsentHashMap.java
@@ -0,0 +1,50 @@
+package com.jn.langx.util.collect;
+
+import com.jn.langx.util.collect.interfacefunction.Suppller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class NonAbsentHashMap<K, V> extends HashMap<K, V> {
+    private Suppller<K, V> suppller;
+
+    public NonAbsentHashMap(Suppller<K, V> suppller) {
+        super();
+        this.suppller = suppller;
+    }
+
+    public NonAbsentHashMap(int initialCapacity, Suppller<K, V> suppller) {
+        this(initialCapacity, initialCapacity, suppller);
+    }
+
+    public NonAbsentHashMap(int initialCapacity, float loadFactor, Suppller<K, V> suppller) {
+        super(initialCapacity, loadFactor);
+        this.suppller = suppller;
+    }
+
+    public NonAbsentHashMap(Map<? extends K, ? extends V> m, Suppller<K, V> suppller) {
+        super(m);
+        this.suppller = suppller;
+    }
+
+    @Override
+    public V get(Object key) {
+        V v = getIfPresent(key);
+        if (v == null) {
+            v = putIfAbsent((K) key, suppller.get((K) key));
+        }
+        return v;
+    }
+
+    public V getIfPresent(Object key) {
+        return super.get(key);
+    }
+
+    public V putIfAbsent(K key, V value) {
+        V v = super.get(key);
+        if (v == null) {
+            return super.put(key, value);
+        }
+        return v;
+    }
+}
---
+++ src/main/java/com/jn/langx/util/collect/WrappedNonAbsentMap.java
@@ -0,0 +1,97 @@
+package com.jn.langx.util.collect;
+
+import com.jn.langx.util.Emptys;
+import com.jn.langx.util.collect.interfacefunction.Suppller;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+public class WrappedNonAbsentMap<K, V> implements Map<K, V> {
+    private Map<K, V> delegate;
+    private Suppller<K, V> suppller;
+
+    public WrappedNonAbsentMap(Map<K, V> map, Suppller<K, V> suppller) {
+        this.delegate = map;
+        this.suppller = suppller;
+    }
+
+    @Override
+    public int size() {
+        return delegate.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return Emptys.isEmpty(delegate);
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return delegate.containsKey(key);
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return delegate.containsValue(value);
+    }
+
+    @Override
+    public V get(Object key) {
+        K key0 = (K) key;
+        V v = getIfPresent(key0);
+        if (v == null) {
+            return putIfAbsent(key0, suppller.get(key0));
+        }
+        return v;
+    }
+
+    public V getIfPresent(Object key) {
+        return delegate.get(key);
+    }
+
+    public V putIfAbsent(K key, V value) {
+        V v = delegate.get(key);
+        if (v == null) {
+            return delegate.put(key, value);
+        }
+        return v;
+    }
+
+    @Override
+    public V put(K key, V value) {
+        return delegate.put(key, value);
+    }
+
+    @Override
+    public V remove(Object key) {
+        return delegate.remove(key);
+    }
+
+    @Override
+    public void putAll(Map<? extends K, ? extends V> m) {
+        if (Emptys.isNotEmpty(m)) {
+            delegate.putAll(m);
+        }
+    }
+
+    @Override
+    public void clear() {
+        delegate.clear();
+    }
+
+    @Override
+    public Set<K> keySet() {
+        return delegate.keySet();
+    }
+
+    @Override
+    public Collection<V> values() {
+        return delegate.values();
+    }
+
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        return delegate.entrySet();
+    }
+}
