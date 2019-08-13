59be40a67dc57b1d57ffa417d1432c8ba3688dc0
fjn <fs1194361820@163.com>
2019-08-13 22:19:45 +0800

    * update java doc

--- src/main/java/com/jn/langx/util/enums/base/EnumDelegate.java
+++ src/main/java/com/jn/langx/util/enums/base/EnumDelegate.java
@@ -1,5 +1,66 @@
 package com.jn.langx.util.enums.base;
 
+/**
+ * Use it, every enum will have the follow field: code, name, displayText;
+ * <p>
+ * you can declare any enum like this:
+ * <pre>
+ *     public enum Period implements Delegatable<EnumDelegate> {
+ *         MINUTES(0, "minutes", "minutes"),
+ *         HOURS(1, "hours", "hours"),
+ *         DAY(2, "day", "day"),
+ *         MONTH(3, "month", "month");
+ *
+ *
+ *         private int code;
+ *         private String name;
+ *         private String displayText;
+ *         private EnumDelegate delegate;
+ *
+ *         Period(int code, String name, String displayText){
+ *             this.code = code;
+ *             this.name = name;
+ *             this.displayText = displayText;
+ *
+ *             setDelegate(new EnumDelegate(code, name, displayText));
+ *         }
+ *
+ *         public int getCode(){
+ *             return code;
+ *         }
+ *
+ *         public String getName(){
+ *             return name;
+ *         }
+ *
+ *         public String getDisplayText(){
+ *             return displayText;
+ *         }
+ *
+ *         public EnumDelegate getDelegate(){
+ *             return delegate;
+ *         }
+ *
+ *         public void setDelegate(EnumDelegate delegate){
+ *             this.delegate = delegate;
+ *         }
+ *     }
+ * </pre>
+ *
+ * if use it, you can get any enum instance by CommonEnums:
+ * for examples:
+ * <pre>
+ *     // get an enum instance by code
+ *     Period p = CommonEnums.ofCode(0);
+ *
+ *     // get an enum instance by name
+ *     Period p = CommonEnums.ofName("minutes");
+ *
+ *     // get an enum instance by displayText
+ *     Period p = CommonEnums.ofDisplayText("minutes");
+ * </pre>
+ *
+ */
 public class EnumDelegate implements CommonEnum {
     private int code;
     private String name;
