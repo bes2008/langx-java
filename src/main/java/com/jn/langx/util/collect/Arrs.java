c6b0893f1d80f5899316ba51c299ffbf71ff8c03
fangjinuo <fs1194361820@163.com>
2019-08-14 05:43:09 +0800

    * update java doc

--- src/main/java/com/jn/langx/util/collect/Arrs.java
+++ src/main/java/com/jn/langx/util/collect/Arrs.java
@@ -2,11 +2,20 @@ package com.jn.langx.util.collect;
 
 import com.jn.langx.util.Emptys;
 
+/**
+ * Array tools
+ */
 public class Arrs {
+    /**
+     * judge whether an object is an Array
+     */
     public static boolean isArray(Object o){
         return Emptys.isNull(o) ? false : o.getClass().isArray();
     }
 
+    /**
+     * wrap a string using new String[]{string}
+     */
     public static String[] wrapAsArray(String string){
         if(Emptys.isNull(string)){
             return new String[0];
@@ -14,6 +23,9 @@ public class Arrs {
         return new String[]{string};
     }
 
+    /**
+     * wrap a number using new String[]{number}
+     */
     public static Number[] wrapAsArray(Number number){
         if(Emptys.isNull(number)){
             return new Number[0];
@@ -21,6 +33,9 @@ public class Arrs {
         return new Number[]{number};
     }
 
+    /**
+     * wrap a boolean using new String[]{bool}
+     */
     public static Boolean[] wrapAsArray(Boolean bool){
         if(Emptys.isNull(bool)){
             return new Boolean[0];
@@ -28,6 +43,9 @@ public class Arrs {
         return new Boolean[]{bool};
     }
 
+    /**
+     * Wrap any object using new Object[]{object};
+     */
     public static Object[] wrapAsArray(Object o){
         if(Emptys.isNull(o)){
             return new Object[0];
