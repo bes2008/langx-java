96734dd30055d087a9227e30eb8de2dccdc0d8a4
fjn <fs1194361820@163.com>
2019-08-14 20:14:00 +0800

    * add test cases for Arrs#wrapAsArray(Object obj)

--- src/main/java/com/jn/langx/util/collection/Arrs.java
+++ src/main/java/com/jn/langx/util/collection/Arrs.java
@@ -11,11 +11,12 @@ public class Arrs {
 
     /**
      * get the length if argument is an array, else -1
+     *
      * @param object any object
      * @return the length if argument is an array, else -1
      */
-    public static int getLengthIfIsArray(Object object){
-        if(isArray(object)){
+    public static int getLengthIfIsArray(Object object) {
+        if (isArray(object)) {
             return Array.getLength(object);
         }
         return -1;
@@ -24,14 +25,15 @@ public class Arrs {
     /**
      * judge whether an object is an Array
      */
-    public static boolean isArray(Object o){
+    public static boolean isArray(Object o) {
         return Emptys.isNull(o) ? false : o.getClass().isArray();
     }
+
     /**
      * wrap a string using new String[]{string}
      */
-    public static String[] wrapAsArray(String string){
-        if(Emptys.isNull(string)){
+    public static String[] wrapAsArray(String string) {
+        if (Emptys.isNull(string)) {
             return new String[0];
         }
         return new String[]{string};
@@ -40,8 +42,8 @@ public class Arrs {
     /**
      * wrap a number using new String[]{number}
      */
-    public static Number[] wrapAsArray(Number number){
-        if(Emptys.isNull(number)){
+    public static Number[] wrapAsArray(Number number) {
+        if (Emptys.isNull(number)) {
             return new Number[0];
         }
         return new Number[]{number};
@@ -50,8 +52,8 @@ public class Arrs {
     /**
      * wrap a boolean using new String[]{bool}
      */
-    public static Boolean[] wrapAsArray(Boolean bool){
-        if(Emptys.isNull(bool)){
+    public static Boolean[] wrapAsArray(Boolean bool) {
+        if (Emptys.isNull(bool)) {
             return new Boolean[0];
         }
         return new Boolean[]{bool};
@@ -60,8 +62,8 @@ public class Arrs {
     /**
      * Wrap any object using new Object[]{object};
      */
-    public static Object[] wrapAsArray(Object o){
-        if(Emptys.isNull(o)){
+    public static Object[] wrapAsArray(Object o) {
+        if (Emptys.isNull(o)) {
             return new Object[0];
         }
         return new Object[]{o};
--- src/test/java/com/jn/langx/test/util/collection/ArrsTests.java
+++ src/test/java/com/jn/langx/test/util/collection/ArrsTests.java
@@ -6,10 +6,18 @@ import org.junit.Test;
 
 public class ArrsTests {
     @Test
-    public void isArrayTests(){
+    public void isArrayTests() {
         Assert.assertFalse(Arrs.isArray(null));
         Assert.assertFalse(Arrs.isArray("string"));
         Assert.assertTrue(Arrs.isArray(new int[]{1}));
         Assert.assertTrue(Arrs.isArray(new String[0]));
+    }
+
+    @Test
+    public void wrapAsArrayTests() {
+        Assert.assertArrayEquals(new String[]{"a"}, Arrs.wrapAsArray("a"));
+        Assert.assertArrayEquals(new Object[]{"a"}, Arrs.wrapAsArray("a"));
+        Object[] a = Arrs.wrapAsArray("a");
+        System.out.println(a.length);
     }
 }
