f324992d392d4c16437ec93a6ca568d1a8ddcd71
fjn <fs1194361820@163.com>
2019-08-16 09:42:00 +0800

    * remove some duplicate methods

--- src/main/java/com/jn/langx/util/Arrs.java
+++ src/main/java/com/jn/langx/util/Arrs.java
@@ -30,35 +30,6 @@ public class Arrs {
         return Emptys.isNull(o) ? false : o.getClass().isArray();
     }
 
-    /**
-     * wrap a string using new String[]{string}
-     */
-    public static String[] wrapAsArray(String string) {
-        if (Emptys.isNull(string)) {
-            return new String[0];
-        }
-        return new String[]{string};
-    }
-
-    /**
-     * wrap a number using new String[]{number}
-     */
-    public static Number[] wrapAsArray(Number number) {
-        if (Emptys.isNull(number)) {
-            return new Number[0];
-        }
-        return new Number[]{number};
-    }
-
-    /**
-     * wrap a boolean using new String[]{bool}
-     */
-    public static Boolean[] wrapAsArray(Boolean bool) {
-        if (Emptys.isNull(bool)) {
-            return new Boolean[0];
-        }
-        return new Boolean[]{bool};
-    }
 
     /**
      * Wrap any object using new Object[]{object};
