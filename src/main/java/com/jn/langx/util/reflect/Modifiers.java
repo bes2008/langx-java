4e5978dcac0ddd5bc946821ea9fb60ce20522a4a
fangjinuo <fs1194361820@163.com>
2019-08-17 08:08:16 +0800

    * supports modifier utilities for class

--- src/main/java/com/jn/langx/util/reflect/Modifiers.java
+++ src/main/java/com/jn/langx/util/reflect/Modifiers.java
@@ -1,157 +1,160 @@
 package com.jn.langx.util.reflect;
 
 import com.jn.langx.text.StringTemplates;
+import com.jn.langx.util.Collects;
+import com.jn.langx.util.function.Function;
+import com.jn.langx.util.function.Predicate;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Member;
 import java.lang.reflect.Modifier;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * @author jinuo.fang
  */
 public class Modifiers {
+    private static final Set<Integer> supportedModifiers = new HashSet<Integer>(Collects.asList(new Integer[]{
+            Modifier.ABSTRACT,
+            Modifier.FINAL,
+            Modifier.INTERFACE,
+            Modifier.NATIVE,
+            Modifier.PUBLIC,
+            Modifier.PRIVATE,
+            Modifier.PROTECTED,
+            Modifier.STATIC,
+            Modifier.SYNCHRONIZED,
+            Modifier.STRICT,
+            Modifier.TRANSIENT,
+            Modifier.VOLATILE
+    }));
+
+    static {
+        List<Field> fields = Reflects.getAllPublicFields(Modifier.class, true);
+        fields = Collects.filter(fields, new Predicate<Field>() {
+            @Override
+            public boolean test(Field field) {
+                return Modifier.isPublic(field.getModifiers()) && Modifier.isStatic(field.getModifiers());
+            }
+        });
+        List<Integer> supportedModifiersInModifierClass = Collects.map(fields, new Function<Field, Integer>() {
+            @Override
+            public Integer apply(Field field) {
+                try {
+                    return (Integer) Reflects.getFieldValue(field, Modifier.class, true, false);
+                } catch (Throwable ex) {
+                    return Modifier.PUBLIC;
+                }
+            }
+        });
+        supportedModifiers.addAll(supportedModifiersInModifierClass);
+    }
+    public static boolean isSupportedModifier(int modifier){
+        return supportedModifiers.contains(modifier);
+    }
+
+
     public static int modifiers(Member member) {
         return member.getModifiers();
     }
 
-    /**
-     * Return <tt>true</tt> if the integer argument includes the
-     * <tt>public</tt> modifier, <tt>false</tt> otherwise.
-     * <p>
-     * <tt>public</tt> modifier; <tt>false</tt> otherwise.
-     */
-    public static boolean isPublic(Member member) {
-        return Modifier.isPublic(modifiers(member));
-    }
 
-    /**
-     * Return <tt>true</tt> if the integer argument includes the
-     * <tt>private</tt> modifier, <tt>false</tt> otherwise.
-     *
-     * @return <tt>true</tt> if <code>mod</code> includes the
-     * <tt>private</tt> modifier; <tt>false</tt> otherwise.
-     */
-    public static boolean isPrivate(Member member) {
-        return Modifier.isPrivate(modifiers(member));
-    }
-
-    /**
-     * Return <tt>true</tt> if the integer argument includes the
-     * <tt>protected</tt> modifier, <tt>false</tt> otherwise.
-     *
-     * @return <tt>true</tt> if <code>mod</code> includes the
-     * <tt>protected</tt> modifier; <tt>false</tt> otherwise.
-     */
-    public static boolean isProtected(Member member) {
-        return Modifier.isProtected(modifiers(member));
-    }
-
-    /**
-     * Return <tt>true</tt> if the integer argument includes the
-     * <tt>static</tt> modifier, <tt>false</tt> otherwise.
-     *
-     * @return <tt>true</tt> if <code>mod</code> includes the
-     * <tt>static</tt> modifier; <tt>false</tt> otherwise.
-     */
-    public static boolean isStatic(Member member) {
-        return Modifier.isStatic(modifiers(member));
-    }
-
-    /**
-     * Return <tt>true</tt> if the integer argument includes the
-     * <tt>final</tt> modifier, <tt>false</tt> otherwise.
-     *
-     * @return <tt>true</tt> if <code>mod</code> includes the
-     * <tt>final</tt> modifier; <tt>false</tt> otherwise.
-     */
-    public static boolean isFinal(Member member) {
-        return Modifier.isFinal(modifiers(member));
-    }
-
-    /**
-     * Return <tt>true</tt> if the integer argument includes the
-     * <tt>synchronized</tt> modifier, <tt>false</tt> otherwise.
-     *
-     * @return <tt>true</tt> if <code>mod</code> includes the
-     * <tt>synchronized</tt> modifier; <tt>false</tt> otherwise.
-     */
-    public static boolean isSynchronized(Member member) {
-        return Modifier.isSynchronized(modifiers(member));
-    }
-
-    /**
-     * Return <tt>true</tt> if the integer argument includes the
-     * <tt>volatile</tt> modifier, <tt>false</tt> otherwise.
-     *
-     * @return <tt>true</tt> if <code>mod</code> includes the
-     * <tt>volatile</tt> modifier; <tt>false</tt> otherwise.
-     */
-    public static boolean isVolatile(Member member) {
-        return Modifier.isVolatile(modifiers(member));
-    }
-
-    /**
-     * Return <tt>true</tt> if the integer argument includes the
-     * <tt>transient</tt> modifier, <tt>false</tt> otherwise.
-     *
-     * @return <tt>true</tt> if <code>mod</code> includes the
-     * <tt>transient</tt> modifier; <tt>false</tt> otherwise.
-     */
-    public static boolean isTransient(Member member) {
-        return Modifier.isTransient(modifiers(member));
-    }
-
-    /**
-     * Return <tt>true</tt> if the integer argument includes the
-     * <tt>native</tt> modifier, <tt>false</tt> otherwise.
-     *
-     * @return <tt>true</tt> if <code>mod</code> includes the
-     * <tt>native</tt> modifier; <tt>false</tt> otherwise.
-     */
-    public static boolean isNative(Member member) {
-        return Modifier.isNative(modifiers(member));
-    }
-
-    /**
-     * Return <tt>true</tt> if the integer argument includes the
-     * <tt>interface</tt> modifier, <tt>false</tt> otherwise.
-     *
-     * @return <tt>true</tt> if <code>mod</code> includes the
-     * <tt>interface</tt> modifier; <tt>false</tt> otherwise.
-     */
-    public static boolean isInterface(Member member) {
-        return Modifier.isInterface(modifiers(member));
-    }
-
-    /**
-     * Return <tt>true</tt> if the integer argument includes the
-     * <tt>abstract</tt> modifier, <tt>false</tt> otherwise.
-     *
-     * @return <tt>true</tt> if <code>mod</code> includes the
-     * <tt>abstract</tt> modifier; <tt>false</tt> otherwise.
-     */
-    public static boolean isAbstract(Member member) {
-        return Modifier.isAbstract(modifiers(member));
-    }
-
-    /**
-     * Return <tt>true</tt> if the integer argument includes the
-     * <tt>strictfp</tt> modifier, <tt>false</tt> otherwise.
-     *
-     * @return <tt>true</tt> if <code>mod</code> includes the
-     * <tt>strictfp</tt> modifier; <tt>false</tt> otherwise.
-     */
-    public static boolean isStrict(Member member) {
-        return Modifier.isStrict(modifiers(member));
-    }
-
-    public static boolean contains(int modifiers, int modifier){
-        if(modifier<=0){
-            throw new IllegalArgumentException(StringTemplates.formatWithoutIndex("Illegal modifier: {}", modifier));
+    public static boolean hasModifier(int modifiers, int modifier) {
+        if (!isSupportedModifier(modifier)) {
+            throw new IllegalArgumentException(StringTemplates.formatWithoutIndex("Unsupported modifier: {}", modifier));
         }
         return (modifiers & modifier) != 0;
     }
 
-    public static boolean contains(Member member, int modifier){
-        return contains(modifiers(member), modifier);
+    public static boolean hasModifier(Class clazz, int modifier) {
+        return hasModifier(clazz.getModifiers(), modifier);
     }
+
+    public static boolean hasModifier(Member member, int modifier) {
+        return hasModifier(modifiers(member), modifier);
+    }
+
+    public static boolean isAbstract(Class clazz) {
+        return hasModifier(clazz, Modifier.ABSTRACT);
+    }
+
+    public static boolean isAbstract(Member member) {
+        return hasModifier(member, Modifier.ABSTRACT);
+    }
+
+    public static boolean isFinal(Class clazz) {
+        return hasModifier(clazz, Modifier.FINAL);
+    }
+
+    public static boolean isFinal(Member member) {
+        return hasModifier(member, Modifier.FINAL);
+    }
+
+    public static boolean isInterface(Class clazz) {
+        return clazz.isInterface();
+    }
+
+    public static boolean isInterface(Member member) {
+        return hasModifier(member, Modifier.INTERFACE);
+    }
+
+    public static boolean isNative(Class clazz) {
+        return hasModifier(clazz, Modifier.NATIVE);
+    }
+
+    public static boolean isNative(Member member) {
+        return hasModifier(member, Modifier.NATIVE);
+    }
+
+    public static boolean isPublic(Class clazz) {
+        return hasModifier(clazz, Modifier.PUBLIC);
+    }
+
+    public static boolean isPublic(Member member) {
+        return hasModifier(member, Modifier.PUBLIC);
+    }
+
+    public static boolean isPrivate(Class clazz) {
+        return hasModifier(clazz, Modifier.PRIVATE);
+    }
+
+    public static boolean isPrivate(Member member) {
+        return hasModifier(member, Modifier.PRIVATE);
+    }
+
+    public static boolean isProtected(Class clazz) {
+        return hasModifier(clazz, Modifier.PROTECTED);
+    }
+
+    public static boolean isProtected(Member member) {
+        return hasModifier(member, Modifier.PROTECTED);
+    }
+
+    public static boolean isStatic(Class clazz) {
+        return hasModifier(clazz, Modifier.STATIC);
+    }
+
+    public static boolean isStatic(Member member) {
+        return hasModifier(member, Modifier.STATIC);
+    }
+
+    public static boolean isSynchronized(Member member) {
+        return hasModifier(member, Modifier.SYNCHRONIZED);
+    }
+
+    public static boolean isStrict(Member member) {
+        return hasModifier(member, Modifier.STRICT);
+    }
+
+    public static boolean isTransient(Member member) {
+        return hasModifier(member, Modifier.TRANSIENT);
+    }
+
+    public static boolean isVolatile(Member member) {
+        return hasModifier(member, Modifier.VOLATILE);
+    }
+
+
 }
---
+++ src/test/java/com/jn/langx/test/util/ModifiersTests.java
@@ -0,0 +1,13 @@
+package com.jn.langx.test.util;
+
+import com.jn.langx.util.Emptys;
+import com.jn.langx.util.reflect.Modifiers;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ModifiersTests {
+    @Test
+    public void test() {
+        Assert.assertTrue(Modifiers.isPublic(Emptys.class));
+    }
+}
