61dee4087e57568ef178b18f8897913b49891e31
fangjinuo <fs1194361820@163.com>
2019-08-18 06:49:44 +0800

    * supports environment variable iterate

--- src/test/java/com/jn/langx/test/util/SystemsTests.java
+++ src/test/java/com/jn/langx/test/util/SystemsTests.java
@@ -7,13 +7,25 @@ import org.junit.Test;
 
 public class SystemsTests {
     @Test
-    public void testSystemProperties(){
-       Collects.forEach(System.getProperties(), new Consumer2<Object, Object>() {
-           @Override
-           public void accept(Object key, Object value) {
-               System.out.println(StringTemplates.formatWithoutIndex("{} = {}", key, value));
-           }
-       });
+    public void testSystemProperties() {
+        System.out.println("====================System Properties===========");
+        Collects.forEach(System.getProperties(), new Consumer2<Object, Object>() {
+            @Override
+            public void accept(Object key, Object value) {
+                System.out.println(StringTemplates.formatWithoutIndex("{} = {}", key, value));
+            }
+        });
 
+    }
+
+    @Test
+    public void testEnvironmentVariables() {
+        System.out.println("====================Environment Variables===========");
+        Collects.forEach(System.getenv(), new Consumer2<String, String>() {
+            @Override
+            public void accept(String key, String value) {
+                System.out.println(StringTemplates.formatWithoutIndex("{} = {}", key, value));
+            }
+        });
     }
 }
