539d4b4a26ac9b7bc8e59efe94ebc36e2c029c2f
fangjinuo <fs1194361820@163.com>
2019-08-21 23:44:45 +0800

    + supports autoincrement id generator

---
+++ src/main/java/com/jn/langx/util/SimpleUUIDGenerator.java
@@ -0,0 +1,27 @@
+package com.jn.langx.util;
+
+import com.jn.langx.IdGenerator;
+
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * A simple {@link IdGenerator} that starts at 1 and increments by 1 with each call.
+ */
+public class SimpleUUIDGenerator implements IdGenerator<Object> {
+
+    private final AtomicLong mostSigBits = new AtomicLong(0);
+
+    private final AtomicLong leastSigBits = new AtomicLong(0);
+
+
+    @Override
+    public String get(Object o) {
+        long leastSigBits = this.leastSigBits.incrementAndGet();
+        if (leastSigBits == 0) {
+            this.mostSigBits.incrementAndGet();
+        }
+        return new UUID(this.mostSigBits.get(), leastSigBits).toString();
+    }
+
+}
