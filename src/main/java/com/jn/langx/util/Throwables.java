d48bbacd7336942c61ac4384fb694aeb4d59e334
fjn <fs1194361820@163.com>
2019-08-22 18:46:30 +0800

    * supply some methods for exception

--- src/main/java/com/jn/langx/util/Throwables.java
+++ src/main/java/com/jn/langx/util/Throwables.java
@@ -1,5 +1,6 @@
 package com.jn.langx.util;
 
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
@@ -15,11 +16,50 @@ public class Throwables {
         }
     }
 
+
+    public static Throwable throwIfError(Throwable ex){
+        if(ex instanceof Error){
+            throw (Error)ex;
+        }
+        return ex;
+    }
+
+    public static Throwable throwIfRuntimeException(Throwable ex){
+        if(ex instanceof RuntimeException){
+            throw (RuntimeException)ex;
+        }
+        return ex;
+    }
+
+    public static Throwable throwIfIOException(Throwable ex) throws IOException{
+        if(ex instanceof IOException){
+            throw (IOException)ex;
+        }
+        return ex;
+    }
+
     public static void throwAsRuntimeException(Throwable ex){
         if(ex instanceof RuntimeException){
             throw (RuntimeException)ex;
         }
         throw new RuntimeException(ex);
+    }
+
+    public static Throwable getRootCause(Throwable ex){
+        while (ex .getCause()!=null){
+            ex = ex.getCause();
+        }
+        return ex;
+    }
+
+    /**
+     * step1 : get root cause
+     * step2 : throwIfIOException(root cause)
+     * @param ex
+     * @return
+     */
+    public static Throwable throwRootCauseIfIOException(Throwable ex) throws IOException{
+        return throwIfIOException(getRootCause(ex));
     }
 
     public static void log(Throwable ex) {
