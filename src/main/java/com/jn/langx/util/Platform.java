10b4417f57660c4bdb3b28ddfe47b11057a8c1b4
fjn <fs1194361820@163.com>
2019-08-22 18:45:29 +0800

    * get current process id

--- src/main/java/com/jn/langx/util/Platform.java
+++ src/main/java/com/jn/langx/util/Platform.java
@@ -1,10 +1,12 @@
 package com.jn.langx.util;
 
+import com.jn.langx.util.reflect.Reflects;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Locale;
-
 
 public class Platform {
     private static final Logger logger = LoggerFactory.getLogger(Platform.class);
@@ -12,6 +14,8 @@ public class Platform {
     public static int JAVA_VERSION_INT = javaVersion();
     public static boolean isAndroid = isAndroid0();
     private static final boolean IS_IVKVM_DOT_NET = isIkvmDotNet0();
+    public static boolean isGroovyAvailable = isGroovyAvailable0();
+    public static String processId = getProcessId0();
 
     private static boolean isWindows0() {
         return System.getProperty("os.name", "").toLowerCase(Locale.US).contains("win");
@@ -70,6 +74,40 @@ public class Platform {
             return version[1];
         } else {
             return version[0];
+        }
+    }
+
+    private static boolean isGroovyAvailable0() {
+        ClassLoader loader = Platform.class.getClassLoader();
+        if (loader == null) {
+            loader = ClassLoader.getSystemClassLoader();
+        }
+        try {
+            Class bindingClass = loader.loadClass("groovy.lang.Binding");
+            return bindingClass != null;
+        } catch (ClassNotFoundException ex) {
+            return false;
+        }
+    }
+
+    private static String getProcessId0() {
+        try {
+            if (isAndroid) {
+                Object runtimeMXBean = Reflects.getDeclaredMethod(Class.forName("java.lang.management.ManagementFactory"), "getRuntimeMXBean").invoke(null);
+                return Reflects.getDeclaredMethod(Class.forName("java.lang.management.RuntimeMXBean"), "getName").invoke(runtimeMXBean).toString().split("@")[0];
+            } else {
+                java.lang.management.RuntimeMXBean runtimeMXBean = java.lang.management.ManagementFactory.getRuntimeMXBean();
+                return runtimeMXBean.getName().split("@")[0];
+            }
+        } catch (Throwable ex) {
+            if (isAndroid) {
+                try {
+                    return new File("/proc/self").getCanonicalFile().getName();
+                } catch (IOException e) {
+                    return null;
+                }
+            }
+            return null;
         }
     }
 
