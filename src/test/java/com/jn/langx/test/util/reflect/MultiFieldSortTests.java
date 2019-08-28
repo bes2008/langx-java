9d12a559da4416385ac4cc3cbe6d08dbe81ebcae
fjn <fs1194361820@163.com>
2019-08-28 11:30:18 +0800

    * test desc order

--- src/test/java/com/jn/langx/test/util/reflect/MultiFieldSortTests.java
+++ src/test/java/com/jn/langx/test/util/reflect/MultiFieldSortTests.java
@@ -3,6 +3,8 @@ package com.jn.langx.test.util.reflect;
 import com.jn.langx.test.bean.Person;
 import com.jn.langx.util.collection.Collects;
 import com.jn.langx.util.comparator.ParallelingComparator;
+import com.jn.langx.util.comparator.ReverseComparator;
+import com.jn.langx.util.comparator.StringComparator;
 import com.jn.langx.util.function.Consumer;
 import com.jn.langx.util.reflect.FieldComparator;
 import org.junit.Test;
@@ -91,6 +93,37 @@ public class MultiFieldSortTests {
         ParallelingComparator comparator = new ParallelingComparator();
         comparator.addComparator(new FieldComparator(Person.class, "id", null));
         comparator.addComparator(new FieldComparator(Person.class, "name", null));
+        comparator.addComparator(new FieldComparator(Person.class, "age", null));
+        Set<Person> pset = new TreeSet<Person>(comparator);
+
+        pset.addAll(persons);
+
+        Collects.forEach(pset, new Consumer<Person>() {
+            @Override
+            public void accept(Person person) {
+                System.out.println(person);
+            }
+        });
+    }
+
+    @Test
+    public void test4(){
+        System.out.println("===================test4==================");
+        List<Person> persons;
+        Random random = new Random(10);
+
+        persons = new LinkedList<Person>();
+        for (int i = 0; i < 100; i++) {
+            Person person = new Person();
+            person.setId("id_" + 0);
+            person.setName("name_" + Math.abs(random.nextInt(10)));
+            person.setAge(Math.abs(random.nextInt(100)));
+            persons.add(person);
+        }
+
+        ParallelingComparator comparator = new ParallelingComparator();
+        comparator.addComparator(new FieldComparator(Person.class, "id", null));
+        comparator.addComparator(new FieldComparator(Person.class, "name", new ReverseComparator(new StringComparator())));
         comparator.addComparator(new FieldComparator(Person.class, "age", null));
         Set<Person> pset = new TreeSet<Person>(comparator);
 
