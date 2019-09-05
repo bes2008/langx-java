f8e201c7ec2688f180f8638d020be1d2f07a2421
fjn <fs1194361820@163.com>
2019-09-05 11:58:52 +0800

    * update test cases

--- src/test/java/com/jn/langx/test/text/StringTemplateTests.java
+++ src/test/java/com/jn/langx/test/text/StringTemplateTests.java
@@ -6,21 +6,25 @@ import org.junit.Test;
 public class StringTemplateTests {
 
     private void testFormatBasedIndex(Object[] args) {
+        System.out.println("====testFormatBasedIndex start ====");
         System.out.println(StringTemplates.format("", args));
         System.out.println(StringTemplates.format(" \t ", args));
         System.out.println(StringTemplates.format(" a, b, c ", args));
         System.out.println(StringTemplates.format(" a{}, b{1}, c{2} ", args));
         System.out.println(StringTemplates.format(" a{1}, b{2}, c{3} ", args));
         System.out.println(StringTemplates.format(" a{0}, b{1}, c{2} ", args));
+        System.out.println("====testFormatBasedIndex end ====");
     }
 
     private void testFormatBasedOrder(Object[] args) {
+        System.out.println("====testFormatBasedOrder start ====");
         System.out.println(StringTemplates.formatWithPlaceholder("", args));
         System.out.println(StringTemplates.formatWithPlaceholder(" \t ", args));
         System.out.println(StringTemplates.formatWithPlaceholder(" a, b, c ", args));
         System.out.println(StringTemplates.formatWithPlaceholder(" a{}, b{1}, c{2} ", args));
         System.out.println(StringTemplates.formatWithPlaceholder(" a{1}, b{}, c{3} ", args));
         System.out.println(StringTemplates.formatWithPlaceholder(" a{0}, b{}, c{} ", args));
+        System.out.println("====testFormatBasedOrder end ====");
     }
 
     @Test
