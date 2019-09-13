ec0b3d68ce3fe6acd83d47e1c96e1b9a622d9d97
fangjinuo <fs1194361820@163.com>
2019-09-13 22:17:19 +0800

    * supports prefix map

---
+++ src/main/java/com/jn/langx/util/collection/PrefixHashMap.java
@@ -0,0 +1,72 @@
+package com.jn.langx.util.collection;
+
+import com.jn.langx.util.Preconditions;
+import com.jn.langx.util.function.Consumer2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PrefixHashMap<V> extends HashMap<String, V> {
+    private String prefix;
+    private boolean readUsingPrefix = true;
+
+    public PrefixHashMap(String prefix) {
+        this(prefix, true);
+    }
+
+    public PrefixHashMap(String prefix, boolean readUsingPrefix) {
+        Preconditions.checkNotNull(prefix);
+        this.prefix = prefix;
+        this.readUsingPrefix = readUsingPrefix;
+    }
+
+
+    public PrefixHashMap(String prefix, Map<String, V> map) {
+        this(prefix, true);
+    }
+
+    public PrefixHashMap(String prefix, boolean readUsingPrefix, Map<String, V> map) {
+        this(prefix, readUsingPrefix);
+        putAll(map);
+    }
+
+
+    private String getInnerKey(Object key) {
+        if (readUsingPrefix) {
+            return prefix + key;
+        } else {
+            // key - prefix
+            String key0 = key.toString();
+            if (key0.startsWith(prefix)) {
+                if (key0.length() > prefix.length()) {
+                    return key0.substring(prefix.length());
+                }
+                return "";
+            }
+            return key0;
+        }
+    }
+
+    @Override
+    public V put(String key, V value) {
+        if (key == null) {
+            return null;
+        }
+        return super.put(getInnerKey(key), value);
+    }
+
+    @Override
+    public void putAll(Map<? extends String, ? extends V> m) {
+        Collects.forEach(m, new Consumer2<String, V>() {
+            @Override
+            public void accept(String key, V value) {
+                put(key, value);
+            }
+        });
+    }
+
+    @Override
+    public Object clone() {
+        return new PrefixHashMap<V>(prefix, readUsingPrefix, this);
+    }
+}
---
+++ src/test/java/com/jn/langx/test/util/collection/PrefixHashMapTests.java
@@ -0,0 +1,37 @@
+package com.jn.langx.test.util.collection;
+
+import com.jn.langx.text.StringTemplates;
+import com.jn.langx.util.collection.Collects;
+import com.jn.langx.util.collection.PrefixHashMap;
+import com.jn.langx.util.function.Consumer;
+import org.junit.Test;
+
+import java.util.Map;
+
+public class PrefixHashMapTests {
+    @Test
+    public void test() {
+        PrefixHashMap<String> map = new PrefixHashMap<String>("xyz.");
+
+        map.put("a", "a1");
+        map.put("b", "b1");
+        map.put("c", "c1");
+        map.put("d", "d1");
+        map.put("e", "e1");
+        map.put("f", "f1");
+        map.put("g", "g1");
+        showMap(map);
+
+        map = new PrefixHashMap<String>("xyz.", false, map);
+        showMap(map);
+    }
+
+    private void showMap(PrefixHashMap<String> map ){
+        Collects.forEach(map.entrySet(), new Consumer<Map.Entry<String, String>>() {
+            @Override
+            public void accept(Map.Entry<String, String> entry) {
+                System.out.println(StringTemplates.formatWithPlaceholder("{}:{}", entry.getKey(), entry.getValue()));
+            }
+        });
+    }
+}
