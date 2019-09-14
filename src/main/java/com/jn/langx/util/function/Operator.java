75cc8f4ff995c220da5502ba8f563b939fceda3b
fangjinuo <fs1194361820@163.com>
2019-09-14 16:50:27 +0800

    + supports Operators

---
+++ src/main/java/com/jn/langx/util/function/Operator.java
@@ -0,0 +1,4 @@
+package com.jn.langx.util.function;
+
+public interface Operator<V> extends Function<V,V> {
+}
---
+++ src/main/java/com/jn/langx/util/function/Operator2.java
@@ -0,0 +1,4 @@
+package com.jn.langx.util.function;
+
+public interface Operator2<V> extends Function2<V, V, V> {
+}
