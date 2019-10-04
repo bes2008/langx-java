54e1a417df8a069551ffcd46e6250ae302abab23
fangjinuo <fs1194361820@163.com>
2019-10-04 11:09:23 +0800

    * avoid NPE

--- src/main/java/com/jn/langx/util/comparator/ComparableComparator.java
+++ src/main/java/com/jn/langx/util/comparator/ComparableComparator.java
@@ -5,6 +5,15 @@ import java.util.Comparator;
 public class ComparableComparator<E extends Comparable<E>> implements Comparator<E> {
     @Override
     public int compare(E o1, E o2) {
+        if (o1 == null && o2 == null) {
+            return 0;
+        }
+        if (o1 == null) {
+            return -1;
+        }
+        if (o2 == null) {
+            return 1;
+        }
         return o1.compareTo(o2);
     }
 }
