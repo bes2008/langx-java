a1d492b4704972416cc81cad5e4afbb3f4812bc1
fjn <fs1194361820@163.com>
2019-10-08 17:01:05 +0800

    * Supports execute a function without any exception

---
+++ src/main/java/com/jn/langx/util/ThrowableFunction.java
@@ -0,0 +1,16 @@
+package com.jn.langx.util;
+
+import com.jn.langx.util.function.Function;
+
+public abstract class ThrowableFunction<I,O> implements Function<I,O> {
+    @Override
+    public O apply(I i) {
+        try {
+            return doFun(i);
+        }catch (Throwable ex){
+            throw com.jn.langx.util.Throwables.wrapAsRuntimeException(ex);
+        }
+    }
+
+    public abstract O doFun(I i) throws Throwable;
+}
