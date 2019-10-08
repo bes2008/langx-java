e64695f5fc5ee322f1e86ca012ceda5fe2dcdd53
fjn <fs1194361820@163.com>
2019-10-08 17:00:29 +0800

    * Supports NOOP Converter

---
+++ src/main/java/com/jn/langx/util/converter/NoopConverter.java
@@ -0,0 +1,11 @@
+package com.jn.langx.util.converter;
+
+import com.jn.langx.Converter;
+
+public class NoopConverter implements Converter {
+    public static final NoopConverter INSTANCE = new NoopConverter();
+    @Override
+    public Object apply(Object input) {
+        return input;
+    }
+}
