9eaeb8ac6df12a789742bf3617bd4cc7ed992671
fs1194361820@163.com <fs1194361820@163.com>
2019-11-01 17:35:20 +0800

    + add network address struct

--- src/main/java/com/jn/langx/util/struct/NetworkAddress.java
+++ src/main/java/com/jn/langx/util/struct/NetworkAddress.java
@@ -1,15 +1,3 @@
-/*
- *  Copyright (c) 2016
- *  BES Software Corporation.
- *  All Rights Reserved
- *      Revision History:
- *                                 Modification       Tracking
- *  Author (Email ID)              Date               Number               Description
- *  -------------------------------------------------------------------------------------------
- *  jinuo.fang                     2019-04-16                              Initial version
- *
- */
-
 package com.jn.langx.util.struct;
 
 public class NetworkAddress {
