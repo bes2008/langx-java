00028d7d5c6ab8c8f762d6cde3d9da908d2d2571
fs1194361820@163.com <fs1194361820@163.com>
2019-11-11 20:46:12 +0800

    + supports more file filters

---
+++ src/main/java/com/jn/langx/util/io/file/FileFilters.java
@@ -0,0 +1,56 @@
+package com.jn.langx.util.io.file;
+
+import com.jn.langx.annotation.NonNull;
+import com.jn.langx.util.Emptys;
+import com.jn.langx.util.Preconditions;
+import com.jn.langx.util.collection.Pipeline;
+import com.jn.langx.util.function.Predicate;
+import com.jn.langx.util.io.file.filter.AbstractFileFilter;
+
+import java.io.File;
+import java.util.List;
+
+public class FileFilters {
+    public static FileFilter allFileFilter(@NonNull List<? extends FileFilter> predicates) {
+        Preconditions.checkTrue(Emptys.isNotEmpty(predicates));
+        final Pipeline<FileFilter> pipeline = Pipeline.<FileFilter>of(predicates);
+        return new AbstractFileFilter() {
+            @Override
+            public boolean accept(final File e) {
+                return pipeline.allMatch(new Predicate<FileFilter>() {
+                    @Override
+                    public boolean test(FileFilter fileFilter) {
+                        return fileFilter.test(e);
+                    }
+                });
+            }
+
+            @Override
+            public boolean accept(File dir, String name) {
+                return accept(new File(dir, name));
+            }
+
+        };
+    }
+
+    public static FileFilter anyFileFilter(@NonNull List<? extends FileFilter> predicates) {
+        Preconditions.checkTrue(Emptys.isNotEmpty(predicates));
+        final Pipeline<FileFilter> pipeline = Pipeline.<FileFilter>of(predicates);
+        return new AbstractFileFilter() {
+            @Override
+            public boolean accept(final File e) {
+                return pipeline.anyMatch(new Predicate<FileFilter>() {
+                    @Override
+                    public boolean test(FileFilter fileFilter) {
+                        return fileFilter.test(e);
+                    }
+                });
+            }
+
+            @Override
+            public boolean accept(File dir, String name) {
+                return accept(new File(dir, name));
+            }
+        };
+    }
+}
