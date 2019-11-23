d2a8e9f26f0e45f37f481eb12ece1020841b93b4
fs1194361820@163.com <fs1194361820@163.com>
2019-11-23 19:53:04 +0800

    * supports the mark annotation

---
+++ src/main/java/com/jn/langx/annotation/Singleton.java
@@ -0,0 +1,13 @@
+package com.jn.langx.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.ElementType.PACKAGE;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({TYPE, FIELD, METHOD, PARAMETER, CONSTRUCTOR, LOCAL_VARIABLE, ANNOTATION_TYPE, PACKAGE})
+public @interface Singleton {
+}
