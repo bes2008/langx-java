d06fde869ed2265cbe6797808407f04064d5ebde
fs1194361820@163.com <fs1194361820@163.com>
2019-11-27 11:51:52 +0800

    * supports sufflix, swap api

--- src/main/java/com/jn/langx/util/collection/Collects.java
+++ src/main/java/com/jn/langx/util/collection/Collects.java
@@ -3,6 +3,7 @@ package com.jn.langx.util.collection;
 import com.jn.langx.annotation.NonNull;
 import com.jn.langx.annotation.Nullable;
 import com.jn.langx.util.Emptys;
+import com.jn.langx.util.GlobalThreadLocalMap;
 import com.jn.langx.util.Preconditions;
 import com.jn.langx.util.collection.diff.*;
 import com.jn.langx.util.collection.iter.ArrayIterator;
@@ -1687,6 +1688,114 @@ public class Collects {
     public static <E, C extends Collection<E>> C clearNulls(@Nullable C collection) {
         final Collection<E> c = Collects.emptyCollectionByInfer(collection);
         return (C) filter(c, Functions.nonNullPredicate());
+    }
+
+
+    /**
+     * Randomly permutes the specified list using a default source of
+     * randomness.  All permutations occur with approximately equal
+     * likelihood.<p>
+     * <p>
+     * The hedge "approximately" is used in the foregoing description because
+     * default source of randomness is only approximately an unbiased source
+     * of independently chosen bits. If it were a perfect source of randomly
+     * chosen bits, then the algorithm would choose permutations with perfect
+     * uniformity.<p>
+     * <p>
+     * This implementation traverses the list backwards, from the last element
+     * up to the second, repeatedly swapping a randomly selected element into
+     * the "current position".  Elements are randomly selected from the
+     * portion of the list that runs from the first element to the current
+     * position, inclusive.<p>
+     * <p>
+     * This method runs in linear time.  If the specified list does not
+     * implement the {@link RandomAccess} interface and is large, this
+     * implementation dumps the specified list into an array before shuffling
+     * it, and dumps the shuffled array back into the list.  This avoids the
+     * quadratic behavior that would result from shuffling a "sequential
+     * access" list in place.
+     *
+     * @param list the list to be shuffled.
+     * @throws UnsupportedOperationException if the specified list or
+     *                                       its list-iterator does not support the <tt>set</tt> operation.
+     */
+    public static void shuffle(List<?> list) {
+        shuffle(list, GlobalThreadLocalMap.getRandom());
+    }
+
+    /**
+     * Randomly permute the specified list using the specified source of
+     * randomness.  All permutations occur with equal likelihood
+     * assuming that the source of randomness is fair.<p>
+     * <p>
+     * This implementation traverses the list backwards, from the last element
+     * up to the second, repeatedly swapping a randomly selected element into
+     * the "current position".  Elements are randomly selected from the
+     * portion of the list that runs from the first element to the current
+     * position, inclusive.<p>
+     * <p>
+     * This method runs in linear time.  If the specified list does not
+     * implement the {@link RandomAccess} interface and is large, this
+     * implementation dumps the specified list into an array before shuffling
+     * it, and dumps the shuffled array back into the list.  This avoids the
+     * quadratic behavior that would result from shuffling a "sequential
+     * access" list in place.
+     *
+     * @param list the list to be shuffled.
+     * @param rnd  the source of randomness to use to shuffle the list.
+     * @throws UnsupportedOperationException if the specified list or its
+     *                                       list-iterator does not support the <tt>set</tt> operation.
+     */
+    public static void shuffle(List<?> list, Random rnd) {
+        int size = list.size();
+        if (size < 5 || list instanceof RandomAccess) {
+            for (int i = size; i > 1; i--) {
+                swap(list, i - 1, rnd.nextInt(i));
+            }
+        } else {
+            Object arr[] = list.toArray();
+
+            // Shuffle array
+            for (int i = size; i > 1; i--) {
+                swap(arr, i - 1, rnd.nextInt(i));
+            }
+
+            // Dump array back into list
+            ListIterator it = list.listIterator();
+            for (int i = 0; i < arr.length; i++) {
+                it.next();
+                it.set(arr[i]);
+            }
+        }
+    }
+
+
+    /**
+     * Swaps the elements at the specified positions in the specified list.
+     * (If the specified positions are equal, invoking this method leaves
+     * the list unchanged.)
+     *
+     * @param list The list in which to swap elements.
+     * @param i    the index of one element to be swapped.
+     * @param j    the index of the other element to be swapped.
+     * @throws IndexOutOfBoundsException if either <tt>i</tt> or <tt>j</tt>
+     *                                   is out of range (i &lt; 0 || i &gt;= list.size()
+     *                                   || j &lt; 0 || j &gt;= list.size()).
+     */
+    public static void swap(@NonNull List<?> list, int i, int j) {
+        Preconditions.checkArgument(i>0, "index i is illegal : {}");
+        Preconditions.checkArgument(i>0, "index j is illegal : {}");
+        final List l = list;
+        l.set(i, l.set(j, l.get(i)));
+    }
+
+    /**
+     * Swaps the two specified elements in the specified array.
+     */
+    private static void swap(Object[] arr, int i, int j) {
+        Object tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
     }
 
 }
