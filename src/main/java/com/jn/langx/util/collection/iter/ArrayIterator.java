fa03187ce2ce452bbb04a7ed560fc8914338115d
fangjinuo <fs1194361820@163.com>
2019-12-27 20:22:36 +0800

    * supports any objects

--- src/main/java/com/jn/langx/util/collection/iter/ArrayIterator.java
+++ src/main/java/com/jn/langx/util/collection/iter/ArrayIterator.java
@@ -1,15 +1,33 @@
 package com.jn.langx.util.collection.iter;
 
+import com.jn.langx.util.Preconditions;
+import com.jn.langx.util.collection.Arrs;
+import com.jn.langx.util.collection.PrimitiveArrays;
+
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 /**
  * @author jinuo.fang
  */
-public class ArrayIterator<E> extends UnmodifiableIterator<E> implements Iterable<E>{
+public class ArrayIterator<E> extends UnmodifiableIterator<E> implements Iterable<E> {
     private final E[] array;
     private int index = 0;
     private final int length;
+
+    public ArrayIterator(Object array) {
+        if (array != null) {
+            Preconditions.checkArgument(Arrs.isArray(array));
+            if (PrimitiveArrays.isPrimitiveArray(array.getClass())) {
+                this.array = PrimitiveArrays.wrap(array);
+            } else {
+                this.array = (E[]) array;
+            }
+        } else {
+            this.array = null;
+        }
+        this.length = this.array == null ? 0 : this.array.length;
+    }
 
     public ArrayIterator(E[] array) {
         this.array = array;
