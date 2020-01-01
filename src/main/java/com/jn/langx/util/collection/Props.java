3ca14765f7d58bae38d7d24f474c9063d58d0d0e
fangjinuo <fs1194361820@163.com>
2020-01-01 18:37:19 +0800

    * supports more properties load methods

--- src/main/java/com/jn/langx/util/collection/Props.java
+++ src/main/java/com/jn/langx/util/collection/Props.java
@@ -1,16 +1,46 @@
 package com.jn.langx.util.collection;
 
 import com.jn.langx.io.resource.Resource;
+import com.jn.langx.io.resource.Resources;
 import com.jn.langx.util.Preconditions;
 import com.jn.langx.util.io.IOs;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.Reader;
+import java.io.*;
+import java.net.URL;
 import java.util.Properties;
 
 public class Props {
     private Props() {
+    }
+
+    public Properties loadFromFile(File file) throws IOException {
+        InputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream(file);
+            return load(inputStream);
+        } finally {
+            IOs.close(inputStream);
+        }
+    }
+
+    public Properties loadFromFile(String location) throws IOException {
+        return load(Resources.loadFileResource(location));
+    }
+
+    public Properties loadFromClasspath(String classpath) throws IOException {
+        return load(Resources.loadClassPathResource(classpath));
+    }
+
+    public Properties loadFromURL(String url) throws IOException {
+        return load(Resources.loadUrlResource(url));
+    }
+
+    public Properties loadFromURL(URL url) throws IOException {
+        return load(Resources.loadUrlResource(url));
+    }
+
+    public Properties loadFromString(String string) throws IOException {
+        return load(Resources.asByteArrayResource(string.getBytes()));
     }
 
     public Properties load(Resource resource) throws IOException {
@@ -36,18 +66,18 @@ public class Props {
         return props;
     }
 
-    public Properties loadFromXml(Resource resource) throws IOException {
+    public Properties loadFromXML(Resource resource) throws IOException {
         Preconditions.checkNotNull(resource);
         InputStream inputStream = null;
         try {
             inputStream = resource.getInputStream();
-            return loadFromXml(inputStream);
+            return loadFromXML(inputStream);
         } finally {
             IOs.close(inputStream);
         }
     }
 
-    public Properties loadFromXml(InputStream inputStream) throws IOException {
+    public Properties loadFromXML(InputStream inputStream) throws IOException {
         Properties props = new Properties();
         props.loadFromXML(inputStream);
         return props;
