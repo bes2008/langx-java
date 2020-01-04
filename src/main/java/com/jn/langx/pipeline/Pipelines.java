d30a03af969321b52c775e2c6715efe4b482a5e2
fs1194361820@163.com <fs1194361820@163.com>
2020-01-04 11:21:09 +0800

    * supply more static factory

--- src/main/java/com/jn/langx/pipeline/Pipelines.java
+++ src/main/java/com/jn/langx/pipeline/Pipelines.java
@@ -6,13 +6,21 @@ import com.jn.langx.util.function.Consumer;
 import java.util.List;
 
 public class Pipelines {
-    public static DefaultPipeline newPipeline(Handler headHandler, Handler tailHandler, List<Handler> handlers){
+
+    public static DefaultPipeline newPipeline(List<Handler> handlers) {
+        DefaultPipeline pipeline = new DefaultPipeline();
+        addHandlers(pipeline, handlers);
+        return pipeline;
+    }
+
+
+    public static DefaultPipeline newPipeline(Handler headHandler, Handler tailHandler, List<Handler> handlers) {
         DefaultPipeline pipeline = new DefaultPipeline(headHandler, tailHandler);
         addHandlers(pipeline, handlers);
         return pipeline;
     }
 
-    public static void addHandlers(final Pipeline pipeline, List<Handler> handlers){
+    public static void addHandlers(final Pipeline pipeline, List<Handler> handlers) {
         Collects.forEach(handlers, new Consumer<Handler>() {
             @Override
             public void accept(Handler handler) {
