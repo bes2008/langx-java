e17c68f563afc9d716822322d079367e79221a3f
fs1194361820@163.com <fs1194361820@163.com>
2020-01-04 14:16:12 +0800

    + add a testcase

---
+++ src/test/java/com/jn/langx/test/pipeline/PipelineTests.java
@@ -0,0 +1,25 @@
+package com.jn.langx.test.pipeline;
+
+import com.jn.langx.pipeline.DebugHandler;
+import com.jn.langx.pipeline.Handler;
+import com.jn.langx.pipeline.Pipeline;
+import com.jn.langx.pipeline.Pipelines;
+import com.jn.langx.test.bean.Person;
+import com.jn.langx.util.collection.Collects;
+import org.junit.Test;
+
+public class PipelineTests {
+    @Test
+    public void test() {
+        Person p = new Person();
+        p.setAge(3);
+        p.setId("id23232");
+        p.setName("hello world");
+        p.setDesc("description");
+
+        Pipeline pipeline = Pipelines.newPipeline(Collects.<Handler>asList(new DebugHandler()));
+        pipeline.bindTarget(p);
+        pipeline.handle();
+        pipeline.clear();
+    }
+}
