5d6f7dd4866a4261ac6945d6a0ae3eb8c29d0eb2
fangjinuo <fs1194361820@163.com>
2020-01-10 20:11:53 +0800

    * more methods

--- langx-java/src/main/java/com/jn/langx/repository/Repository2.java
+++ langx-java/src/main/java/com/jn/langx/repository/Repository2.java
@@ -3,6 +3,10 @@ package com.jn.langx.repository;
 import java.util.List;
 
 public interface Repository2<E, ID> extends Repository<E, ID> {
+    boolean has(ID id);
+
+    long count();
+
     List<E> getByIds(Iterable<ID> ids);
 
     void add(Iterable<E> entities);
