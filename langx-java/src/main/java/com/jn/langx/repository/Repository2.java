2b6ac24523c6a0bad94b3fb67a0febe0028e3d54
fangjinuo <fs1194361820@163.com>
2020-01-10 20:09:36 +0800

    * rename it

--- langx-java/src/main/java/com/jn/langx/repository/Repository2.java
+++ langx-java/src/main/java/com/jn/langx/repository/Repository2.java
@@ -2,7 +2,7 @@ package com.jn.langx.repository;
 
 import java.util.List;
 
-public interface BatchRepository<E, ID> extends Repository<E, ID> {
+public interface Repository2<E, ID> extends Repository<E, ID> {
     List<E> getByIds(Iterable<ID> ids);
 
     void add(Iterable<E> entities);
