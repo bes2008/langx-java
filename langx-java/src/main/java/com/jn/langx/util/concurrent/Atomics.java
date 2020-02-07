ad4d14aa77edfc240fad60c055971dccd8eb0b6c
fangjinuo <fs1194361820@163.com>
2020-02-07 11:01:34 +0800

    + supports atomics

---
+++ langx-java/src/main/java/com/jn/langx/util/concurrent/Atomics.java
@@ -0,0 +1,26 @@
+package com.jn.langx.util.concurrent;
+
+import com.jn.langx.annotation.Nullable;
+
+import java.util.concurrent.atomic.AtomicIntegerArray;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.atomic.AtomicReferenceArray;
+
+public class Atomics {
+    public static <R> AtomicReference<R> newReference(){
+        return new AtomicReference<R>();
+    }
+
+    public static <R> AtomicReference<R> newReference(@Nullable R initialValue){
+        return new AtomicReference<R>();
+    }
+
+    public static <R> AtomicReferenceArray<R> newReferenceArray(int length){
+        return new AtomicReferenceArray<R>(length);
+    }
+
+    public static <R> AtomicReferenceArray<R> newReferenceArray(R[] array){
+        return new AtomicReferenceArray<R>(array);
+    }
+
+}
