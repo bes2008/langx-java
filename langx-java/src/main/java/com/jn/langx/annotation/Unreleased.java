07e46369a1d5b3749b1d07646428348d3156d088
fangjinuo <fs1194361820@163.com>
2020-02-22 20:47:54 +0800

    + add an unreleased annotation

---
+++ langx-java/src/main/java/com/jn/langx/annotation/Unreleased.java
@@ -0,0 +1,16 @@
+package com.jn.langx.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Target(value = {TYPE, METHOD})
+
+public @interface Unreleased {
+}
