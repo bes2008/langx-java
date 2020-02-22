a765285767490117d3c5349d43615c2fe3c90be0
fangjinuo <fs1194361820@163.com>
2020-02-22 17:12:29 +0800

    + add more xml accessor

---
+++ langx-java/src/main/java/com/jn/langx/text/xml/XmlElementAttributeAccessor.java
@@ -0,0 +1,27 @@
+package com.jn.langx.text.xml;
+
+import com.jn.langx.util.BasedStringAccessor;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Element;
+
+public class XmlElementAttributeAccessor extends BasedStringAccessor<String, Element> {
+    @Override
+    public Object get(String attributeName) {
+        return getString(attributeName, null);
+    }
+
+    @Override
+    public String getString(String attributeName, String defaultValue) {
+        Attr attr = getTarget().getAttributeNode(attributeName);
+        return attr == null ? null : attr.getValue();
+    }
+
+    @Override
+    public void set(String attributeName, Object value) {
+        String attributeValue = value == null ? "" : value.toString();
+        Attr attr = getTarget().getAttributeNode(attributeName);
+        if (attr != null) {
+            attr.setValue(attributeValue);
+        }
+    }
+}
