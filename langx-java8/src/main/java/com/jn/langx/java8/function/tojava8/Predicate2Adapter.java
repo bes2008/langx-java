b05a86b8944fb53abbbc61e7b60c9693a0e9053d
fangjinuo <fs1194361820@163.com>
2020-02-23 19:50:04 +0800

    + adapter predicate2 to java8 BiPredicate

---
+++ langx-java8/src/main/java/com/jn/langx/java8/function/tojava8/Predicate2Adapter.java
@@ -0,0 +1,18 @@
+package com.jn.langx.java8.function.tojava8;
+
+import com.jn.langx.util.function.Predicate2;
+
+import java.util.function.BiPredicate;
+
+public class Predicate2Adapter<V1, V2> implements BiPredicate<V1, V2> {
+    private Predicate2<V1, V2> delegate;
+
+    public Predicate2Adapter(Predicate2<V1, V2> delegate) {
+        this.delegate = delegate;
+    }
+
+    @Override
+    public boolean test(V1 key, V2 value) {
+        return delegate.test(key, value);
+    }
+}
