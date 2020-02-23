85909563848f9e4db78e5d0cb0e28f38e35ae10c
fangjinuo <fs1194361820@163.com>
2020-02-23 20:00:17 +0800

    + adapter consumer to java8 Consumer

---
+++ langx-java8/src/main/java/com/jn/langx/java8/function/tojava8/ConsumerAdapter.java
@@ -0,0 +1,16 @@
+package com.jn.langx.java8.function.tojava8;
+
+import java.util.function.Consumer;
+
+public class ConsumerAdapter<V> implements Consumer<V> {
+    private com.jn.langx.util.function.Consumer<V> delegate;
+
+    public ConsumerAdapter(com.jn.langx.util.function.Consumer<V> delegate) {
+        this.delegate = delegate;
+    }
+
+    @Override
+    public void accept(V v) {
+        this.delegate.accept(v);
+    }
+}
