a25003c4a2685981841c3a527f087c71cadd8243
fangjinuo <fs1194361820@163.com>
2020-02-27 11:06:48 +0800

    * refactor it

--- langx-java/src/main/java/com/jn/langx/util/reflect/type/ParameterizedTypeGetter.java
+++ langx-java/src/main/java/com/jn/langx/util/reflect/type/ParameterizedTypeGetter.java
@@ -19,9 +19,40 @@ import java.lang.reflect.Type;
  *
  * @param <T>
  */
-public class ParameterizedTypeGetter<T> {
-    public Type getType() {
-        ParameterizedType genericSuperclass = (ParameterizedType) getClass().getGenericSuperclass();
-        return genericSuperclass.getActualTypeArguments()[0];
+public abstract class ParameterizedTypeGetter<T> {
+    private final Type rawType;
+
+    protected ParameterizedTypeGetter() {
+        rawType = getSuperclassTypeParameter(getClass());
+    }
+
+    Type getSuperclassTypeParameter(Class<?> clazz) {
+        Type genericSuperclass = clazz.getGenericSuperclass();
+        if (genericSuperclass instanceof Class) {
+            // try to climb up the hierarchy until meet something useful
+            if (ParameterizedTypeGetter.class != genericSuperclass) {
+                return getSuperclassTypeParameter(clazz.getSuperclass());
+            }
+
+            throw new RuntimeException("'" + getClass() + "' extends TypeReference but misses the type parameter. "
+                    + "Remove the extension or add a type parameter to it.");
+        }
+
+        Type rawType = ((ParameterizedType) genericSuperclass).getActualTypeArguments()[0];
+        // TODO remove this when Reflector is fixed to return Types
+        if (rawType instanceof ParameterizedType) {
+            rawType = ((ParameterizedType) rawType).getRawType();
+        }
+
+        return rawType;
+    }
+
+    public final Type getRawType() {
+        return rawType;
+    }
+
+    @Override
+    public String toString() {
+        return rawType.toString();
     }
 }
