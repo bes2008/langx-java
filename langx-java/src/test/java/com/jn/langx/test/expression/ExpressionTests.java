54f180c735a4dc92708f5c1cd0b671010f6d872a
fangjinuo <fs1194361820@163.com>
2020-03-30 10:08:56 +0800

    + add a test case for expression

---
+++ langx-java/src/test/java/com/jn/langx/test/expression/ExpressionTests.java
@@ -0,0 +1,34 @@
+package com.jn.langx.test.expression;
+
+import com.jn.langx.expression.operator.compare.GE;
+import com.jn.langx.expression.operator.compare.NI;
+import com.jn.langx.expression.operator.logic.And;
+import com.jn.langx.expression.value.StringExpression;
+import com.jn.langx.expression.value.ValueExpression;
+import com.jn.langx.util.collection.Collects;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ExpressionTests {
+    @Test
+    public void testLogicOperator() {
+        GE ge = new GE();
+        ge.setLeft(new StringExpression("zhangsan"));
+        ge.setRight(new StringExpression("zhangsa"));
+
+        NI ni = new NI();
+        ni.setLeft(new StringExpression("zhangsan2"));
+        ni.setRight(new ValueExpression(Collects.asList("wangwu", "zhangsan", "lisi")));
+
+        And and = new And();
+        and.setLeft(ge);
+        and.setRight(ni);
+
+        System.out.println(and.toString());
+        Assert.assertTrue(and.execute().execute());
+
+
+
+
+    }
+}
