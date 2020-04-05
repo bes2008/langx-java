eb4e2d348fa5811d9d952471a5f82ffcec742144
fangjinuo <fs1194361820@163.com>
2020-04-05 11:52:55 +0800

    * refactor code

--- langx-java/src/main/java/com/jn/langx/expression/BooleanResultExpression.java
+++
@@ -1,5 +0,0 @@
-package com.jn.langx.expression;
-
-
-public interface BooleanResultExpression extends Expression<Boolean> {
-}
---
+++ langx-java/src/main/java/com/jn/langx/expression/value/BooleanResultExpression.java
@@ -0,0 +1,7 @@
+package com.jn.langx.expression.value;
+
+
+import com.jn.langx.expression.Expression;
+
+public interface BooleanResultExpression extends Expression<Boolean> {
+}
