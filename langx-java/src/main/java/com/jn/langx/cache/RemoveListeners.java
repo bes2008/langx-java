978fdd81f36fc96835438deb63f214303654105e
fs1194361820@163.com <fs1194361820@163.com>
2020-04-12 18:10:08 +0800

    + supports async RemoveListener

---
+++ langx-java/src/main/java/com/jn/langx/cache/RemoveListeners.java
@@ -0,0 +1,25 @@
+package com.jn.langx.cache;
+
+import com.jn.langx.util.Preconditions;
+import com.jn.langx.util.concurrent.WrappedTasks;
+
+import java.util.concurrent.Executor;
+
+public class RemoveListeners {
+    private RemoveListeners(){}
+    public static <K,V> RemoveListener<K,V> async(final RemoveListener<K,V> listener, final Executor executor){
+        Preconditions.checkNotNull(listener);
+        Preconditions.checkNotNull(executor);
+        return new RemoveListener<K, V>() {
+            @Override
+            public void onRemove(final K key, final V value, final RemoveCause cause) {
+                executor.execute(WrappedTasks.wrap(new Runnable() {
+                    @Override
+                    public void run() {
+                        listener.onRemove(key, value, cause);
+                    }
+                }));
+            }
+        };
+    }
+}
