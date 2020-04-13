2f8f14827ee332dc54c9ea040c5d46baf9f79328
fs1194361820@163.com <fs1194361820@163.com>
2020-04-13 15:22:31 +0800

    + 支持全量加载

---
+++ langx-java/src/main/java/com/jn/langx/configuration/FullLoadConfigurationLoader.java
@@ -0,0 +1,11 @@
+package com.jn.langx.configuration;
+
+import java.util.List;
+
+/**
+ * 支持全量加载的 Loader
+ * @param <T>
+ */
+public interface FullLoadConfigurationLoader<T extends Configuration> extends ConfigurationLoader<T> {
+    List<T> loadAll();
+}
