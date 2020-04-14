b0f3469c23cc409656017fe4713d644393c4b16f
fs1194361820@163.com <fs1194361820@163.com>
2020-04-14 10:38:59 +0800

    * override method

--- langx-java/src/main/java/com/jn/langx/configuration/AbstractMultipleLevelConfigurationRepository.java
+++ langx-java/src/main/java/com/jn/langx/configuration/AbstractMultipleLevelConfigurationRepository.java
@@ -3,8 +3,11 @@ package com.jn.langx.configuration;
 import com.jn.langx.util.collection.Collects;
 import com.jn.langx.util.collection.NonAbsentHashMap;
 import com.jn.langx.util.collection.Pipeline;
+import com.jn.langx.util.function.Consumer;
 import com.jn.langx.util.function.Consumer2;
+import com.jn.langx.util.function.Predicate;
 import com.jn.langx.util.function.Supplier;
+import com.jn.langx.util.struct.Holder;
 
 import java.util.Comparator;
 import java.util.Map;
@@ -17,6 +20,7 @@ public abstract class AbstractMultipleLevelConfigurationRepository<T extends Con
             return Integer.MAX_VALUE;
         }
     });
+
     private Map<String, ConfigurationRepository> delegates = new TreeMap<String, ConfigurationRepository>(new Comparator<String>() {
         @Override
         public int compare(String repositoryName1, String repositoryName2) {
@@ -27,11 +31,91 @@ public abstract class AbstractMultipleLevelConfigurationRepository<T extends Con
     @Override
     public void startup() {
         // reverse it
-        Collects.forEach(Collects.reverse(Collects.asList(delegates.values()),true), new Consumer2<Integer, ConfigurationRepository>() {
+        Pipeline.of(delegates.values()).reverse(true).forEach(new Consumer2<Integer, ConfigurationRepository>() {
             @Override
             public void accept(Integer index, ConfigurationRepository repository) {
                 repository.startup();
             }
         });
+        super.startup();
+    }
+
+    public void shutdown() {
+        super.shutdown();
+        Pipeline.of(delegates.values()).forEach(new Consumer<ConfigurationRepository>() {
+            @Override
+            public void accept(ConfigurationRepository configurationRepository) {
+                configurationRepository.shutdown();
+            }
+        });
+    }
+
+    @Override
+    public T getById(final String id) {
+        T t = super.getById(id);
+
+        if (t == null) {
+            final Holder<T> holder = new Holder<T>();
+            Collects.forEach(delegates.values(), new Consumer<ConfigurationRepository>() {
+                @Override
+                public void accept(ConfigurationRepository configurationRepository) {
+                    holder.set((T) configurationRepository.getById(id));
+                }
+            }, new Predicate<ConfigurationRepository>() {
+                @Override
+                public boolean test(ConfigurationRepository configurationRepository) {
+                    return !holder.isNull();
+                }
+            });
+            t = holder.get();
+        }
+        return t;
+    }
+
+    public void removeById(final String id, final boolean sync) {
+        super.removeById(id, sync);
+        Collects.forEach(delegates.values(), new Consumer<ConfigurationRepository>() {
+            @Override
+            public void accept(ConfigurationRepository configurationRepository) {
+                configurationRepository.removeById(id, sync);
+            }
+        });
+    }
+
+    private ConfigurationRepository findFirstRepository(final String id) {
+        if (super.getById(id) != null) {
+            return this;
+        }
+        return Collects.findFirst(delegates.values(), new Predicate<ConfigurationRepository>() {
+            @Override
+            public boolean test(ConfigurationRepository value) {
+                return value.getById(id) != null;
+            }
+        });
+    }
+
+    @Override
+    public T add(T configuration, boolean sync) {
+        return super.add(configuration, sync);
+    }
+
+    public void update(T configuration, boolean sync) {
+        ConfigurationRepository old = findFirstRepository(configuration.getId());
+        if (old != null) {
+            old.update(configuration, sync);
+        } else {
+            super.update(configuration, sync);
+        }
+    }
+
+    public Map<String, T> getAll() {
+        final Map<String, T> map = Collects.<String, T>emptyHashMap(true);
+        Pipeline.of(delegates.values()).reverse(true).forEach(new Consumer<ConfigurationRepository>() {
+            @Override
+            public void accept(ConfigurationRepository configurationRepository) {
+                map.putAll(configurationRepository.getAll());
+            }
+        });
+        return map;
     }
 }
