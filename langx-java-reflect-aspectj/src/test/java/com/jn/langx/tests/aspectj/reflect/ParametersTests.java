44f8441a8ab53606641624cc15309ae2549deed0
fs1194361820@163.com <fs1194361820@163.com>
2020-05-17 19:24:24 +0800

    + 添加Parameter API测试用例

---
+++ langx-java-reflect-aspectj/src/test/java/com/jn/langx/tests/aspectj/reflect/ParametersTests.java
@@ -0,0 +1,38 @@
+package com.jn.langx.tests.aspectj.reflect;
+
+
+import com.jn.langx.aspectj.reflect.AjReflectConstants;
+import com.jn.langx.java8.util.reflect.parameter.Java8MethodParameter;
+import com.jn.langx.util.reflect.Reflects;
+import com.jn.langx.util.reflect.parameter.ConstructorParameter;
+import com.jn.langx.util.reflect.parameter.MethodParameter;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.List;
+
+public class ParametersTests {
+
+    @BeforeClass
+    public static void init() {
+
+    }
+
+    private static void init0(boolean booleanValue, Integer intValue, String stringValue) {
+
+    }
+
+    @Test
+    public void getMethodParametersTest() throws NoSuchMethodException {
+        Method method = Reflects.getAnyMethod(ParametersTests.class, "init0", Boolean.TYPE, Integer.class, String.class);
+        List<MethodParameter> parameters = Reflects.getMethodParameters(AjReflectConstants.DEFAULT_PARAMETER_SUPPLIER_NAME, method);
+        System.out.println(parameters);
+        Constructor constructor = Reflects.getConstructor(Java8MethodParameter.class, Parameter.class);
+        List<ConstructorParameter> constructorParameters = Reflects.getConstructorParameters(AjReflectConstants.DEFAULT_PARAMETER_SUPPLIER_NAME, constructor);
+        System.out.println(constructorParameters);
+
+    }
+}
