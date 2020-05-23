c6324d7dc6fa04e1ffa0a8b70de7dc01f215d454
fs1194361820@163.com <fs1194361820@163.com>
2020-05-23 18:23:39 +0800

    * 实现 DirectoryClasspath

--- langx-java/src/main/java/com/jn/langx/classpath/Classpaths.java
+++ langx-java/src/main/java/com/jn/langx/classpath/Classpaths.java
@@ -5,9 +5,14 @@ import com.jn.langx.util.Preconditions;
 
 public class Classpaths {
     public static String getPath(@NonNull String path, boolean isClass) {
+        return getPath(path, isClass, null);
+    }
+
+    public static String getPath(@NonNull String path, boolean isClass, String pathSeparator) {
+        pathSeparator = pathSeparator == null ? "/" : pathSeparator;
         Preconditions.checkNotNull(path, "path is null or empty");
         if (isClass) {
-            path = path.replace('.', '/') + ".class";
+            path = path.replace("\\.", pathSeparator) + ".class";
         }
         return path;
     }
--- langx-java/src/main/java/com/jn/langx/classpath/cp/DirectoryClasspath.java
+++ langx-java/src/main/java/com/jn/langx/classpath/cp/DirectoryClasspath.java
@@ -1,32 +1,57 @@
 package com.jn.langx.classpath.cp;
 
+import com.jn.langx.annotation.NonNull;
 import com.jn.langx.classpath.Classpaths;
 import com.jn.langx.io.resource.DirectoryBasedFileResourceLoader;
+import com.jn.langx.io.resource.FileResource;
 import com.jn.langx.io.resource.Location;
 import com.jn.langx.io.resource.Resource;
+import com.jn.langx.util.collection.Collects;
+import com.jn.langx.util.function.Consumer;
 
+import java.io.File;
 import java.util.Set;
 
 public class DirectoryClasspath extends AbstractClasspath {
     private DirectoryBasedFileResourceLoader loader;
+    private Location root;
 
     public DirectoryClasspath(String rootDirectory) {
-        loader = new DirectoryBasedFileResourceLoader(rootDirectory);
+        this.root = new Location(FileResource.PREFIX, rootDirectory, "/");
+        this.loader = new DirectoryBasedFileResourceLoader(rootDirectory);
     }
 
     @Override
     public Resource findResource(String relativePath, boolean isClass) {
-        relativePath = Classpaths.getPath(relativePath, isClass);
+        relativePath = Classpaths.getPath(relativePath, isClass, root.getPathSeparator());
         return loader.loadResource(relativePath);
     }
 
     @Override
     public Location getRoot() {
-        return null;
+        return root;
     }
 
     @Override
     public Set<Location> allResources() {
-        return null;
+        File rootFile = new File(root.getPath());
+        Set<Location> locations = Collects.emptyTreeSet();
+        if (rootFile.exists() && rootFile.isDirectory()) {
+            scan(locations, rootFile);
+        }
+        return locations;
+    }
+
+    private void scan(@NonNull final Set<Location> results, @NonNull final File current) {
+        if (current.isFile()) {
+            results.add(new Location(FileResource.PREFIX, current.getAbsolutePath()));
+        } else {
+            Collects.forEach(current.listFiles(), new Consumer<File>() {
+                @Override
+                public void accept(File file) {
+                    scan(results, file);
+                }
+            });
+        }
     }
 }
