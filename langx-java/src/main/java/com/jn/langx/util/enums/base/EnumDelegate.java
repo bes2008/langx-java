6fd84c4e26bad401fd8b1170689af991f1ed20db
fs1194361820@163.com <fs1194361820@163.com>
2020-05-29 10:10:32 +0800

    * 更新注释

--- langx-java/src/main/java/com/jn/langx/util/enums/base/EnumDelegate.java
+++ langx-java/src/main/java/com/jn/langx/util/enums/base/EnumDelegate.java
@@ -5,36 +5,29 @@ package com.jn.langx.util.enums.base;
  * <p>
  * you can declare any enum like this:
  * <pre>
- *     public enum Period implements Delegatable<EnumDelegate> {
+ *     public enum Period implements CommonEnum,Delegatable<EnumDelegate> {
  *         MINUTES(0, "minutes", "minutes"),
  *         HOURS(1, "hours", "hours"),
  *         DAY(2, "day", "day"),
  *         MONTH(3, "month", "month");
  *
  *
- *         private int code;
- *         private String name;
- *         private String displayText;
  *         private EnumDelegate delegate;
  *
  *         Period(int code, String name, String displayText){
- *             this.code = code;
- *             this.name = name;
- *             this.displayText = displayText;
- *
  *             setDelegate(new EnumDelegate(code, name, displayText));
  *         }
  *
  *         public int getCode(){
- *             return code;
+ *             return this.delegate.getCode();
  *         }
  *
  *         public String getName(){
- *             return name;
+ *             return this.delegate.getName();
  *         }
  *
  *         public String getDisplayText(){
- *             return displayText;
+ *             return this.delegate.getDisplayText();
  *         }
  *
  *         public EnumDelegate getDelegate(){
@@ -51,13 +44,13 @@ package com.jn.langx.util.enums.base;
  * for examples:
  * <pre>
  *     // get an enum instance by code
- *     Period p = CommonEnums.ofCode(0);
+ *     Period p = Enums.ofCode(0);
  *
  *     // get an enum instance by name
- *     Period p = CommonEnums.ofName("minutes");
+ *     Period p = Enums.ofName("minutes");
  *
  *     // get an enum instance by displayText
- *     Period p = CommonEnums.ofDisplayText("minutes");
+ *     Period p = Enums.ofDisplayText("minutes");
  * </pre>
  */
 public class EnumDelegate implements CommonEnum {
