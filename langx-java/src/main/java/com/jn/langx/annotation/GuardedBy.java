f765d352c9ba76bc90e18b122e552933d39fca87
fs1194361820@163.com <fs1194361820@163.com>
2020-07-09 10:10:35 +0800

    * 支持 concurrent Linked hash map

---
+++ langx-java/src/main/java/com/jn/langx/annotation/GuardedBy.java
@@ -0,0 +1,16 @@
+package com.jn.langx.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.ElementType.LOCAL_VARIABLE;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Target(value = {TYPE, FIELD, METHOD, PARAMETER, CONSTRUCTOR, LOCAL_VARIABLE})
+public @interface GuardedBy {
+    public String value();
+}
---
+++ langx-java/src/main/java/com/jn/langx/util/concurrent/clhm/readme.md
@@ -0,0 +1,1 @@
+copy from google code ConcurrentLinkedHashMap
