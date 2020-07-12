0fd4d6ea7e079c5668cb3e4ce889f9b6059f099e
fs1194361820@163.com <fs1194361820@163.com>
2020-07-12 21:34:39 +0800

    * 更新aspectj版本

--- langx-java-reflect-aspectj/src/test/java/com/jn/langx/tests/aspectj/reflect/ParametersTests.java
+++ langx-java-reflect-aspectj/src/test/java/com/jn/langx/tests/aspectj/reflect/ParametersTests.java
@@ -2,19 +2,14 @@ package com.jn.langx.tests.aspectj.reflect;
 
 
 import com.jn.langx.aspectj.reflect.AjReflectConstants;
-import com.jn.langx.java8.util.reflect.parameter.Java8MethodParameter;
 import com.jn.langx.util.reflect.Reflects;
-import com.jn.langx.util.reflect.parameter.ConstructorParameter;
 import com.jn.langx.util.reflect.parameter.MethodParameter;
 import com.jn.langx.util.reflect.type.Types;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
-import java.lang.reflect.Parameter;
 import java.lang.reflect.Type;
-import java.sql.Ref;
 import java.util.List;
 
 public class ParametersTests {
@@ -30,17 +25,18 @@ public class ParametersTests {
 
     @Test
     public void getMethodParametersTest() throws NoSuchMethodException {
+        /*
         Method method = Reflects.getAnyMethod(ParametersTests.class, "init0", Boolean.TYPE, Integer.class, String.class);
         List<MethodParameter> parameters = Reflects.getMethodParameters(AjReflectConstants.DEFAULT_PARAMETER_SUPPLIER_NAME, method);
         System.out.println(parameters);
         Constructor constructor = Reflects.getConstructor(Java8MethodParameter.class, Parameter.class);
         List<ConstructorParameter> constructorParameters = Reflects.getConstructorParameters(AjReflectConstants.DEFAULT_PARAMETER_SUPPLIER_NAME, constructor);
         System.out.println(constructorParameters);
+        */
 
-
-        Method showMethod = Reflects.getAnyMethod(ParametersTests.class,"show", List.class);
+        Method showMethod = Reflects.getAnyMethod(ParametersTests.class, "show", List.class);
         List<MethodParameter> showMethodParameters = Reflects.getMethodParameters(AjReflectConstants.DEFAULT_PARAMETER_SUPPLIER_NAME, showMethod);
-        Type type = Types.resolve(null, null,showMethodParameters.get(0).getParameterizedType());
+        Type type = Types.resolve(null, null, showMethodParameters.get(0).getParameterizedType());
         System.out.println(showMethodParameters);
     }
 
