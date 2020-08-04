5bc29ad3cab4be2e9725f64a8fe687bee53340fe
fs1194361820@163.com <fs1194361820@163.com>
2020-08-04 11:21:52 +0800

    * 功能增强

--- langx-java/src/main/java/com/jn/langx/util/collection/multivalue/MultiValueMapAccessor.java
+++ langx-java/src/main/java/com/jn/langx/util/collection/multivalue/MultiValueMapAccessor.java
@@ -2,12 +2,10 @@ package com.jn.langx.util.collection.multivalue;
 
 import com.jn.langx.util.BasedStringAccessor;
 import com.jn.langx.util.Emptys;
-import com.jn.langx.util.converter.ConverterService;
 
 import java.util.List;
 
 public class MultiValueMapAccessor<V> extends BasedStringAccessor<String, MultiValueMap<String, V>> {
-    private ConverterService converterService = new ConverterService();
 
     @Override
     public Object get(String key) {
