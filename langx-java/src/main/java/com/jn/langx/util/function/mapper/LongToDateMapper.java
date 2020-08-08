9c11ea10999441eefa50c62275d73ebdd882b267
fs1194361820@163.com <fs1194361820@163.com>
2020-08-08 18:19:06 +0800

    * refactor code

--- langx-java/src/main/java/com/jn/langx/util/function/mapper/LongToDateMapper.java
+++ langx-java/src/main/java/com/jn/langx/util/function/mapper/LongToDateMapper.java
@@ -1,6 +1,6 @@
 package com.jn.langx.util.function.mapper;
 
-import com.jn.langx.util.converter.LongToDateConverter;
+import com.jn.langx.util.converter.ConverterService;
 import com.jn.langx.util.function.Mapper;
 
 import java.util.Date;
@@ -8,6 +8,6 @@ import java.util.Date;
 public class LongToDateMapper implements Mapper<Long, Date> {
     @Override
     public Date apply(Long value) {
-        return LongToDateConverter.INSTANCE.apply(value);
+        return ConverterService.DEFAULT.convert(value, Date.class);
     }
 }
--- langx-java/src/main/java/com/jn/langx/util/function/mapper/literal/LongStringToDateMapper.java
+++ langx-java/src/main/java/com/jn/langx/util/function/mapper/literal/LongStringToDateMapper.java
@@ -1,6 +1,6 @@
 package com.jn.langx.util.function.mapper.literal;
 
-import com.jn.langx.util.converter.LongToDateConverter;
+import com.jn.langx.util.converter.ConverterService;
 import com.jn.langx.util.function.mapper.StringLiteralMapper;
 
 import java.util.Date;
@@ -8,7 +8,7 @@ import java.util.Date;
 public class LongStringToDateMapper implements StringLiteralMapper<Date> {
     @Override
     public Date apply(String value) {
-        Long v = new ToLongMapper().apply(value);
-        return LongToDateConverter.INSTANCE.apply(v);
+        Long v = ConverterService.DEFAULT.convert(value, Long.class);
+        return ConverterService.DEFAULT.convert(v, Date.class);
     }
 }
