c57d35fc8cd71b2ca5926463094198ce3ff9f0c5
fs1194361820@163.com <fs1194361820@163.com>
2020-08-14 12:45:17 +0800

    * 更新测试用例

--- langx-java/src/test/java/com/jn/langx/test/security/signature/dsa/OpenSSLDSATests.java
+++ langx-java/src/test/java/com/jn/langx/test/security/signature/dsa/OpenSSLDSATests.java
@@ -2,14 +2,25 @@ package com.jn.langx.test.security.signature.dsa;
 
 import com.jn.langx.io.resource.Resource;
 import com.jn.langx.io.resource.Resources;
+import com.jn.langx.security.DSAs;
 import com.jn.langx.security.KeyFileIOs;
+import com.jn.langx.util.io.Charsets;
+import org.junit.Test;
 
 public class OpenSSLDSATests {
 
+    @Test
     public void test() {
-        Resource privateResource = Resources.loadClassPathResource("/com/jn/langx/test/security/signature/dsa/data/openssl/mydsa_pri_pkcs8.pem");
+        Resource privateResource = Resources.loadClassPathResource("/security/dsa/data/openssl/mydsa_pri_pkcs8.pem");
         byte[] privateKey = KeyFileIOs.readKeyFileAndBase64Decode(privateResource);
-        
+        String content = "你好，OpenSSL 生成的 DSA";
+        byte[] data = content.getBytes(Charsets.UTF_8);
+        byte[] signature = DSAs.sign(privateKey, data);
+        Resource publicResource =   Resources.loadClassPathResource("/security/dsa/data/openssl/mydsa_pub.pem");
+        byte[] publicKey = KeyFileIOs.readKeyFileAndBase64Decode(publicResource);
+        if(DSAs.verify(publicKey, data, signature)){
+            System.out.println("验证通过");
+        }
     }
 
 }
