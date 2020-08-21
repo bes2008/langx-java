672c4a354c0a6ca380f0c893e9014c7fd143b8b4
fs1194361820@163.com <fs1194361820@163.com>
2020-08-21 11:28:08 +0800

    * 提供单例

--- langx-java/src/main/java/com/jn/langx/util/comparator/EqualsComparator.java
+++ langx-java/src/main/java/com/jn/langx/util/comparator/EqualsComparator.java
@@ -3,6 +3,8 @@ package com.jn.langx.util.comparator;
 import java.util.Comparator;
 
 public final class EqualsComparator<V> implements Comparator<V> {
+    public static final EqualsComparator INSTANCE = new EqualsComparator();
+
     @Override
     public int compare(V o1, V o2) {
         return o1.equals(o2) ? 0 : o1.hashCode() - o2.hashCode();
