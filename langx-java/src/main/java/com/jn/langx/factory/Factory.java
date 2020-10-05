beda26fd1e31650b482662815d7107973ce22d26
fangjinuo <fs1194361820@163.com>
2020-10-05 11:10:26 +0800

    * 简化代码

--- langx-java/src/main/java/com/jn/langx/factory/Factory.java
+++ langx-java/src/main/java/com/jn/langx/factory/Factory.java
@@ -3,4 +3,6 @@ package com.jn.langx.factory;
 import com.jn.langx.util.function.Supplier;
 
 public interface Factory<I, O> extends Supplier<I, O> {
+    @Override
+    O get(I input);
 }
--- langx-java/src/main/java/com/jn/langx/util/valuegetter/ValueGetter.java
+++ langx-java/src/main/java/com/jn/langx/util/valuegetter/ValueGetter.java
@@ -3,5 +3,6 @@ package com.jn.langx.util.valuegetter;
 import com.jn.langx.factory.Factory;
 
 public interface ValueGetter<I,O> extends Factory<I,O> {
-
+    @Override
+    O get(I input);
 }
