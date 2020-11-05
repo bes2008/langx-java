beb453e1d1b8e38fbb745f45365a4427ae7feac6
fjn <1194361820@qq.com>
2020-11-05 18:13:13 +0800

    * 增强 HashCodeBuilder

--- langx-java/src/main/java/com/jn/langx/util/hash/HashCodeBuilder.java
+++ langx-java/src/main/java/com/jn/langx/util/hash/HashCodeBuilder.java
@@ -43,4 +43,54 @@ public class HashCodeBuilder implements Builder<Integer> {
     public int hashCode() {
         return hash;
     }
+
+
+    public static int generate(boolean value) {
+        return value ? 1231 : 1237;
+    }
+
+    public static int generate(long value) {
+        return (int)(value ^ value >> 32);
+    }
+
+    public static int generate(double value) {
+        return generate(Double.doubleToLongBits(value));
+    }
+
+    public static int generate(float value) {
+        return Float.floatToIntBits(value);
+    }
+
+    public static int generate(byte[] bytes) {
+        int hashcode = 0;
+
+        for(int i = 0; i < bytes.length; ++i) {
+            hashcode <<= 1;
+            hashcode ^= bytes[i];
+        }
+
+        return hashcode;
+    }
+
+    public static int generate(Object[] array, boolean deep) {
+        int hashcode = 0;
+
+        for(int i = 0; i < array.length; ++i) {
+            if (deep && array[i] instanceof Object[]) {
+                hashcode ^= generate((Object[])((Object[])array[i]), true);
+            } else {
+                hashcode ^= array[i].hashCode();
+            }
+        }
+
+        return hashcode;
+    }
+
+    public static int generate(Object[] array) {
+        return generate(array, false);
+    }
+
+    public static int generate(Object obj) {
+        return obj != null ? obj.hashCode() : 0;
+    }
 }
