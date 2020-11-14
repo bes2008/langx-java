33dbe57facbb0e4ec75ffd589129797e95001dea
fjn <1194361820@qq.com>
2020-11-14 13:18:47 +0800

    * 移除无用的imports

--- langx-java/src/main/java/com/jn/langx/proxy/ConstructorInvocation.java
+++ langx-java/src/main/java/com/jn/langx/proxy/ConstructorInvocation.java
@@ -1,7 +1,5 @@
 package com.jn.langx.proxy;
 
-import com.jn.langx.proxy.Invocation;
-
 import java.lang.reflect.Constructor;
 
 public interface ConstructorInvocation extends Invocation<Constructor> {
