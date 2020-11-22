764ca69d43c943942c8b8b303198dc4dd049a748
fjn <1194361820@qq.com>
2020-11-22 17:36:12 +0800

    * 代码重构

--- langx-java/src/main/java/com/jn/langx/text/i18n/SimpleResourceBundleI18nMessageStorage.java
+++ langx-java/src/main/java/com/jn/langx/text/i18n/SimpleResourceBundleI18nMessageStorage.java
@@ -2,10 +2,7 @@ package com.jn.langx.text.i18n;
 
 import com.jn.langx.annotation.NonNull;
 import com.jn.langx.annotation.Nullable;
-import com.jn.langx.text.StringTemplates;
-import com.jn.langx.util.Emptys;
 import com.jn.langx.util.Preconditions;
-import com.jn.langx.util.function.Function2;
 
 import java.util.Locale;
 import java.util.ResourceBundle;
@@ -21,27 +18,7 @@ public class SimpleResourceBundleI18nMessageStorage extends AbstractResourceBund
 
     @Override
     protected String getMessageInternal(@Nullable final String basename, @Nullable final Locale locale, final @Nullable ClassLoader classLoader, @NonNull String key, Object... args) {
-        if (bundle == null) {
-            return null;
-        }
-        String message = bundle.getString(key);
-        if (args == null) {
-            args = Emptys.EMPTY_OBJECTS;
-        }
-
-        message = StringTemplates.format(message, "${", "}", new Function2<String, Object[], String>() {
-            @Override
-            public String apply(String variable, Object[] args) {
-                return getMessageInternal(basename, locale, classLoader, variable);
-            }
-        });
-
-        // https://blog.csdn.net/new03/article/details/84826958
-        // 使用 {0},{1},{2}... 来进行参数替换
-        //MessageFormat formatter = new MessageFormat(message, locale);
-        //message = formatter.format(args);
-        message = StringTemplates.formatWithIndex(message, args);
-        return message;
+        return ResourceBundles.getString(bundle, key, args);
     }
 
 }
