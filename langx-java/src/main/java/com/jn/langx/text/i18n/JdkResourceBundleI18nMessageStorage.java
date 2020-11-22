7ed2d85cc125e5a84f1cc308a86ff1c48a6a5a50
fjn <1194361820@qq.com>
2020-11-22 17:33:07 +0800

    * 代码重构

--- langx-java/src/main/java/com/jn/langx/text/i18n/JdkResourceBundleI18nMessageStorage.java
+++ langx-java/src/main/java/com/jn/langx/text/i18n/JdkResourceBundleI18nMessageStorage.java
@@ -1,38 +1,11 @@
 package com.jn.langx.text.i18n;
 
-import com.jn.langx.text.StringTemplates;
-import com.jn.langx.util.Emptys;
-import com.jn.langx.util.function.Function2;
-
 import java.util.Locale;
-import java.util.ResourceBundle;
 
 public class JdkResourceBundleI18nMessageStorage extends AbstractResourceBundleI18nMessageStorage {
 
     @Override
     protected String getMessageInternal(final String basename, final Locale locale, final ClassLoader classLoader, String key, Object... args) {
-
-        ResourceBundle bundle = ResourceBundle.getBundle(basename, locale, classLoader);
-        if (bundle == null) {
-            return null;
-        }
-        String message = bundle.getString(key);
-        if (args == null) {
-            args = Emptys.EMPTY_OBJECTS;
-        }
-
-        message = StringTemplates.format(message, "${", "}", new Function2<String, Object[], String>() {
-            @Override
-            public String apply(String variable, Object[] args) {
-                return getMessageInternal(basename, locale, classLoader, variable);
-            }
-        });
-
-        // https://blog.csdn.net/new03/article/details/84826958
-        // 使用 {0},{1},{2}... 来进行参数替换
-        //MessageFormat formatter = new MessageFormat(message, locale);
-        //message = formatter.format(args);
-        message = StringTemplates.formatWithIndex(message, args);
-        return message;
+        return ResourceBundles.getString(basename, locale, classLoader, key, args);
     }
 }
---
+++ langx-java/src/main/java/com/jn/langx/text/i18n/ResourceBundles.java
@@ -0,0 +1,48 @@
+package com.jn.langx.text.i18n;
+
+import com.jn.langx.annotation.NonNull;
+import com.jn.langx.text.StringTemplates;
+import com.jn.langx.util.ClassLoaders;
+import com.jn.langx.util.Emptys;
+import com.jn.langx.util.function.Function2;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class ResourceBundles {
+    private ResourceBundles() {
+    }
+
+    public static String getString(@NonNull final String basename, @NonNull String key, Object... args) {
+        return getString(basename, Locale.getDefault(), key, args);
+    }
+
+    public static String getString(@NonNull final String basename, @NonNull Locale locale, @NonNull String key, Object... args) {
+        return getString(basename, locale, ClassLoaders.getDefaultClassLoader(), key, args);
+    }
+
+    public static String getString(@NonNull final String basename, @NonNull final Locale locale, @NonNull final ClassLoader classLoader, @NonNull final String key, Object... args) {
+        ResourceBundle bundle = ResourceBundle.getBundle(basename, locale, classLoader);
+        if (bundle == null) {
+            return null;
+        }
+        String message = bundle.getString(key);
+        if (args == null) {
+            args = Emptys.EMPTY_OBJECTS;
+        }
+
+        message = StringTemplates.format(message, "${", "}", new Function2<String, Object[], String>() {
+            @Override
+            public String apply(String variable, Object[] args) {
+                return ResourceBundles.getString(basename, locale, classLoader, variable);
+            }
+        });
+
+        // https://blog.csdn.net/new03/article/details/84826958
+        // 使用 {0},{1},{2}... 来进行参数替换
+        //MessageFormat formatter = new MessageFormat(message, locale);
+        //message = formatter.format(args);
+        message = StringTemplates.formatWithIndex(message, args);
+        return message;
+    }
+}
