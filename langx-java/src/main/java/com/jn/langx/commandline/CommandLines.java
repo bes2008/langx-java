5f0a9febd15821d7ad80ed3a853c991e32377612
fjn <1194361820@qq.com>
2020-11-30 10:20:42 +0800

    * refactor code

--- langx-java/src/main/java/com/jn/langx/commandline/CommandLines.java
+++ langx-java/src/main/java/com/jn/langx/commandline/CommandLines.java
@@ -3,6 +3,7 @@ package com.jn.langx.commandline;
 import com.jn.langx.annotation.NonNull;
 import com.jn.langx.annotation.Nullable;
 import com.jn.langx.commandline.streamhandler.OutputLinesExecuteStreamHandler;
+import com.jn.langx.commandline.streamhandler.OutputAsStringExecuteStreamHandler;
 import com.jn.langx.util.Preconditions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,13 +18,35 @@ public class CommandLines {
     private CommandLines() {
     }
 
-    public static List<String> execute(@NonNull CommandLine commandLine, @Nullable File workDirectory, @Nullable Map<String, String> environment) {
+    /**
+     * 执行命令，并将输出内容以lines的形式返回
+     */
+    public static List<String> executeAndReadLines(@NonNull CommandLine commandLine, @Nullable File workDirectory, @Nullable Map<String, String> environment) {
         Preconditions.checkNotEmpty(commandLine, "the command line is null or empty");
         CommandLineExecutor executor = new DefaultCommandLineExecutor();
         if (workDirectory != null) {
             executor.setWorkingDirectory(workDirectory);
         }
         OutputLinesExecuteStreamHandler h = new OutputLinesExecuteStreamHandler();
+        executor.setStreamHandler(h);
+        try {
+            executor.execute(commandLine, environment);
+        } catch (Throwable ex) {
+            logger.error(ex.getMessage(), ex);
+        }
+        return h.getOutputContent();
+    }
+
+    /**
+     * 执行命令，并将输出内容以字符串的形式返回
+     */
+    public static String executeAndReadAsString(@NonNull CommandLine commandLine, @Nullable File workDirectory, @Nullable Map<String, String> environment) {
+        Preconditions.checkNotEmpty(commandLine, "the command line is null or empty");
+        CommandLineExecutor executor = new DefaultCommandLineExecutor();
+        if (workDirectory != null) {
+            executor.setWorkingDirectory(workDirectory);
+        }
+        OutputAsStringExecuteStreamHandler h = new OutputAsStringExecuteStreamHandler();
         executor.setStreamHandler(h);
         try {
             executor.execute(commandLine, environment);
