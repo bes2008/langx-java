acd6a7cd6d1eac21639e672016cfeea60dc3f535
fjn <1194361820@qq.com>
2020-12-17 10:29:57 +0800

    * 支持建议Cache

---
+++ langx-java/src/main/java/com/jn/langx/cache/SimpleCache.java
@@ -0,0 +1,54 @@
+package com.jn.langx.cache;
+
+import com.jn.langx.util.Preconditions;
+import com.jn.langx.util.timing.timer.Timer;
+
+import java.util.List;
+
+/**
+ * 不过期的缓存
+ * @param <K>
+ * @param <V>
+ */
+public class SimpleCache<K,V> extends AbstractCache<K,V>{
+
+    protected SimpleCache(int maxCapacity, long evictExpiredInterval) {
+        super(maxCapacity, evictExpiredInterval, null);
+    }
+
+
+    @Override
+    protected void addToCache(Entry<K, V> entry) {
+        // NOOP
+    }
+
+    @Override
+    protected void beforeRecomputeExpireTimeOnRead(Entry<K, V> entry) {
+        // NOOP
+    }
+
+    @Override
+    protected void afterRecomputeExpireTimeOnRead(Entry<K, V> entry) {
+        // NOOP
+    }
+
+    @Override
+    protected void removeFromCache(Entry<K, V> entry, RemoveCause removeCause) {
+        // NOOP
+    }
+
+    @Override
+    protected void beforeRead(Entry<K, V> entry) {
+        // NOOP
+    }
+
+    @Override
+    protected void afterRead(Entry<K, V> entry) {
+        // NOOP
+    }
+
+    @Override
+    protected List<K> forceEvict(int count) {
+        return null;
+    }
+}
