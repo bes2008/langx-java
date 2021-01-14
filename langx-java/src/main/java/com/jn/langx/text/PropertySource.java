5eb6b695cad5ea4baedd3b488a16c9ab83c56129
fjn <1194361820@qq.com>
2021-01-14 13:27:44 +0800

    * 增强 PropertySource, 支持 Accessor API

--- langx-java/src/main/java/com/jn/langx/text/PropertySource.java
+++ langx-java/src/main/java/com/jn/langx/text/PropertySource.java
@@ -3,6 +3,18 @@ package com.jn.langx.text;
 import com.jn.langx.Named;
 
 public interface PropertySource extends Named {
+
+    /**
+     * whether contains the specified property or not
+     *
+     * @param name
+     * @return
+     */
     boolean containsProperty(String name);
+
+    /**
+     * @param name the property name
+     * @return null if not contains the specified name
+     */
     String getProperty(String name);
 }
