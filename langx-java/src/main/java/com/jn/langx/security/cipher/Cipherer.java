f2681a6f576f96595e14d7ca3b961026cf06e95f
fjn <1194361820@qq.com>
2021-01-20 18:03:54 +0800

    * 添加异常说明

--- langx-java/src/main/java/com/jn/langx/security/cipher/Cipherer.java
+++ langx-java/src/main/java/com/jn/langx/security/cipher/Cipherer.java
@@ -4,6 +4,14 @@ public interface Cipherer<ORIGIN, ENCRYPTED> {
 
     ENCRYPTED encrypt(ORIGIN text);
 
+    /**
+     * 解密操作。
+     *
+     * @param encryptedText
+     * @return
+     * @throws IllegalArgumentException 当参数 encryptedText 的格式不符合时，会抛出该异常
+     * @throws com.jn.langx.security.exception.SecurityException 其他情况下抛出该异常
+     */
     ORIGIN decrypt(ENCRYPTED encryptedText);
 
 }
