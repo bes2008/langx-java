9448d6c3ce3921318349e286956c267318384d39
fjn <1194361820@qq.com>
2021-02-24 09:50:26 +0800

    * 增强CompletableFutures

---
+++ langx-java8/src/main/java/com/jn/langx/java8/concurrent/CompletableFutures.java
@@ -0,0 +1,26 @@
+package com.jn.langx.java8.concurrent;
+
+import com.jn.langx.annotation.NonNull;
+import com.jn.langx.annotation.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.function.Supplier;
+
+public class CompletableFutures {
+    public static CompletableFuture<Void> runAsync(@NonNull Runnable runnable, @Nullable Executor executor) {
+        if (executor == null) {
+            return CompletableFuture.runAsync(runnable);
+        } else {
+            return CompletableFuture.runAsync(runnable, executor);
+        }
+    }
+
+    public static <T> CompletableFuture<T> supplyAsync(@NonNull Supplier<T> runnable, @Nullable Executor executor) {
+        if (executor == null) {
+            return CompletableFuture.<T>supplyAsync(runnable);
+        } else {
+            return CompletableFuture.<T>supplyAsync(runnable, executor);
+        }
+    }
+}
