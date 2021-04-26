e84b63f6cc4f83f7691d167cd6dd19bbaf9b1485
fjn <1194361820@qq.com>
2021-04-26 20:04:39 +0800

    * 支持排序

--- langx-java/src/main/java/com/jn/langx/util/collection/graph/Graphs.java
+++ langx-java/src/main/java/com/jn/langx/util/collection/graph/Graphs.java
@@ -1,12 +1,11 @@
 package com.jn.langx.util.collection.graph;
 
 import com.jn.langx.util.collection.Collects;
+import com.jn.langx.util.collection.graph.traverser.DeepFirstGraphTraverser;
+import com.jn.langx.util.function.Consumer;
 import com.jn.langx.util.function.Supplier;
 
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Graphs {
 
@@ -114,4 +113,29 @@ public class Graphs {
         return false;
     }
 
+    public static <T> List<Vertex<T>> dfsSort(final Graph<T> graph) {
+        final List<Vertex<T>> ret = new ArrayList<Vertex<T>>();
+
+        Collects.forEach(graph.getVertices(), new Consumer<Vertex<T>>() {
+            @Override
+            public void accept(Vertex<T> vertex) {
+                ret.addAll(dfsSort(graph, vertex.getName()));
+            }
+        });
+        return ret;
+    }
+
+    public static <T> List<Vertex<T>> dfsSort(Graph<T> graph, final String vertexName) {
+        // we need to use addFirst method so we will use LinkedList explicitly
+        final List<Vertex<T>> retValue = new LinkedList<Vertex<T>>();
+        VertexConsumer<T> consumer = new VertexConsumer<T>() {
+            @Override
+            public void accept(Graph<T> graph, Vertex<T> vertex, Edge<T> edge) {
+                retValue.add(vertex);
+            }
+        };
+        DeepFirstGraphTraverser<T> dfs = new DeepFirstGraphTraverser<T>();
+        dfs.traverse(graph, vertexName, consumer);
+        return retValue;
+    }
 }
