a79f9ee52c77293fd9fa0c73128fbb3423c12de2
fjn <1194361820@qq.com>
2021-07-20 20:02:13 +0800

    $ 更新测试用例

--- langx-java/src/main/java/com/jn/langx/security/crypto/digest/provider/md4/MD4DigestProviderConfigurer.java
+++ langx-java/src/main/java/com/jn/langx/security/crypto/digest/provider/md4/MD4DigestProviderConfigurer.java
@@ -3,12 +3,11 @@ package com.jn.langx.security.crypto.digest.provider.md4;
 import com.jn.langx.security.crypto.provider.LangxSecurityProvider;
 import com.jn.langx.security.crypto.provider.SecurityProviderConfigurer;
 import com.jn.langx.util.reflect.Reflects;
-import org.bouncycastle.asn1.pkcs.PKCSObjectIdentifiers;
 
 public class MD4DigestProviderConfigurer implements SecurityProviderConfigurer {
     @Override
     public void configure(LangxSecurityProvider provider) {
         provider.addAlgorithm("MessageDigest.MD4", Reflects.getFQNClassName(MD4MessageDigest.class));
-        provider.addAlgorithm("Alg.Alias.MessageDigest." + PKCSObjectIdentifiers.md4, "MD4");
+        provider.addAlgorithm("Alg.Alias.MessageDigest", "1.2.840.113549.2.4", "MD4");
     }
 }
--- langx-java/src/test/java/com/jn/langx/test/security/messagedigest/MessageDigestsTests.java
+++ langx-java/src/test/java/com/jn/langx/test/security/messagedigest/MessageDigestsTests.java
@@ -1,6 +1,7 @@
 package com.jn.langx.test.security.messagedigest;
 
 import com.jn.langx.security.crypto.digest.MessageDigests;
+import com.jn.langx.text.StringTemplates;
 import org.junit.Test;
 
 
@@ -8,12 +9,25 @@ public class MessageDigestsTests {
     @Test
     public void test(){
         String str = MessageDigestsTests.class.getName();
-        System.out.println(MessageDigests.getDigestHexString("MD2", str));
-        System.out.println(MessageDigests.getDigestHexString("MD4", str));
-        System.out.println(MessageDigests.getDigestHexString("MD5", str));
-        System.out.println(MessageDigests.getDigestHexString("SM3", str));
-        System.out.println(MessageDigests.getDigestHexString("SHA1", str));
-        System.out.println(MessageDigests.getDigestHexString("SHA-1", str));
-        System.out.println(MessageDigests.getDigestHexString("SHA3", str));
+        digestAndPrint("MD2", str);
+        digestAndPrint("MD4", str);
+        digestAndPrint("MD5", str);
+        digestAndPrint("SM3", str);
+        digestAndPrint("SHA1", str);
+        digestAndPrint("SHA-1", str);
+        digestAndPrint("SHA3", str);
+        digestAndPrint("SHA-3", str);
+        digestAndPrint("SHA3-224", str);
+        digestAndPrint("SHA3-256", str);
+        digestAndPrint("SHA3-384", str);
+        digestAndPrint("SHA3-512", str);
+        digestAndPrint("SHA-224", str);
+        digestAndPrint("SHA-256", str);
+        digestAndPrint("SHA-384", str);
+        digestAndPrint("SHA-512", str);
+    }
+
+    private void digestAndPrint(String algorithm, String content){
+        System.out.println(StringTemplates.formatWithPlaceholder("{}\t\t: {}",algorithm, MessageDigests.getDigestHexString(algorithm, content)));
     }
 }
