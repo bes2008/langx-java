29d0d19abe1b607216fcc5aa59ad28d1b50b745d
fjn <1194361820@qq.com>
2021-07-24 11:50:05 +0800

    * 代码整理

--- langx-java-security-gm/src/main/java/com/jn/langx/security/gm/bc/crypto/asymmetric/sm2/SM2AlgorithmParametersSpi.java
+++ langx-java-security-gm/src/main/java/com/jn/langx/security/gm/bc/crypto/asymmetric/sm2/SM2AlgorithmParametersSpi.java
@@ -2,5 +2,5 @@ package com.jn.langx.security.gm.bc.crypto.asymmetric.sm2;
 
 import org.bouncycastle.jcajce.provider.asymmetric.ec.AlgorithmParametersSpi;
 
-public class SM2AlgorithmParameterSpi extends AlgorithmParametersSpi {
+public class SM2AlgorithmParametersSpi extends AlgorithmParametersSpi {
 }
