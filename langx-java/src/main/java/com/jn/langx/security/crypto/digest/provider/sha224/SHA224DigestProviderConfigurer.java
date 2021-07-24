6496a09ff4c40469f50420bda0090f7d11fbebb4
fjn <1194361820@qq.com>
2021-07-24 10:17:30 +0800

    * 支持SHA224

---
+++ langx-java/src/main/java/com/jn/langx/security/crypto/digest/provider/sha224/SHA224DigestProviderConfigurer.java
@@ -0,0 +1,14 @@
+package com.jn.langx.security.crypto.digest.provider.sha224;
+
+import com.jn.langx.security.crypto.provider.LangxSecurityProvider;
+import com.jn.langx.security.crypto.provider.SecurityProviderConfigurer;
+import com.jn.langx.util.reflect.Reflects;
+
+public class SHA224DigestProviderConfigurer implements SecurityProviderConfigurer {
+    @Override
+    public void configure(LangxSecurityProvider provider) {
+        provider.addAlgorithm("MessageDigest.SHA-224", Reflects.getFQNClassName(SHA224MessageDigest.class));
+        provider.addAlgorithm("Alg.Alias.MessageDigest.SHA224", "SHA-224");
+        provider.addAlgorithm("Alg.Alias.MessageDigest.2.16.840.1.101.3.4.2.4", "SHA-224");
+    }
+}
