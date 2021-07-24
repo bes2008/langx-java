a9e29e98e77887a4558218596161cc433bcb6ed6
fjn <1194361820@qq.com>
2021-07-24 15:21:55 +0800

    * 整理测试用例

--- langx-java-security-gm/src/test/java/com/jn/langx/security/gm/tests/SM2Tests.java
+++ langx-java-security-gm/src/test/java/com/jn/langx/security/gm/tests/SM2Tests.java
@@ -1,19 +1,15 @@
 package com.jn.langx.security.gm.tests;
 
-import com.jn.langx.security.crypto.cipher.AlgorithmParameterSupplier;
 import com.jn.langx.security.crypto.cipher.Asymmetrics;
 import com.jn.langx.security.crypto.key.PKIs;
 import com.jn.langx.security.crypto.key.supplier.bytesbased.BytesBasedPrivateKeySupplier;
 import com.jn.langx.security.crypto.key.supplier.bytesbased.BytesBasedPublicKeySupplier;
 import com.jn.langx.security.crypto.signature.Signatures;
-import com.jn.langx.security.gm.bc.crypto.asymmetric.sm2.SM2AlgorithmParametersSpi;
 import com.jn.langx.security.gm.bc.crypto.asymmetric.sm2.SM2ParameterSpec;
 import com.jn.langx.util.io.Charsets;
 import org.junit.Test;
 
-import java.security.Key;
 import java.security.KeyPair;
-import java.security.Provider;
 import java.security.SecureRandom;
 
 public class SM2Tests {
@@ -34,7 +30,7 @@ public class SM2Tests {
         KeyPair keyPair = PKIs.createKeyPair("SM2", null, 256, null);
         SM2ParameterSpec parameterSpec = new SM2ParameterSpec();
         byte[] digitSignature = Signatures.sign(string.getBytes(Charsets.UTF_8), keyPair.getPrivate().getEncoded(), "SM3WithSM2", null, null, parameterSpec);
-        boolean verified = Signatures.verify(string.getBytes(Charsets.UTF_8),  digitSignature, keyPair.getPublic().getEncoded(),"SM3WithSM2", null,parameterSpec);
+        boolean verified = Signatures.verify(string.getBytes(Charsets.UTF_8), digitSignature, keyPair.getPublic().getEncoded(), "SM3WithSM2", null, parameterSpec);
         System.out.println("verified: " + verified);
     }
 }
