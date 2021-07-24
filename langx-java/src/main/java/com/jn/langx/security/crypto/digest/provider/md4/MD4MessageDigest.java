f3cb9fe6736c98891bb032144b734a8907bcc3a4
fjn <1194361820@qq.com>
2021-07-24 10:16:37 +0800

    * 简化构造器

--- langx-java/src/main/java/com/jn/langx/security/crypto/digest/provider/md4/MD4MessageDigest.java
+++ langx-java/src/main/java/com/jn/langx/security/crypto/digest/provider/md4/MD4MessageDigest.java
@@ -1,19 +1,10 @@
 package com.jn.langx.security.crypto.digest.provider.md4;
 
 import com.jn.langx.security.crypto.digest.LangxMessageDigest;
-import com.jn.langx.security.crypto.digest.internal.Digest;
-import com.jn.langx.security.crypto.digest.internal.Xof;
 import com.jn.langx.security.crypto.digest.internal.impl._MD4Digest;
 
 public class MD4MessageDigest extends LangxMessageDigest {
     public MD4MessageDigest(){
-        this(new _MD4Digest());
-    }
-    public MD4MessageDigest(Digest digest) {
-        super(digest);
-    }
-
-    public MD4MessageDigest(Xof digest, int outputSize) {
-        super(digest, outputSize);
+        super(new _MD4Digest());
     }
 }
--- langx-java/src/main/java/com/jn/langx/security/crypto/digest/provider/sm3/SM3MessageDigest.java
+++ langx-java/src/main/java/com/jn/langx/security/crypto/digest/provider/sm3/SM3MessageDigest.java
@@ -1,19 +1,10 @@
 package com.jn.langx.security.crypto.digest.provider.sm3;
 
 import com.jn.langx.security.crypto.digest.LangxMessageDigest;
-import com.jn.langx.security.crypto.digest.internal.Xof;
 import com.jn.langx.security.crypto.digest.internal.impl._SM3Digest;
 
 public class SM3MessageDigest extends LangxMessageDigest {
     public SM3MessageDigest(){
-        this(new _SM3Digest());
-    }
-
-    public SM3MessageDigest(_SM3Digest digest) {
-        super(digest);
-    }
-
-    public SM3MessageDigest(Xof digest, int outputSize) {
-        super(digest, outputSize);
+        super(new _SM3Digest());
     }
 }
