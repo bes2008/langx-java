3ff0431d3470366cdcaddbeef2d1ca955e9b8b1d
fjn <1194361820@qq.com>
2021-07-26 12:45:56 +0800

    * 重命名

--- langx-java/src/main/java/com/jn/langx/security/crypto/digest/spi/md4/MD4MessageDigestSpi.java
+++ langx-java/src/main/java/com/jn/langx/security/crypto/digest/spi/md4/MD4MessageDigestSpi.java
@@ -3,8 +3,8 @@ package com.jn.langx.security.crypto.digest.spi.md4;
 import com.jn.langx.security.crypto.digest.LangxMessageDigestSpi;
 import com.jn.langx.security.crypto.digest.internal.impl._MD4Digest;
 
-public class MD4MessageDigest extends LangxMessageDigestSpi {
-    public MD4MessageDigest(){
+public class MD4MessageDigestSpi extends LangxMessageDigestSpi {
+    public MD4MessageDigestSpi(){
         super(new _MD4Digest());
     }
 }
