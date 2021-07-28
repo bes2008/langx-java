b47da1c7bdc4c524a5390cdd91c9f7659d7f883e
fjn <1194361820@qq.com>
2021-07-28 11:56:48 +0800

    * 添加注释

--- langx-java/src/main/java/com/jn/langx/util/struct/counter/Counter.java
+++ langx-java/src/main/java/com/jn/langx/util/struct/counter/Counter.java
@@ -1,16 +1,51 @@
 package com.jn.langx.util.struct.counter;
 
 public interface Counter<E extends Number> {
+    /**
+     * +1
+     * @return 返回增加后的值
+     */
     E increment();
 
+    /**
+     * +n
+     * @return 返回增加后的值
+     */
     E increment(E delta);
 
+
+    /**
+     * -1
+     * @return 返回减少后的值
+     */
     E decrement();
 
+    /**
+     * -n
+     * @return 返回减少后的值
+     */
     E decrement(E delta);
+
+    /**
+     * +1
+     * @return 返回增加之前的值
+     */
     E getAndIncrement();
+
+    /**
+     * +n
+     * @return 返回增加之前的值
+     */
     E getAndIncrement(E delta);
+
+    /**
+     * @return 返回当前值
+     */
     E get();
 
+    /**
+     * 设置值
+     * @param value
+     */
     void set(E value);
 }
