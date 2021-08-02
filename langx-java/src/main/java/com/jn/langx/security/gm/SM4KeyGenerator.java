e1cd91a94ed060c12114f07599b4e0befc46b985
fjn <1194361820@qq.com>
2021-08-02 20:45:59 +0800

    * 提供SM4 KeyGenerator

---
+++ langx-java/src/main/java/com/jn/langx/security/gm/SM4KeyGenerator.java
@@ -0,0 +1,24 @@
+package com.jn.langx.security.gm;
+
+import com.jn.langx.security.crypto.key.SecureRandoms;
+import com.jn.langx.security.crypto.key.spi.CipherKeyGeneratorEngine;
+
+import java.security.SecureRandom;
+
+public class SM4KeyGenerator {
+    private CipherKeyGeneratorEngine engine;
+
+    public SM4KeyGenerator() {
+        this(SecureRandoms.getSHA1PRNG());
+    }
+
+    public SM4KeyGenerator(SecureRandom random) {
+        this.engine = new CipherKeyGeneratorEngine();
+        this.engine.init(128, random);
+    }
+
+    public byte[] genSecretKey() {
+        return this.engine.generateKey();
+    }
+
+}
