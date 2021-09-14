9f137cde6e4c86fc8984d27ee75415d8ae192093
fjn <fs1194361820@163.com>
2021-09-14 15:01:53 +0800

    * 提供线程池属性

---
+++ langx-java/src/main/java/com/jn/langx/util/concurrent/ThreadPoolProperties.java
@@ -0,0 +1,43 @@
+package com.jn.langx.util.concurrent;
+
+import java.util.concurrent.TimeUnit;
+
+public class ThreadPoolProperties {
+
+    private int coreSize;
+    private int maxSize;
+    private long keepAliveTime;
+    private TimeUnit unit;
+
+    public int getCoreSize() {
+        return coreSize;
+    }
+
+    public void setCoreSize(int coreSize) {
+        this.coreSize = coreSize;
+    }
+
+    public int getMaxSize() {
+        return maxSize;
+    }
+
+    public void setMaxSize(int maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    public long getKeepAliveTime() {
+        return keepAliveTime;
+    }
+
+    public void setKeepAliveTime(long keepAliveTime) {
+        this.keepAliveTime = keepAliveTime;
+    }
+
+    public TimeUnit getUnit() {
+        return unit;
+    }
+
+    public void setUnit(TimeUnit unit) {
+        this.unit = unit;
+    }
+}
