7d91a91887ca8d9f5717cc5740b752ec5c198290
fjn <fjn@163.com>
2021-12-15 10:26:43 +0800

    * 代码整理

--- langx-java-security-gm/src/main/java/com/jn/langx/security/gm/crypto/bc/crypto/asymmetric/sm2/IDGetter.java
+++
@@ -1,5 +0,0 @@
-package com.jn.langx.security.gm.crypto.bc.crypto.asymmetric.sm2;
-
-public interface IDGetter {
-    byte[] getID();
-}
---
+++ langx-java-security/langx-java-security-gm-jca-bouncycastle/src/main/java/com/jn/langx/security/gm/crypto/bc/asymmetric/sm2/IDGetter.java
@@ -0,0 +1,5 @@
+package com.jn.langx.security.gm.crypto.bc.asymmetric.sm2;
+
+public interface IDGetter {
+    byte[] getID();
+}
