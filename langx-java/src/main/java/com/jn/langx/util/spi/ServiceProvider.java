ed810736d0920b9fc86cb57b3ced97e40a547840
fjn <1194361820@qq.com>
2021-12-30 15:59:03 +0800

    * 支持OnClasses注解

---
+++ langx-java/src/main/java/com/jn/langx/util/spi/ServiceProvider.java
@@ -0,0 +1,10 @@
+package com.jn.langx.util.spi;
+
+import com.jn.langx.Provider;
+
+import java.util.Iterator;
+
+public interface ServiceProvider<T> extends Provider<Class<T>, Iterator<T>> {
+    @Override
+    public Iterator<T> get(Class<T> serviceClass);
+}
