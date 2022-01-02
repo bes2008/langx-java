3e1e5261bea12268d4e25e900dc3c50231587be7
fjn <1194361820@qq.com>
2022-01-02 16:45:08 +0800

    * 增强构造器

--- langx-java/src/main/java/com/jn/langx/security/crypto/IllegalKeyException.java
+++ langx-java/src/main/java/com/jn/langx/security/crypto/IllegalKeyException.java
@@ -1,8 +1,14 @@
 package com.jn.langx.security.crypto;
 
+import com.jn.langx.text.StringTemplates;
+
 public class IllegalKeyException extends SecurityException {
     public IllegalKeyException() {
         super();
+    }
+
+    public IllegalKeyException(String algorithm, String key) {
+        this(StringTemplates.formatWithPlaceholder("illegal {} key {}", algorithm, key));
     }
 
     public IllegalKeyException(String s) {
