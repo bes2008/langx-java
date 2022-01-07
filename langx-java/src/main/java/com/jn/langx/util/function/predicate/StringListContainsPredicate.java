d67b03f20bb5ef340ac34a1a43b437048a52b864
fjn <fjn@163.com>
2022-01-07 23:13:53 +0800

    * 增强contains

--- langx-java/src/main/java/com/jn/langx/util/function/predicate/StringListContainsPredicate.java
+++ langx-java/src/main/java/com/jn/langx/util/function/predicate/StringListContainsPredicate.java
@@ -1,19 +1,31 @@
 package com.jn.langx.util.function.predicate;
 
+import com.jn.langx.util.Strings;
 import com.jn.langx.util.collection.Collects;
 
 import java.util.Collection;
 import java.util.Comparator;
 
-public class StringListContainsPredicate extends ContainsPredicate<String>{
-    public StringListContainsPredicate(String... array){
-        this(Collects.asList(array));
-    }
-    public StringListContainsPredicate(Collection<String> collection) {
-        super(collection);
+public class StringListContainsPredicate extends ContainsPredicate<String> {
+    public StringListContainsPredicate(String... array) {
+        this(true, array);
     }
 
-    public StringListContainsPredicate(Collection<String> collection, Comparator<String> comparator) {
-        super(collection, comparator);
+    public StringListContainsPredicate(boolean ignoreCase, String... array) {
+        this(ignoreCase, Collects.asList(array));
     }
+
+    public StringListContainsPredicate(Collection<String> collection) {
+        this(true, collection);
+    }
+
+    public StringListContainsPredicate(final boolean ignoreCase, Collection<String> collection) {
+        super(collection, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return Strings.equals(o1, o2, ignoreCase) ? o1.compareTo(o2) : 0;
+            }
+        });
+    }
+
 }
