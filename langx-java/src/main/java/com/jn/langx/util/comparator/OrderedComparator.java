7906daadf78924f96ac4eb1ee6eec2dccecea9a4
fjn <fjn@163.com>
2022-02-12 12:05:01 +0800

    * 完善 ordered

--- langx-java/src/main/java/com/jn/langx/util/comparator/OrderedComparator.java
+++ langx-java/src/main/java/com/jn/langx/util/comparator/OrderedComparator.java
@@ -10,9 +10,12 @@ import java.util.Comparator;
 public class OrderedComparator<T> implements Comparator<T> {
     @Override
     public int compare(T o1, T o2) {
+        if(o1==o2){
+            return 0;
+        }
         int order1 = Orders.getOrder(o1);
         int order2 = Orders.getOrder(o2);
-        // 优先级 高 -> 低，对应的值是：从大到小， 也就是需要的是降序排序
-        return order2 - order1;
+        // 优先级 高 -> 低，对应的值是：从小到大， 也就是需要的是升序排序
+        return order1 - order2;
     }
 }
