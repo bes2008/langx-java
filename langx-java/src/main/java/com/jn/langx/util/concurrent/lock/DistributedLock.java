580fe6380a9e49ee9e86995c99f82b4d0f856316
fjn <fjn@163.com>
2022-02-26 16:21:31 +0800

    * 整理import

--- langx-java/src/main/java/com/jn/langx/util/concurrent/lock/DistributedLock.java
+++ langx-java/src/main/java/com/jn/langx/util/concurrent/lock/DistributedLock.java
@@ -2,7 +2,5 @@ package com.jn.langx.util.concurrent.lock;
 
 import com.jn.langx.distributed.locks.AbstractDLock;
 
-import java.util.concurrent.locks.Condition;
-
 public abstract class DistributedLock extends AbstractDLock {
 }
