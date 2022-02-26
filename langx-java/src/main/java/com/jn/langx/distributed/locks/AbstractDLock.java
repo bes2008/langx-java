fe7e01da7a18fff04a4196dfefe80f2c13c61879
fjn <fjn@163.com>
2022-02-26 16:20:53 +0800

    * 完善分布式lock接口

---
+++ langx-java/src/main/java/com/jn/langx/distributed/locks/AbstractDLock.java
@@ -0,0 +1,47 @@
+package com.jn.langx.distributed.locks;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+
+public abstract class AbstractDLock implements DLock {
+    @Override
+    public void lock() {
+        tryLock(-1, TimeUnit.MILLISECONDS, -1, TimeUnit.MILLISECONDS);
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+        lock(-1, TimeUnit.MILLISECONDS, true);
+    }
+
+    @Override
+    public void lock(long ttl, TimeUnit ttlUnit, boolean interruptibly) throws InterruptedException {
+        tryLock(-1, TimeUnit.MILLISECONDS, ttl, ttlUnit);
+    }
+
+    @Override
+    public boolean tryLock(long tryTime, TimeUnit tryTimeUnit, long ttl, TimeUnit ttlUnit) {
+        return false;
+    }
+
+    @Override
+    public boolean tryLock(long tryTime, TimeUnit tryTimeUnit) {
+        return tryLock(tryTime, tryTimeUnit, -1, TimeUnit.MILLISECONDS);
+    }
+
+    @Override
+    public boolean tryLock() {
+        return tryLock(-1, TimeUnit.MILLISECONDS);
+    }
+
+    @Override
+    public void unlock() {
+        forceUnlock();
+    }
+
+    @Override
+    public Condition newCondition() {
+        throw new UnsupportedOperationException("newCondition is not unsupported for distributed lock");
+    }
+
+}
--- langx-java/src/main/java/com/jn/langx/util/concurrent/lock/DistributedLock.java
+++ langx-java/src/main/java/com/jn/langx/util/concurrent/lock/DistributedLock.java
@@ -1,12 +1,8 @@
 package com.jn.langx.util.concurrent.lock;
 
-import com.jn.langx.distributed.locks.DLock;
+import com.jn.langx.distributed.locks.AbstractDLock;
 
 import java.util.concurrent.locks.Condition;
 
-public abstract class DistributedLock implements DLock {
-    @Override
-    public Condition newCondition() {
-        throw new UnsupportedOperationException("newCondition is not unsupported for distributed lock");
-    }
+public abstract class DistributedLock extends AbstractDLock {
 }
