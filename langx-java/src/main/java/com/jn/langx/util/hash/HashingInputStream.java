4ea5181f86baeb688fdc00dc3942cd237e422e69
fjn <1194361820@qq.com>
2022-03-18 21:45:45 +0800

    + 提供基于Hash的Stream

---
+++ langx-java/src/main/java/com/jn/langx/util/hash/HashingInputStream.java
@@ -0,0 +1,85 @@
+package com.jn.langx.util.hash;
+
+
+import com.jn.langx.util.Preconditions;
+
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+
+public final class HashingInputStream extends FilterInputStream {
+    private final Hasher hasher;
+
+    /**
+     * Creates an input stream that hashes using the given {@link Hasher} and delegates all data
+     * read from it to the underlying {@link InputStream}.
+     *
+     * <p>The {@link InputStream} should not be read from before or after the hand-off.
+     */
+    public HashingInputStream(Hasher hasher, InputStream in) {
+        super(Preconditions.checkNotNull(in));
+        this.hasher = Preconditions.checkNotNull(hasher);
+    }
+
+    /**
+     * Reads the next byte of data from the underlying input stream and updates the hasher with the
+     * byte read.
+     */
+    @Override
+    public int read() throws IOException {
+        int b = in.read();
+        if (b != -1) {
+            byte[] bytes = new byte[]{(byte) b};
+            hasher.update(bytes,0,1);
+        }
+        return b;
+    }
+
+    /**
+     * Reads the specified bytes of data from the underlying input stream and updates the hasher with
+     * the bytes read.
+     */
+    @Override
+    public int read(byte[] bytes, int off, int len) throws IOException {
+        int numOfBytesRead = in.read(bytes, off, len);
+        if (numOfBytesRead != -1) {
+            hasher.update(bytes, off, numOfBytesRead);
+        }
+        return numOfBytesRead;
+    }
+
+    /**
+     * mark() is not supported for HashingInputStream
+     *
+     * @return {@code false} always
+     */
+    @Override
+    public boolean markSupported() {
+        return false;
+    }
+
+    /**
+     * mark() is not supported for HashingInputStream
+     */
+    @Override
+    public void mark(int readlimit) {}
+
+    /**
+     * reset() is not supported for HashingInputStream.
+     *
+     * @throws IOException this operation is not supported
+     */
+    @Override
+    public void reset() throws IOException {
+        throw new IOException("reset not supported");
+    }
+
+    /**
+     * Returns the {@literal hash code} based on the data read from this stream. The result is unspecified
+     * if this method is called more than once on the same instance.
+     */
+    public long getHash() {
+        return hasher.get();
+    }
+}
---
+++ langx-java/src/main/java/com/jn/langx/util/hash/HashingOutputStream.java
@@ -0,0 +1,62 @@
+package com.jn.langx.util.hash;
+
+
+import com.jn.langx.util.Preconditions;
+
+import java.io.FilterOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+
+/**
+ * An {@link OutputStream} that maintains a hash of the data written to it.
+ */
+public final class HashingOutputStream extends FilterOutputStream {
+    private final Hasher hasher;
+
+    /**
+     * Creates an output stream that hashes using the given {@link Hasher}, and forwards all
+     * data written to it to the underlying {@link OutputStream}.
+     *
+     * <p>The {@link OutputStream} should not be written to before or after the hand-off.
+     */
+    public HashingOutputStream(Hasher hasher,OutputStream out) {
+        this(hasher,null,out);
+    }
+    public HashingOutputStream(Hasher hasher, Integer seed, OutputStream out) {
+        super(Preconditions.checkNotNull(out));
+        this.hasher =Preconditions. checkNotNull(hasher);
+        if (seed == null) {
+            seed = 0;
+        }
+        this.hasher.setSeed(seed);
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        byte[] bytes = new byte[]{(byte) b};
+        write(bytes, 0, 1);
+    }
+
+    @Override
+    public void write(byte[] bytes, int off, int len) throws IOException {
+        out.write(bytes, off, len);
+        hasher.update(bytes, off, len);
+    }
+
+    /**
+     * Returns the {@literal hash code} based on the data written to this stream. The result is
+     * unspecified if this method is called more than once on the same instance.
+     */
+    public long getHash() {
+        return hasher.get();
+    }
+
+    // Overriding close() because FilterOutputStream's close() method pre-JDK8 has bad behavior:
+    // it silently ignores any exception thrown by flush(). Instead, just close the delegate stream.
+    // It should flush itself if necessary.
+    @Override
+    public void close() throws IOException {
+        out.close();
+    }
+}
