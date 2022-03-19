bb2c6d7eab96d2f657a66b53861a995b8c535b9a
fjn <fjn@163.com>
2022-03-19 11:18:59 +0800

    * 整理测试用例

--- langx-java/src/test/java/com/jn/langx/test/util/hash/Murmur3_32Tests.java
+++ langx-java/src/test/java/com/jn/langx/test/util/hash/Murmur3_32Tests.java
@@ -1,7 +1,5 @@
 package com.jn.langx.test.util.hash;
 
-import com.google.common.hash.HashFunction;
-import com.google.common.hash.Hashing;
 import com.jn.langx.util.hash.Hasher;
 import com.jn.langx.util.hash.Hashs;
 import com.jn.langx.util.hash.Murmur3_32Hasher;
@@ -14,9 +12,7 @@ public class Murmur3_32Tests {
     public void test() {
         byte[] bytes = "helloworld".getBytes(Charsets.UTF_8);
         long hash1 = Hashs.hash( new Murmur3_32Hasher(),bytes, bytes.length, -1);
-        long hash2 = Hashing.murmur3_32(-1).hashBytes(bytes, 0, bytes.length).asInt();
         System.out.println(hash1);
-        System.out.println(hash2);
     }
 
     @Test
@@ -27,29 +23,11 @@ public class Murmur3_32Tests {
 
     }
 
-    @Test
-    public void test2() {
-        String text = "hello,langx hasher";
-        HashFunction hashFunction2 = Hashing.murmur3_32(-1);
-        //   hashFunction2.hashBytes(bytes1, 0, bytes1.length);
-        com.google.common.hash.Hasher hasher2 = hashFunction2.newHasher();
-        testWithGuava(text, hasher2);
-
-    }
-
     private void testWithLangx(String text, Hasher hasher) {
         hasher.setSeed(-1);
         byte[] bytes2 = text.getBytes(Charsets.UTF_8);
         hasher.update(bytes2, 0, bytes2.length);
         long hash = hasher.getHash();
         System.out.println(hash);
-    }
-
-
-    private void testWithGuava(String text, com.google.common.hash.Hasher hasher) {
-        byte[] bytes2 = text.getBytes(Charsets.UTF_8);
-        hasher.putBytes(bytes2, 0, bytes2.length);
-        long hash2 = hasher.hash().asInt();
-        System.out.println(hash2);
     }
 }
