71c3d49736305ea1b48f3ada8169034f30776c31
fjn <fjn@163.com>
2022-03-28 10:40:02 +0800

    + 提供更多的consumer接口

---
+++ langx-java/src/main/java/com/jn/langx/util/function/Consumer3.java
@@ -0,0 +1,8 @@
+package com.jn.langx.util.function;
+
+/**
+ * @since 4.4.3
+ */
+public interface Consumer3<ARG1, ARG2, ARG3> {
+    void accept(ARG1 arg1, ARG2 arg2, ARG3 arg3);
+}
---
+++ langx-java/src/main/java/com/jn/langx/util/function/Consumer4.java
@@ -0,0 +1,5 @@
+package com.jn.langx.util.function;
+
+public interface Consumer4 <ARG1, ARG2, ARG3, ARG4> {
+    void accept(ARG1 arg1, ARG2 arg2, ARG3 arg3, ARG4 arg4);
+}
