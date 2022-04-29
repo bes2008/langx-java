63f3e2640219896f2ac85247e248bbaeaf4fad3b
fjn <fjn@163.com>
2022-04-29 16:19:57 +0800

    * 代码优化

--- langx-java/src/main/java/com/jn/langx/asn1/spec/ASN1IA5String.java
+++ langx-java/src/main/java/com/jn/langx/asn1/spec/ASN1IA5String.java
@@ -1,44 +1,9 @@
-/*
- * Copyright 2017-2022 Ping Identity Corporation
- * All Rights Reserved.
- */
-/*
- * Copyright 2017-2022 Ping Identity Corporation
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-/*
- * Copyright (C) 2017-2022 Ping Identity Corporation
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License (GPLv2 only)
- * or the terms of the GNU Lesser General Public License (LGPLv2.1 only)
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, see <http://www.gnu.org/licenses>.
- */
 package com.jn.langx.asn1.spec;
 
 
 import com.jn.langx.annotation.NonNull;
 import com.jn.langx.annotation.Nullable;
-import com.unboundid.util.StaticUtils;
+import com.jn.langx.util.io.bytes.Utf8s;
 
 import static com.jn.langx.asn1.spec.ASN1Messages.*;
 
@@ -92,7 +57,7 @@ public final class ASN1IA5String
      */
     public ASN1IA5String(final byte type, @Nullable final String stringValue)
             throws ASN1Exception {
-        this(type, stringValue, StaticUtils.getBytes(stringValue));
+        this(type, stringValue, Utf8s.getBytes(stringValue));
     }
 
 
@@ -174,7 +139,7 @@ public final class ASN1IA5String
             System.arraycopy(elementBytes, valueStartPos, elementValue, 0, length);
 
             return new ASN1IA5String(elementBytes[0],
-                    StaticUtils.toUTF8String(elementValue), elementValue);
+                    Utf8s.toUTF8String(elementValue), elementValue);
         } catch (final ASN1Exception ae) {
             throw ae;
         } catch (final Exception e) {
@@ -196,8 +161,7 @@ public final class ASN1IA5String
             @NonNull final ASN1Element element)
             throws ASN1Exception {
         final byte[] elementValue = element.getValue();
-        return new ASN1IA5String(element.getType(),
-                StaticUtils.toUTF8String(elementValue), elementValue);
+        return new ASN1IA5String(element.getType(), Utf8s.toUTF8String(elementValue), elementValue);
     }
 
 
