431c723ab92152683e3acf53124bfae169c9d6c8
fjn <fjn@163.com>
2022-05-21 15:04:29 +0800

    * 完善filter chain

--- langx-java/src/main/java/com/jn/langx/chain/FilterChain.java
+++ langx-java/src/main/java/com/jn/langx/chain/FilterChain.java
@@ -3,24 +3,56 @@ package com.jn.langx.chain;
 import com.jn.langx.annotation.NonNull;
 import com.jn.langx.util.Preconditions;
 import com.jn.langx.util.collection.Collects;
+import com.jn.langx.util.struct.counter.IntegerCounter;
+import com.jn.langx.util.struct.counter.SimpleIntegerCounter;
+import com.jn.langx.util.struct.counter.ThreadLocalIntegerCounter;
 
 import java.util.List;
 
 /**
- * 类似于 Java EE  Servlet 规范中的 FilterChain。 当然了，这个只是简易版本的，每次需要Chain的时候，需要重建。
+ * 类似于 Java EE  Servlet 规范中的 FilterChain。
+ * 并且需要在 handler 的 handle方法内部 调用 chain.handle(req, resp)
  *
  * @param <REQ>
  * @param <RESP>
  */
 public class FilterChain<REQ, RESP> extends AbstractChain<REQ, RESP> {
     private List<Handler<REQ, RESP>> handlers = Collects.emptyArrayList();
-    int pos = 0;
+    private IntegerCounter posHolder;
+
+    public FilterChain() {
+        this(false);
+    }
+
+    public FilterChain(IntegerCounter posHolder) {
+        this.posHolder = posHolder;
+    }
+
+    /**
+     * 如果 是独占 chain，则 每一次使用chain时，需要新建一个独有的chain
+     * @param shareChain
+     */
+    public FilterChain(boolean shareChain) {
+        if(shareChain){
+            this.posHolder = new ThreadLocalIntegerCounter();
+        }else{
+            this.posHolder = new SimpleIntegerCounter();
+        }
+    }
+
 
     @Override
     public void handle(final REQ request, final RESP response) {
-        if (pos < handlers.size() && pos >= 0) {
-            Handler<REQ, RESP> handler = handlers.get(this.pos++);
+        int pos = posHolder.get();
+        if (pos < handlers.size() - 1 && pos >= -1) {
+            posHolder.increment();
+            Handler<REQ, RESP> handler = handlers.get(pos);
             handler.handle(request, response, this);
+        }
+
+        if (pos >= this.handlers.size() - 1) {
+            // 执行完毕后重置
+            posHolder.set(-1);
         }
     }
 
