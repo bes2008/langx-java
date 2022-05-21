210ffc773e702db58a68944a8fcc3561a62978a2
fjn <fjn@163.com>
2022-05-21 17:30:05 +0800

    * 更新代码

---
+++ langx-java/src/main/java/com/jn/langx/chain/PipelineChain.java
@@ -0,0 +1,35 @@
+package com.jn.langx.chain;
+
+import com.jn.langx.annotation.NonNull;
+import com.jn.langx.util.Preconditions;
+import com.jn.langx.util.collection.Collects;
+import com.jn.langx.util.function.Consumer;
+
+import java.util.List;
+
+/**
+ * Pipeline 风格的 Chain，一直往后进行处理。 需要注意的是，使用该模式时，不能在handler里调用chain.handle()
+ *
+ * @param <REQ>
+ * @param <RESP>
+ */
+public class PipelineChain<REQ, RESP> extends AbstractChain<REQ, RESP> {
+
+    private List<Handler<REQ, RESP>> handlers = Collects.emptyArrayList();
+
+    @Override
+    public void handle(final REQ request, final RESP response) {
+        Collects.forEach(handlers, new Consumer<Handler<REQ, RESP>>() {
+            @Override
+            public void accept(Handler<REQ, RESP> handler) {
+                handler.handle(request, response, PipelineChain.this);
+            }
+        });
+    }
+
+    public void addHandler(@NonNull Handler<REQ, RESP> handler) {
+        Preconditions.checkNotNull(handler);
+        handlers.add(handler);
+    }
+
+}
