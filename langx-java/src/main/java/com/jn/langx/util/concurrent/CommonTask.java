c39bae280c5e964c869d281321fd9c4ea26c5416
fjn <fjn@163.com>
2022-06-16 23:18:37 +0800

    * api增强

--- langx-java/src/main/java/com/jn/langx/util/concurrent/CommonTask.java
+++ langx-java/src/main/java/com/jn/langx/util/concurrent/CommonTask.java
@@ -1,5 +1,7 @@
 package com.jn.langx.util.concurrent;
 
+import com.jn.langx.util.Throwables;
+
 import java.util.concurrent.Callable;
 
 public class CommonTask<V> implements Callable<V>, Runnable {
@@ -14,20 +16,58 @@ public class CommonTask<V> implements Callable<V>, Runnable {
         this.t1 = t;
     }
 
-    @Override
-    public void run() {
-        t1.run();
-    }
 
     @Override
     public V call() throws Exception {
         return t0.call();
     }
 
-    public Class getExpectClass(){
-        if(t0!=null){
+    @Override
+    public void run() {
+        t1.run();
+    }
+
+    public Class getExpectClass() {
+        if (t0 != null) {
             return Callable.class;
         }
         return Runnable.class;
+    }
+
+    public static class RunnableToCallable<V> implements Callable<V> {
+        private Runnable runnable;
+        private V expectedResult;
+
+        public RunnableToCallable(Runnable runnable) {
+            this(runnable, null);
+        }
+
+        public RunnableToCallable(Runnable runnable, V expectedResult) {
+            this.runnable = runnable;
+            this.expectedResult = expectedResult;
+        }
+
+        @Override
+        public V call() throws Exception {
+            runnable.run();
+            return expectedResult;
+        }
+    }
+
+    public static class CallableToRunnable implements Runnable {
+        private Callable callable;
+
+        public CallableToRunnable(Callable callable) {
+            this.callable = callable;
+        }
+
+        @Override
+        public void run() {
+            try {
+                callable.call();
+            } catch (Throwable ex) {
+                throw Throwables.wrapAsRuntimeException(ex);
+            }
+        }
     }
 }
