dea31574dacbd206ee93b8f0e1eb9eb2c2ca9cf3
fjn <fjn@163.com>
2022-07-25 17:03:16 +0800

    * refactor StringTemplates#format

--- langx-java/src/main/java/com/jn/langx/text/grok/Grok.java
+++ langx-java/src/main/java/com/jn/langx/text/grok/Grok.java
@@ -1,7 +1,24 @@
 package com.jn.langx.text.grok;
 
+import com.jn.langx.util.regexp.Regexp;
+import com.jn.langx.util.regexp.Regexps;
+
 /**
  * @since 4.5.0
  */
 public class Grok {
+    public static final Regexp GROK_VARIABLE_REGEXP = Regexps.createRegexp(
+            "%\\{"
+                    + "(?<name>)"
+                    + "(?<pattern>[A-z0-9]+)"
+                    + "(?::(?<subname>[A-z0-9_:;,\\-\\/\\s\\.']+))?"
+                    + ")"
+                    + "(?:=(?<definition>"
+                    + "(?:"
+                    + "(?:[~{}]+|\\.+)+"
+                    + ")+"
+                    + ")"
+                    + ")?"
+                    + "\\}"
+    );
 }
