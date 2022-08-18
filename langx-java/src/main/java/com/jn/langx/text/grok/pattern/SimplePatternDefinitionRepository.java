99bb662c1eed1f3993e3d296effdd996eeb727ba
fjn <fjn@163.com>
2022-08-18 16:50:29 +0800

    % release 4.7.3

--- langx-java/src/main/java/com/jn/langx/text/grok/pattern/SimplePatternDefinitionRepository.java
+++ langx-java/src/main/java/com/jn/langx/text/grok/pattern/SimplePatternDefinitionRepository.java
@@ -4,12 +4,9 @@ import com.jn.langx.cache.Cache;
 import com.jn.langx.cache.CacheBuilder;
 import com.jn.langx.configuration.*;
 import com.jn.langx.lifecycle.InitializationException;
-import com.jn.langx.util.timing.timer.HashedWheelTimer;
 import com.jn.langx.util.timing.timer.Timer;
 import com.jn.langx.util.timing.timer.WheelTimers;
 
-import java.util.Comparator;
-import java.util.Map;
 
 /**
  * @since 4.5.1
@@ -20,55 +17,6 @@ public class SimplePatternDefinitionRepository extends AbstractConfigurationRepo
         setName("Grok-Pattern-Definition-Simple-Repository");
     }
 
-    @Override
-    public PatternDefinition getById(String id) {
-        return super.getById(id);
-    }
-
-    @Override
-    public void setCache(Cache<String, PatternDefinition> cache) {
-        super.setCache(cache);
-    }
-
-    @Override
-    public void setComparator(Comparator<PatternDefinition> comparator) {
-        super.setComparator(comparator);
-    }
-
-    @Override
-    public Comparator<PatternDefinition> getComparator() {
-        return super.getComparator();
-    }
-
-    @Override
-    public void add(PatternDefinition configuration) {
-        super.add(configuration);
-    }
-
-    @Override
-    public PatternDefinition add(PatternDefinition configuration, boolean sync) {
-        return super.add(configuration, sync);
-    }
-
-    @Override
-    public void update(PatternDefinition configuration) {
-        super.update(configuration);
-    }
-
-    @Override
-    public void update(PatternDefinition configuration, boolean sync) {
-        super.update(configuration, sync);
-    }
-
-    @Override
-    public Map<String, PatternDefinition> getAll() {
-        return super.getAll();
-    }
-
-    @Override
-    public void reload() {
-        super.reload();
-    }
 
     /**
      * @since 4.7.3
