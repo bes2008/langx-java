6caa2193e32859728a177cc15192715396cc5f6e
fjn1194361820 <fs1194361820@163.com>
2022-10-11 11:27:31 +0800

    * 代码整理

--- langx-java/src/main/java/com/jn/langx/io/stream/IOStreamInterceptor.java
+++ langx-java/src/main/java/com/jn/langx/io/stream/IOStreamInterceptor.java
@@ -1,11 +1,11 @@
 package com.jn.langx.io.stream;
 
-public interface IOStreamInterceptor<Stream> {
-    boolean beforeRead(Stream stream, final byte[] b, final int off, final int len);
+public interface IOStreamInterceptor<IOStream> {
+    boolean beforeRead(IOStream stream, final byte[] b, final int off, final int len);
 
-    boolean afterRead(Stream stream, final byte[] b, final int off, final int len);
+    boolean afterRead(IOStream stream, final byte[] b, final int off, final int len);
 
-    boolean beforeWrite(Stream stream, final byte[] b, final int off, final int len);
+    boolean beforeWrite(IOStream stream, final byte[] b, final int off, final int len);
 
-    boolean afterWrite(Stream stream, final byte[] b, final int off, final int len);
+    boolean afterWrite(IOStream stream, final byte[] b, final int off, final int len);
 }
