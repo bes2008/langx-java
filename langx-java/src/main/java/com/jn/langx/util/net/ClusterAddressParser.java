5173127b84a65194d11e99554284f78bd0342cb7
fjn1194361820 <fs1194361820@163.com>
2022-11-04 19:26:36 +0800

    * 完善集群地址解析

--- langx-java/src/main/java/com/jn/langx/util/net/ClusterAddressParser.java
+++ langx-java/src/main/java/com/jn/langx/util/net/ClusterAddressParser.java
@@ -2,6 +2,7 @@ package com.jn.langx.util.net;
 
 import com.jn.langx.Parser;
 import com.jn.langx.util.Emptys;
+import com.jn.langx.util.Numbers;
 import com.jn.langx.util.Preconditions;
 import com.jn.langx.util.Strings;
 import com.jn.langx.util.collection.Collects;
@@ -10,11 +11,15 @@ import com.jn.langx.util.function.Consumer2;
 import com.jn.langx.util.function.Predicate;
 import com.jn.langx.util.function.Predicate2;
 import com.jn.langx.util.logging.Loggers;
+import com.jn.langx.util.regexp.Regexp;
+import com.jn.langx.util.regexp.RegexpPatterns;
+import com.jn.langx.util.regexp.Regexps;
 import org.slf4j.Logger;
 
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * <pre>
@@ -38,7 +43,6 @@ public class ClusterAddressParser implements Parser<String, List<NetworkAddress>
 
     /**
      * 转换为 host1:port1,host2:port2,host3:port3结构
-     *
      */
     public static String normalize(List<NetworkAddress> addresses, final ClusterAddressStyle style) {
         Preconditions.checkNotEmpty(addresses);
@@ -72,45 +76,93 @@ public class ClusterAddressParser implements Parser<String, List<NetworkAddress>
 
         String[] segments = Strings.split(s, ",");
         Logger logger = Loggers.getLogger(getClass());
+        Regexp IP_SEGMENT_PATTERNS = Regexps.createRegexp("(?<ip>[^/]*)(/(?<prefixLength>\\d{1,6})(:(?<port>\\d{1,5}))?)?");
+        Regexp IPv4_SEGMENT_PATTERN = Regexps.createRegexp("(?<ip>[^:]*)(:(?<port>\\d{1,5}))?");
         for (String segment : segments) {
             segment = Strings.strip(segment);
-            String[] ipSegments = segment.split(":");
-            if (ipSegments.length < 1) {
-                // ignore
-            } else if (ipSegments.length == 1) {
-                // 只有 ip, 且是ipv4
-                ret.add(new NetworkAddress(ipSegments[0], 0));
-            } else if (ipSegments.length == 2) {
-                // ip:port 结构，且是 ipv4
-                try {
-                    int port = Integer.parseInt(ipSegments[1]);
-                    ret.add(new NetworkAddress(ipSegments[0], port));
-                } catch (NumberFormatException ex) {
-                    logger.error("not a illegal address string: {}", s);
+            // ip/prefixLength:prot
+            Map<String, String> stringMap = Regexps.findNamedGroup(IP_SEGMENT_PATTERNS, segment);
+            if (stringMap != null) {
+                String ip = stringMap.get("ip");
+                if (Strings.isBlank(ip)) {
+                    logger.warn("invalid ip address: {}", segment);
                 }
-            } else if (ipSegments.length == 3 && segment.contains("::")) {
-                // ip v6 缩写形式，无端口
-                ret.add(new NetworkAddress(segment, 0));
-            } else if (ipSegments.length == 4 && segment.contains("::")) {
-                // ip v6 缩写形式，有端口
-                ret.add(new NetworkAddress(segment, 0));
-            } else if (ipSegments.length == 8) {
-                // ip v6 缩写形式，无端口
-                ret.add(new NetworkAddress(segment, 0));
-            } else if (ipSegments.length == 9) {
-                // ip v6 缩写形式，有端口
-                try {
-                    int port = Integer.parseInt(ipSegments[ipSegments.length - 1]);
-                    ret.add(new NetworkAddress(segment.substring(0, segment.lastIndexOf(":")), port));
-                } catch (NumberFormatException ex) {
-                    logger.error("not a illegal address string: {}", s);
+
+                String prefixLength = stringMap.get("prefixLength");
+                if (Strings.isNotEmpty(prefixLength)) {
+                    ip = ip + "/" + prefixLength;
+                }
+                boolean hasBrace = ip.startsWith("[") && segment.endsWith("]");
+                ip = hasBrace ? Strings.substring(ip, 1, ip.length() - 1) : segment;
+
+                String portString = stringMap.get("port");
+                int port = Strings.isEmpty(portString) ? 0 : Numbers.createInteger(portString);
+                if (port > 65535) {
+                    logger.warn("invalid port : {}", segment);
+                    continue;
+                }
+                if (!hasBrace && Regexps.match(RegexpPatterns.PATTERN_IP, ip)) {
+                    // IP v4
+                    ret.add(new NetworkAddress(ip, port));
+                } else if (!hasBrace && Regexps.match("\\w+(:\\d{1,5})?", ip)) {
+                    Map<String, String> string2Map = Regexps.findNamedGroup(IPv4_SEGMENT_PATTERN, segment);
+                    if (string2Map != null) {
+                        ip = string2Map.get("ip");
+                        if (Strings.isBlank(ip)) {
+                            logger.warn("invalid ip address: {}", segment);
+                        }
+                        portString = string2Map.get("port");
+                        port = Strings.isEmpty(portString) ? 0 : Numbers.createInteger(portString);
+                        if (port > 65535) {
+                            logger.warn("invalid port : {}", segment);
+                            continue;
+                        }
+                        ret.add(new NetworkAddress(ip, port));
+                    }
+                } else if (Regexps.match(RegexpPatterns.PATTERN_IPv6, ip)) {
+                    if (!hasBrace) {
+                        ret.add(new NetworkAddress(ip, port));
+                    } else {
+                        ret.add(new NetworkAddress("[" + ip + "]", port));
+                    }
+                } else {
+                    int portSeparator = Strings.lastIndexOf(segment, ":");
+                    if (portSeparator >= 1 && portSeparator < segment.length() - 1) {
+                        if (':' == segment.charAt(portSeparator - 1)) {
+                            logger.warn("invalid ip address: {}", segment);
+                            continue;
+                        } else {
+                            portString = Strings.substring(segment, portSeparator + 1);
+                            try {
+                                port = Strings.isEmpty(portString) ? 0 : Numbers.createInteger(portString);
+                            } catch (Throwable e) {
+                                logger.warn("invalid ip address: {}", segment);
+                                continue;
+                            }
+                            ip = Strings.substring(segment, 0, portSeparator);
+                            if (!hasBrace && Regexps.match(RegexpPatterns.PATTERN_IP, ip)) {
+                                // IP v4
+                                ret.add(new NetworkAddress(ip, port));
+                                continue;
+                            } else if (Regexps.match(RegexpPatterns.PATTERN_IPv6, ip)) {
+                                if (!hasBrace) {
+                                    ret.add(new NetworkAddress(ip, port));
+                                } else {
+                                    ret.add(new NetworkAddress("[" + ip + "]", port));
+                                }
+                                continue;
+                            }
+                        }
+                    }
+
+                    logger.warn("invalid ipv6 address: {}", segment);
                 }
             } else {
-                logger.error("not a illegal address string: {}", s);
+                logger.warn("invalid ip address: {}", segment);
             }
         }
 
-        // 只有最后一个地址又端口时，useUnifiedPortInAddressString 的值为true，即表示统一使用该端口
+        // 只有最后一个地址有端口时，useUnifiedPortInAddressString 的值为true，即表示统一使用该端口
         int firstValidPortIndex = Collects.firstOccurrence(ret, new Predicate2<Integer, NetworkAddress>() {
             @Override
             public boolean test(Integer index, NetworkAddress address) {
