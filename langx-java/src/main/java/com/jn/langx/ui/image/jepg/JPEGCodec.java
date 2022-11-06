1c813bf9d236ef5c8fc5d93e0343e9ce9161989a
fjn <1194361820@qq.com>
2022-11-06 21:55:33 +0800

    * 简化代码

--- langx-java/src/main/java/com/jn/langx/ui/image/jepg/JPEGCodec.java
+++ langx-java/src/main/java/com/jn/langx/ui/image/jepg/JPEGCodec.java
@@ -1,9 +1,3 @@
-/*
- *
- * Copyright (c) 2007, Oracle and/or its affiliates. All rights reserved.
- * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- */
-
 package com.jn.langx.ui.image.jepg;
 
 
--- langx-java/src/main/java/com/jn/langx/ui/image/jepg/TruncatedFileException.java
+++ langx-java/src/main/java/com/jn/langx/ui/image/jepg/TruncatedFileException.java
@@ -1,19 +1,3 @@
-/*
- *
- * Copyright (c) 2007, Oracle and/or its affiliates. All rights reserved.
- * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- */
-
-/* ********************************************************************
- **********************************************************************
- **********************************************************************
- *** COPYRIGHT (c) Eastman Kodak Company, 1997                      ***
- *** As  an unpublished  work pursuant to Title 17 of the United    ***
- *** States Code.  All rights reserved.                             ***
- **********************************************************************
- **********************************************************************
- **********************************************************************/
-
 package com.jn.langx.ui.image.jepg;
 
 import java.awt.image.BufferedImage;
