c1ae0065b237d0f000e5a638715deabe06e9ae68
fjn1194361820 <fs1194361820@163.com>
2022-12-23 15:46:51 +0800

    * 提供默认clock

---
+++ langx-java/src/main/java/com/jn/langx/util/timing/clock/Clocks.java
@@ -0,0 +1,14 @@
+package com.jn.langx.util.timing.clock;
+
+public class Clocks {
+    private static final Clock DEFAULT = new SystemClock();
+
+    /**
+     * The default clock to use.
+     *
+     * @return the default {@link Clock} instance
+     */
+    public static Clock defaultClock() {
+        return DEFAULT;
+    }
+}
