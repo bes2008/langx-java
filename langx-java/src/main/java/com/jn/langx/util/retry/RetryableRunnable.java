fb6275486b174640e7fa3ec306ada450a1f985e5
fjn1194361820 <fs1194361820@163.com>
2023-01-14 16:51:53 +0800

    * upgrade retry

---
+++ langx-java/src/main/java/com/jn/langx/util/retry/RetryableRunnable.java
@@ -0,0 +1,52 @@
+package com.jn.langx.util.retry;
+
+public class RetryableRunnable implements Runnable{
+    private transient Runnable task;
+    private RetryConfig retryConfig = new RetryConfig();
+    /**
+     * 已尝试次数
+     */
+    private int attempts;
+
+    public int getAttempts() {
+        return attempts;
+    }
+
+    public void setAttempts(int attempts) {
+        this.attempts = attempts;
+    }
+
+    public void addAttempts(){
+        this.addAttempts(1);
+    }
+
+    public void addAttempts(int count){
+        this.attempts = this.attempts + count;
+    }
+
+    public void setTask(Runnable task) {
+        this.task = task;
+    }
+
+    public Runnable getTask() {
+        return task;
+    }
+
+    public RetryConfig getRetryConfig() {
+        return retryConfig;
+    }
+
+    public void setRetryConfig(RetryConfig retryConfig) {
+        this.retryConfig = retryConfig;
+    }
+
+    @Override
+    public void run() {
+        if(this.retryConfig==null){
+            this.retryConfig  = new RetryConfig();
+        }
+        if(this.attempts<retryConfig.getMaxAttempts()){
+            this.task.run();
+        }
+    }
+}
--- langx-java/src/main/java/com/jn/langx/util/retry/Retryer.java
+++ langx-java/src/main/java/com/jn/langx/util/retry/Retryer.java
@@ -1,37 +1,43 @@
 package com.jn.langx.util.retry;
 
 import com.jn.langx.annotation.NonNull;
+import com.jn.langx.annotation.Nullable;
 import com.jn.langx.text.StringTemplates;
 import com.jn.langx.util.concurrent.Executable;
-import com.jn.langx.util.function.Consumer2;
+import com.jn.langx.util.function.Consumer3;
 import com.jn.langx.util.function.Functions;
-import com.jn.langx.util.function.Predicate;
+import com.jn.langx.util.function.Predicate2;
 
-public class Retryer {
+public class Retryer<CTX> {
     @NonNull
     private RetryConfig config;
     @NonNull
-    private Predicate<Throwable> retryPredicate;
+    private Predicate2<CTX, Throwable> retryPredicate;
     @NonNull
-    private Consumer2<RetryInfo, Throwable> errorListener;
+    private Consumer3<RetryInfo, CTX, Throwable> errorListener;
 
 
     public Retryer(RetryConfig config) {
         this(null, config);
     }
 
-    public Retryer(Predicate<Throwable> retryPredicate, RetryConfig config) {
+    public Retryer(Predicate2<CTX, Throwable> retryPredicate, RetryConfig config) {
         this(retryPredicate, config, null);
     }
 
-    public Retryer(Predicate<Throwable> retryPredicate, RetryConfig config, Consumer2<RetryInfo, Throwable> errorListener) {
-        this.retryPredicate = retryPredicate == null ? Functions.<Throwable>truePredicate() : retryPredicate;
-        this.errorListener = errorListener == null ? Functions.<RetryInfo, Throwable>noopConsumer2() : errorListener;
+    public Retryer(Predicate2<CTX, Throwable> retryPredicate, RetryConfig config, Consumer3<RetryInfo, CTX, Throwable> errorListener) {
+        this.retryPredicate = retryPredicate == null ? Functions.<CTX, Throwable>truePredicate2() : retryPredicate;
+        this.errorListener = errorListener == null ? new Consumer3<RetryInfo, CTX, Throwable>() {
+            @Override
+            public void accept(RetryInfo retryInfo, CTX ctx, Throwable throwable) {
+                // noop
+            }
+        } : errorListener;
         this.config = config;
     }
 
-    public static <R> R execute(Predicate<Throwable> retryPredicate, RetryConfig retryConfig, Consumer2<RetryInfo, Throwable> errorListener, Executable<R> executable, Object parameters) throws Exception {
-        Retryer retryer = new Retryer(retryPredicate, retryConfig, errorListener);
+    public static <CTX, R> R execute(Predicate2<CTX, Throwable> retryPredicate, RetryConfig retryConfig, Consumer3<RetryInfo, CTX, Throwable> errorListener, Executable<R> executable, Object parameters) throws Exception {
+        Retryer<CTX> retryer = new Retryer<CTX>(retryPredicate, retryConfig, errorListener);
         return retryer.executeWithRetry(executable, 1, parameters);
     }
 
@@ -43,7 +49,7 @@ public class Retryer {
             R r = executable.execute(parameters);
             return r;
         } catch (Throwable e) {
-            if (waitAndRetry(attempt, e)) {
+            if (waitAndJudgeRetry(attempt, e)) {
                 return executeWithRetry(executable, attempt + 1, parameters);
             } else {
                 if (e instanceof Error) {
@@ -54,26 +60,34 @@ public class Retryer {
         }
     }
 
+    private boolean waitAndJudgeRetry(int attempt, Throwable e) {
+        return waitAndJudgeRetry(attempt, this.config, retryPredicate, errorListener, null, e);
+    }
+
     /**
      * @return 返回是否需要retry
      */
-    private boolean waitAndRetry(int attempt, Throwable e) {
-        if (!isExhausted(attempt, this.config.getMaxAttempts()) && this.retryPredicate.test(e)) {
-            long backoffMillis = this.config.getBackoffPolicy().getBackoffTime(this.config, attempt);
+    public static <CTX> boolean waitAndJudgeRetry(int attempt, RetryConfig retryConfig, @Nullable Predicate2<CTX, Throwable> retryPredicate, @Nullable Consumer3<RetryInfo, CTX, Throwable> errorListener, @Nullable CTX ctx, Throwable error) {
+
+        if (!isExhausted(attempt, retryConfig.getMaxAttempts()) && (retryPredicate != null && retryPredicate.test(ctx, error))) {
+            long backoffMillis = retryConfig.getBackoffPolicy().getBackoffTime(retryConfig, attempt);
             if (backoffMillis < 0) {
                 throw new RuntimeException(StringTemplates.formatWithPlaceholder("invalid retry backoff: {}", backoffMillis));
             }
-            RetryInfo retryInfo = new RetryInfo(attempt, this.config.getMaxAttempts(), backoffMillis);
-            this.errorListener.accept(retryInfo, e);
+
+            if (errorListener != null) {
+                RetryInfo retryInfo = new RetryInfo(attempt, retryConfig.getMaxAttempts(), backoffMillis);
+                errorListener.accept(retryInfo, ctx, error);
+            }
             try {
                 if (backoffMillis > 0) {
-                    this.config.getTimeUnit().sleep(backoffMillis);
+                    retryConfig.getTimeUnit().sleep(backoffMillis);
                 } else {
                     return false;
                 }
 
             } catch (InterruptedException interruptedException) {
-                throw new RuntimeException(e);
+                throw new RuntimeException(error);
             }
             return true;
         } else {
@@ -81,7 +95,7 @@ public class Retryer {
         }
     }
 
-    private boolean isExhausted(int attempt, int maxAttempts) {
+    public static boolean isExhausted(int attempt, int maxAttempts) {
         if (maxAttempts <= 0) {
             // 无次数限制
             return false;
