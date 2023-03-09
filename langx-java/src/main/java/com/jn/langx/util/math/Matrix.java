2cc3d617dce10c3533c184aeba9c605b15758641
fjn <1194361820@qq.com>
2023-03-09 22:11:11 +0800

    + 支持矩阵运算

---
+++ langx-java/src/main/java/com/jn/langx/util/math/Matrix.java
@@ -0,0 +1,296 @@
+package com.jn.langx.util.math;
+import com.jn.langx.util.math.NumVector.Modified;
+
+public final class Matrix {
+    private final int width;
+    private final int height;
+    private final NumVector vector;
+
+    public static Matrix create(int height, double... values) {
+        if (values.length == 0) {
+            throw new IllegalArgumentException("no values");
+        } else if (height <= 0) {
+            throw new IllegalArgumentException("unexpected height");
+        } else {
+            int width = values.length / height;
+            if (width * height != values.length) {
+                throw new IllegalArgumentException("unexpected amount of values");
+            } else {
+                return new Matrix(width, height, (double[])values.clone());
+            }
+        }
+    }
+
+    public static Matrix createIdentity(int size) {
+        if (size <= 0) {
+            throw new IllegalArgumentException("unexpected size");
+        } else {
+            double[] array = new double[size * size];
+            int index = 0;
+
+            for(int i = 0; i < size; index += size + 1) {
+                array[index] = 1.0D;
+                ++i;
+            }
+
+            return new Matrix(size, size, array);
+        }
+    }
+
+    public static Matrix createColumn(NumVector vector) {
+        return new Matrix(1, vector.getSize(), vector);
+    }
+
+    public static Matrix createRow(NumVector vector) {
+        return new Matrix(vector.getSize(), 1, vector);
+    }
+
+    public boolean equals(Object object) {
+        if (!(object instanceof Matrix)) {
+            return false;
+        } else {
+            Matrix matrix = (Matrix)object;
+            return this.width == matrix.width && this.height == matrix.height && this.vector.equals(matrix.vector);
+        }
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Matrix ").append(this.height).append("x").append(this.width).append(" { ");
+        int i = 0;
+
+        for(int h = 0; h < this.height; ++h) {
+            if (h != 0) {
+                sb.append(", ");
+            }
+
+            sb.append("{");
+
+            for(int w = 0; w < this.width; ++i) {
+                if (w != 0) {
+                    sb.append(", ");
+                }
+
+                sb.append(this.vector.get(i));
+                ++w;
+            }
+
+            sb.append("}");
+        }
+
+        return sb.append(" }").toString();
+    }
+
+    public double get(int column, int row) {
+        validate(column, this.width);
+        validate(row, this.height);
+        return this.vector.get(column + row * this.width);
+    }
+
+    public int getColumns() {
+        return this.width;
+    }
+
+    public NumVector getColumn(final int column) {
+        validate(column, this.width);
+        return new NumVector.Modified(this.vector) {
+            public double get(int row) {
+                Matrix.validate(row, Matrix.this.height);
+                return super.get(column + row * Matrix.this.width);
+            }
+
+            public int getSize() {
+                return Matrix.this.height;
+            }
+        };
+    }
+
+    public int getRows() {
+        return this.height;
+    }
+
+    public NumVector getRow(final int row) {
+        validate(row, this.height);
+        return new Modified(this.vector) {
+            public double get(int column) {
+                Matrix.validate(column, Matrix.this.width);
+                return super.get(column + row * Matrix.this.width);
+            }
+
+            public int getSize() {
+                return Matrix.this.width;
+            }
+        };
+    }
+
+    public Matrix plus(Matrix matrix) {
+        if (this.getColumns() != matrix.getColumns()) {
+            throw new IllegalArgumentException("columns mismatch");
+        } else if (this.getRows() != matrix.getRows()) {
+            throw new IllegalArgumentException("rows mismatch");
+        } else {
+            return new Matrix(this.width, this.height, this.vector.plus(matrix.vector));
+        }
+    }
+
+    public Matrix minus(Matrix matrix) {
+        if (this.getColumns() != matrix.getColumns()) {
+            throw new IllegalArgumentException("columns mismatch");
+        } else if (this.getRows() != matrix.getRows()) {
+            throw new IllegalArgumentException("rows mismatch");
+        } else {
+            return new Matrix(this.width, this.height, this.vector.minus(matrix.vector));
+        }
+    }
+
+    public Matrix multiply(double value) {
+        return new Matrix(this.width, this.height, this.vector.multiply(value));
+    }
+
+    public Matrix multiply(Matrix matrix) {
+        if (this.getColumns() != matrix.getRows()) {
+            throw new IllegalArgumentException("columns mismatch rows");
+        } else {
+            int width = matrix.getColumns();
+            int height = this.getRows();
+            double[] result = new double[width * height];
+            int i = 0;
+
+            for(int h = 0; h < height; ++h) {
+                NumVector row = this.getRow(h);
+
+                for(int w = 0; w < width; ++i) {
+                    result[i] = row.multiply(matrix.getColumn(w));
+                    ++w;
+                }
+            }
+
+            return new Matrix(width, height, result);
+        }
+    }
+
+    public NumVector multiply(NumVector vector) {
+        if (this.getColumns() != vector.getSize()) {
+            throw new IllegalArgumentException("columns mismatch length");
+        } else {
+            double[] result = new double[this.getRows()];
+
+            for(int i = 0; i < result.length; ++i) {
+                result[i] = this.getRow(i).multiply(vector);
+            }
+
+            return new NumVector(result);
+        }
+    }
+
+    public double determinant() {
+        if (this.width != this.height) {
+            throw new IllegalArgumentException("not a square");
+        } else if (this.width == 1) {
+            return this.vector.get(0);
+        } else if (this.width == 2) {
+            return this.vector.get(0) * this.vector.get(3) - this.vector.get(1) * this.vector.get(2);
+        } else {
+            double result = 0.0D;
+
+            for(int i = 0; i < this.width; ++i) {
+                double value = this.vector.get(i) * this.exclude(i, 0).determinant();
+                result -= isEven(i) ? -value : value;
+            }
+
+            return result;
+        }
+    }
+
+    public Matrix transpose() {
+        double[] result = new double[this.vector.getSize()];
+        int i = 0;
+
+        for(int w = 0; w < this.width; ++w) {
+            for(int h = 0; h < this.height; ++i) {
+                result[i] = this.get(w, h);
+                ++h;
+            }
+        }
+
+        return new Matrix(this.height, this.width, result);
+    }
+
+    public Matrix inverse() {
+        double value = this.determinant();
+        if (value == 0.0D) {
+            throw new IllegalArgumentException("determinant is 0");
+        } else {
+            return this.cofactor().transpose().multiply(1.0D / value);
+        }
+    }
+
+    private Matrix exclude(int column, int row) {
+        validate(column, this.width);
+        validate(row, this.height);
+        int width = this.getColumns() - 1;
+        if (width == 0) {
+            throw new IllegalArgumentException("cannot exclude last column");
+        } else {
+            int height = this.getRows() - 1;
+            if (height == 0) {
+                throw new IllegalArgumentException("cannot exclude last row");
+            } else {
+                double[] result = new double[width * height];
+                int index = 0;
+                int i = 0;
+
+                for(int h = 0; h <= height; ++h) {
+                    for(int w = 0; w <= width; ++i) {
+                        if (w != column && h != row) {
+                            result[index++] = this.vector.get(i);
+                        }
+
+                        ++w;
+                    }
+                }
+
+                return new Matrix(width, height, result);
+            }
+        }
+    }
+
+    private Matrix cofactor() {
+        if (this.width != this.height) {
+            throw new IllegalArgumentException("not a square");
+        } else {
+            double[] result = new double[this.vector.getSize()];
+            int i = 0;
+
+            for(int h = 0; h < this.height; ++h) {
+                for(int w = 0; w < this.width; ++i) {
+                    double value = this.exclude(w, h).determinant();
+                    result[i] = isEven(w) != isEven(h) ? -value : value;
+                    ++w;
+                }
+            }
+
+            return new Matrix(this.width, this.height, result);
+        }
+    }
+
+    private Matrix(int width, int height, double... values) {
+        this(width, height, new NumVector(values));
+    }
+
+    private Matrix(int width, int height, NumVector vector) {
+        this.width = width;
+        this.height = height;
+        this.vector = vector;
+    }
+
+    private static boolean isEven(int i) {
+        return i % 2 == 0;
+    }
+
+    private static void validate(int index, int max) {
+        if (index < 0 || max <= index) {
+            throw new ArrayIndexOutOfBoundsException(index);
+        }
+    }
+}
---
+++ langx-java/src/main/java/com/jn/langx/util/math/NumVector.java
@@ -0,0 +1,158 @@
+package com.jn.langx.util.math;
+
+public class NumVector {
+    private final double[] array;
+
+    public static NumVector create(double... values) {
+        if (values.length == 0) {
+            throw new IllegalArgumentException("no values");
+        } else {
+            return new NumVector((double[])values.clone());
+        }
+    }
+
+    public boolean equals(Object object) {
+        if (object == this) {
+            return true;
+        } else {
+            if (object instanceof NumVector) {
+                NumVector vector = (NumVector)object;
+                int size = vector.getSize();
+                if (size == this.getSize()) {
+                    for(int i = 0; i < size; ++i) {
+                        if (Double.doubleToLongBits(this.get(i)) != Double.doubleToLongBits(vector.get(i))) {
+                            return false;
+                        }
+                    }
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    public String toString() {
+        int size = this.getSize();
+        StringBuilder sb = new StringBuilder();
+        sb.append("Vector ").append(size).append(" { ");
+
+        for(int i = 0; i < size; ++i) {
+            if (i != 0) {
+                sb.append(", ");
+            }
+
+            sb.append(this.get(i));
+        }
+
+        return sb.append(" }").toString();
+    }
+
+    public double get(int index) {
+        return this.array[index];
+    }
+
+    public int getSize() {
+        return this.array.length;
+    }
+
+    public NumVector plus(NumVector vector) {
+        int size = vector.getSize();
+        if (size != this.getSize()) {
+            throw new IllegalArgumentException("sizes mismatch");
+        } else {
+            double[] result = this.toArray();
+
+            for(int i = 0; i < result.length; ++i) {
+                result[i] += vector.get(i);
+            }
+
+            return new NumVector(result);
+        }
+    }
+
+    public NumVector minus(NumVector vector) {
+        int size = vector.getSize();
+        if (size != this.getSize()) {
+            throw new IllegalArgumentException("sizes mismatch");
+        } else {
+            double[] result = this.toArray();
+
+            for(int i = 0; i < result.length; ++i) {
+                result[i] -= vector.get(i);
+            }
+
+            return new NumVector(result);
+        }
+    }
+
+    public NumVector multiply(double value) {
+        double[] result = this.toArray();
+
+        for(int i = 0; i < result.length; ++i) {
+            result[i] *= value;
+        }
+
+        return new NumVector(result);
+    }
+
+    public NumVector multiply(Matrix matrix) {
+        int size = matrix.getRows();
+        if (size != this.getSize()) {
+            throw new IllegalArgumentException("size mismatch rows");
+        } else {
+            double[] result = new double[matrix.getColumns()];
+
+            for(int i = 0; i < result.length; ++i) {
+                result[i] = this.multiply(matrix.getColumn(i));
+            }
+
+            return new NumVector(result);
+        }
+    }
+
+    public double multiply(NumVector vector) {
+        int size = vector.getSize();
+        if (size != this.getSize()) {
+            throw new IllegalArgumentException("sizes mismatch");
+        } else {
+            double result = 0.0D;
+
+            for(int i = 0; i < size; ++i) {
+                result += this.get(i) * vector.get(i);
+            }
+
+            return result;
+        }
+    }
+
+    public double length() {
+        return Math.sqrt(this.multiply(this));
+    }
+
+    NumVector(double[] array) {
+        this.array = array;
+    }
+
+    double[] toArray() {
+        return (double[])this.array.clone();
+    }
+
+    static class Modified extends NumVector {
+        Modified(NumVector vector) {
+            super(vector.array);
+        }
+
+        double[] toArray() {
+            int size = this.getSize();
+            double[] result = new double[size];
+
+            for(int i = 0; i < result.length; ++i) {
+                result[i] = this.get(i);
+            }
+
+            return result;
+        }
+    }
+}
