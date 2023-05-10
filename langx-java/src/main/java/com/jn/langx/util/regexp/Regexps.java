e390d767f3f1836739a00c81caed1d216d290d41
fjn1194361820 <fs1194361820@163.com>
2023-05-10 20:56:27 +0800

    * 默认采集JDK regexp

--- langx-java/src/main/java/com/jn/langx/util/regexp/Regexps.java
+++ langx-java/src/main/java/com/jn/langx/util/regexp/Regexps.java
@@ -105,8 +105,9 @@ public class Regexps {
         if (option == null) {
             option = Option.DEFAULT;
         }
+
         if (engine == null) {
-            engine = getRegexpEngine("joni");
+            engine = getRegexpEngine("jdk");
         }
 
         Regexp regexp = cache.get(pattern, option, engine.getName());
--- langx-java/src/test/java/com/jn/langx/test/util/regexp/ChineseTests.java
+++ langx-java/src/test/java/com/jn/langx/test/util/regexp/ChineseTests.java
@@ -1,17 +1,32 @@
 package com.jn.langx.test.util.regexp;
 
+import com.jn.langx.security.crypto.digest.MessageDigests;
 import com.jn.langx.util.regexp.RegexpPatterns;
+import com.jn.langx.util.regexp.Regexps;
 import org.junit.Test;
 
 public class ChineseTests {
     @Test
-    public void test(){
+    public void test() {
         System.out.println(isChinese("你"));
         System.out.println(isChinese("︱"));
         System.out.println(isChinese("\uD840\uDC04"));
     }
 
-    private boolean isChinese(String st){
+    private boolean isChinese(String st) {
         return st.matches(RegexpPatterns.CHINESE_CHAR_PATTERN);
+    }
+
+    public static String chineseToMD5String(String str) {
+        if (Regexps.contains(str, RegexpPatterns.CHINESE_CHAR)) {
+            str = MessageDigests.md5(str);
+        }
+        return str;
+    }
+
+    @Test
+    public void test1(){
+        String s = "123421中你3人23";
+        System.out.println(chineseToMD5String(s));
     }
 }
