ed937bb755fb23b8fcfd01b3462a7a000d149b75
fjn <1194361820@qq.com>
2023-05-10 08:53:07 +0800

    + 提供regexp watch dog

--- langx-java/src/main/java/com/jn/langx/util/regexp/MatcherWatchdog.java
+++ langx-java/src/main/java/com/jn/langx/util/regexp/MatcherWatchdog.java
@@ -1,19 +1,14 @@
 package com.jn.langx.util.regexp;
 
-
-import com.jn.langx.util.function.Consumer2;
-import com.jn.langx.util.function.supplier.LongSupplier;
-
-
 /**
  * Protects against long running operations that happen between the register and unregister invocations.
  * Threads that invoke {@link #register(RegexpMatcher)}, but take too long to invoke the {@link #unregister(RegexpMatcher)} method
  * will be interrupted.
- *
+ * <p>
  * This is needed for Joni's {@link org.joni.Matcher#search(int, int, int)} method, because
  * it can end up spinning endlessly if the regular expression is too complex. Joni has checks
  * that for every 30k iterations it checks if the current thread is interrupted and if so
- * returns {@link org.joni.Matcher#INTERRUPTED}.
+ * returns {@link RegexpMatcher#interrupt()}.
  */
 public interface MatcherWatchdog {
 
@@ -27,7 +22,7 @@ public interface MatcherWatchdog {
 
     /**
      * @return The maximum allowed time in milliseconds for a thread to invoke {@link #unregister(RegexpMatcher)}
-     *         after {@link #register(RegexpMatcher)} has been invoked before this ThreadWatchDog starts to interrupting that thread.
+     * after {@link #register(RegexpMatcher)} has been invoked before this ThreadWatchDog starts to interrupting that thread.
      */
     long maxExecutionTimeInMillis();
 
@@ -37,10 +32,6 @@ public interface MatcherWatchdog {
      * @param matcher The matcher to unregister
      */
     void unregister(RegexpMatcher matcher);
-
-
-
-
 
 
 }
