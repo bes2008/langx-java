cadd916d5b23e50ad153dce7bfc089375a92a65d
fjn1194361820 <fs1194361820@163.com>
2023-05-12 14:32:39 +0800

    + 独立 commandline 模块

---
+++ langx-commandline/src/main/java/com/jn/langx/commandline/launcher/CommandLauncherProxy.java
@@ -0,0 +1,35 @@
+package com.jn.langx.commandline.launcher;
+
+import com.jn.langx.commandline.CommandLine;
+import com.jn.langx.commandline.ProcessAdapter;
+
+import java.io.IOException;
+import java.util.Map;
+
+
+/**
+ * A command launcher that proxies another command launcher. Sub-classes
+ * override exec(args, env, workdir)
+ */
+public abstract class CommandLauncherProxy extends LocalCommandLauncher {
+
+    protected CommandLauncherProxy(final CommandLauncher<ProcessAdapter> launcher) {
+        myLauncher = launcher;
+    }
+
+    private final CommandLauncher<ProcessAdapter> myLauncher;
+
+    /**
+     * Launches the given command in a new process. Delegates this method to the
+     * proxied launcher
+     *
+     * @param cmd the command line to execute as an array of strings
+     * @param env the environment to set as an array of strings
+     * @throws IOException forwarded from the exec method of the command launcher
+     */
+    @Override
+    public ProcessAdapter exec(final CommandLine cmd, final Map<String, String> env)
+            throws IOException {
+        return myLauncher.exec(cmd, env);
+    }
+}
---
+++ langx-commandline/src/main/java/com/jn/langx/util/os/hardware/cpu/CPUs.java
@@ -0,0 +1,49 @@
+package com.jn.langx.util.os.hardware.cpu;
+
+import com.jn.langx.commandline.CommandLine;
+import com.jn.langx.commandline.DefaultCommandLineExecutor;
+import com.jn.langx.exception.UnsupportedPlatformException;
+import com.jn.langx.util.logging.Loggers;
+import com.jn.langx.util.os.OS;
+import com.jn.langx.util.struct.Holder;
+import org.slf4j.Logger;
+
+
+public class CPUs {
+    private static Holder<String> cpuId;
+
+    private CPUs() {
+    }
+
+    public static String getCpuId() {
+        if (cpuId == null) {
+            DefaultCommandLineExecutor executor = new DefaultCommandLineExecutor();
+            CommandLine commandLine = null;
+            GetCpuIdStreamHandler handler = null;
+            if (OS.isFamilyWindows()) {
+                commandLine = CommandLine.parse("wmic cpu get ProcessorId");
+                handler = new WindowsGetCpuIdStreamHandler();
+            } else if (OS.isFamilyUnix()) {
+                commandLine = CommandLine.parse("sudo dmidecode -t 4 | grep ID");
+                handler = new LinuxGetCpuIdStreamHandler();
+            }
+            final Logger logger = Loggers.getLogger(CPUs.class);
+            if (commandLine != null) {
+                try {
+                    executor.setStreamHandler(handler);
+                    executor.execute(commandLine);
+                } catch (Exception ex) {
+                    logger.error(ex.getMessage(), ex);
+                }
+            } else {
+                logger.error("Unsupported operation for current platform");
+            }
+            if (handler != null) {
+                cpuId = new Holder<String>(handler.getCpuId());
+            } else {
+                throw new UnsupportedPlatformException();
+            }
+        }
+        return cpuId.get();
+    }
+}
