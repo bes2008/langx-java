93a11a09a9cb9c55212f852ce0dfc3f6029902e2
fjn1194361820 <fs1194361820@163.com>
2023-05-12 14:46:56 +0800

    Revert "+ 独立 commandline 模块"
    This reverts commit cadd916d5b23e50ad153dce7bfc089375a92a65d.

--- langx-commandline/src/main/java/com/jn/langx/util/os/hardware/cpu/GetCpuIdStreamHandler.java
+++
@@ -1,7 +0,0 @@
-package com.jn.langx.util.os.hardware.cpu;
-
-import com.jn.langx.commandline.streamhandler.OutputExtractExecuteStreamHandler;
-
-abstract class GetCpuIdStreamHandler extends OutputExtractExecuteStreamHandler<String> {
-    public abstract String getCpuId();
-}
