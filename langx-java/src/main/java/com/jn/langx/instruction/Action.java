56dd0a28bcb52c9e1347734748e9243fc3ac0f79
fjn1194361820 <fs1194361820@163.com>
2023-06-07 17:25:30 +0800

    - 移出未完成的代码，在 dsl_instruction 演进

--- langx-java/src/main/java/com/jn/langx/instruction/Action.java
+++
@@ -1,5 +0,0 @@
-package com.jn.langx.instruction;
-
-public interface Action<R> {
-    <R> R doAction(Statement statement);
-}
--- langx-java/src/main/java/com/jn/langx/instruction/UndefinedException.java
+++
@@ -1,7 +0,0 @@
-package com.jn.langx.instruction;
-
-public class UndefinedException extends RuntimeException {
-    public UndefinedException(String message) {
-        super(message);
-    }
-}
