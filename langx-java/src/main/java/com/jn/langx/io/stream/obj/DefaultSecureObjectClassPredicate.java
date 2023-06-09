d84271bdb45d45234c1014d1aead580eb262e6be
fjn1194361820 <fs1194361820@163.com>
2023-06-09 13:51:20 +0800

    * 支持 Secure Object InputStream

--- langx-java/src/main/java/com/jn/langx/io/stream/obj/DefaultSecureObjectClassPredicate.java
+++ langx-java/src/main/java/com/jn/langx/io/stream/obj/DefaultSecureObjectClassPredicate.java
@@ -1,13 +1,26 @@
 package com.jn.langx.io.stream.obj;
 
+import com.jn.langx.util.function.Functions;
+import com.jn.langx.util.function.Predicate;
+
 import java.io.ObjectStreamClass;
 
 /**
  * @since 5.2.9
  */
-final class DefaultSecureObjectClassPredicate implements SecureObjectClassPredicate {
+public final class DefaultSecureObjectClassPredicate implements SecureObjectClassPredicate {
+    private Predicate<String> classNameMatcher = Functions.booleanPredicate(true);
+
+    public DefaultSecureObjectClassPredicate() {
+    }
+
+    public DefaultSecureObjectClassPredicate(Predicate<String> matcher) {
+        this.classNameMatcher = matcher;
+    }
+
+
     @Override
-    public boolean test(ObjectStreamClass objectStreamClass) {
-        return false;
+    public boolean test(ObjectStreamClass osc) {
+        return classNameMatcher.test(osc.getName());
     }
 }
