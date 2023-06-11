14c5586bf9bd823560209d2e611fd93df1074311
fjn <1194361820@qq.com>
2023-06-12 07:21:53 +0800

    * 处理流关闭

--- langx-java/src/main/java/com/jn/langx/text/xml/resolver/DTDEntityResolver.java
+++ langx-java/src/main/java/com/jn/langx/text/xml/resolver/DTDEntityResolver.java
@@ -1,6 +1,9 @@
 package com.jn.langx.text.xml.resolver;
 
 import com.jn.langx.util.io.Charsets;
+import com.jn.langx.util.io.IOs;
+import com.jn.langx.util.logging.Loggers;
+import org.slf4j.Logger;
 import org.xml.sax.EntityResolver;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
@@ -8,10 +11,11 @@ import org.xml.sax.SAXException;
 import java.io.*;
 
 public class DTDEntityResolver implements EntityResolver {
-    private InputStream dtdInputStream;
+    private static final Logger logger = Loggers.getLogger(DTDEntityResolver.class);
+    private StringReader dtdReader;
 
     public DTDEntityResolver(final InputStream dtdInputStream) {
-        this.dtdInputStream = dtdInputStream;
+        setDtdInputStream(dtdInputStream);
     }
 
     @Override
@@ -25,16 +29,29 @@ public class DTDEntityResolver implements EntityResolver {
                 file = new File(publicId);
             }
             if (file != null && file.exists()) {
-                source = new InputSource(new FileInputStream(file));
+                FileInputStream fis = new FileInputStream(file);
+                setDtdInputStream(fis);
             }
-            if (source == null) {
-                if (this.dtdInputStream != null) {
-                    return new InputSource(this.dtdInputStream);
-                }
-                source = new InputSource(new ByteArrayInputStream("<?xml version=\"1.0\" encoding=\"UTF-8\"?>".getBytes(Charsets.UTF_8)));
+            if (this.dtdReader != null) {
+                return new InputSource(this.dtdReader);
             }
+            source = new InputSource(new ByteArrayInputStream("<?xml version=\"1.0\" encoding=\"UTF-8\"?>".getBytes(Charsets.UTF_8)));
         } catch (Exception ex) {
         }
         return source;
+    }
+
+    void setDtdInputStream(final InputStream dtdInputStream) {
+        String content = null;
+        try {
+            content = IOs.readAsString(dtdInputStream);
+            StringReader reader = new StringReader(content);
+            this.dtdReader = reader;
+        } catch (Throwable e) {
+            logger.error(e.getMessage(), e);
+        } finally {
+            IOs.close(dtdInputStream);
+        }
+
     }
 }
