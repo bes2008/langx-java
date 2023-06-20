d24a4691d834a45a5515bddd18ae884e68cb13df
fjn1194361820 <fs1194361820@163.com>
2023-06-20 14:22:56 +0800

    * 支持质数快速查找

--- langx-java/src/main/java/com/jn/langx/util/Maths.java
+++ langx-java/src/main/java/com/jn/langx/util/Maths.java
@@ -2,6 +2,7 @@ package com.jn.langx.util;
 
 import com.jn.langx.util.collection.Arrs;
 import com.jn.langx.util.collection.Collects;
+import com.jn.langx.util.collection.Lists;
 import com.jn.langx.util.collection.Pipeline;
 import com.jn.langx.util.comparator.ComparableComparator;
 import com.jn.langx.util.function.Consumer;
@@ -10,11 +11,13 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
+import java.util.List;
 
 public class Maths {
-    private Maths(){
+    private Maths() {
 
     }
+
     /**
      * 求最大值
      */
@@ -210,7 +213,6 @@ public class Maths {
 
     /**
      * 判断是否为 2的 power 值
-     *
      */
     public static boolean isPower2(int value) {
         return value >= 1 && ((value & (value - 1)) == 0);
@@ -445,28 +447,28 @@ public class Maths {
         }
     }
 
-    public static int clz32(int a){
+    public static int clz32(int a) {
         return clzN(a, 32);
     }
 
-    public static int clzN(int a, int n){
-        String binaryText =Radixs.toBinary(a);
+    public static int clzN(int a, int n) {
+        String binaryText = Radixs.toBinary(a);
         int clz = 0;
-        if(binaryText.length()<n){
+        if (binaryText.length() < n) {
             clz = n - binaryText.length();
             clz = Maths.max(0, clz);
         }
-        char[] cs=binaryText.toCharArray();
+        char[] cs = binaryText.toCharArray();
         int delta = 0;
-        for (int i = 0; i < cs.length; i++){
+        for (int i = 0; i < cs.length; i++) {
             char c = cs[i];
-            if(c != '0'){
+            if (c != '0') {
                 break;
-            }else{
-                delta = i+1;
+            } else {
+                delta = i + 1;
             }
         }
-        return clz+delta;
+        return clz + delta;
     }
 
     /**
@@ -475,7 +477,6 @@ public class Maths {
      * @param v1 被乘数
      * @param v2 乘数
      * @return 积
-     *
      * @since 4.6.13
      */
     public static double mul(float v1, float v2) {
@@ -488,9 +489,7 @@ public class Maths {
      * @param v1 被乘数
      * @param v2 乘数
      * @return 积
-     *
      * @since 4.6.13
-     *
      */
     public static double mul(float v1, double v2) {
         return mul(Float.toString(v1), Double.toString(v2)).doubleValue();
@@ -502,7 +501,6 @@ public class Maths {
      * @param v1 被乘数
      * @param v2 乘数
      * @return 积
-     *
      * @since 4.6.13
      */
     public static double mul(double v1, float v2) {
@@ -515,7 +513,6 @@ public class Maths {
      * @param v1 被乘数
      * @param v2 乘数
      * @return 积
-     *
      * @since 4.6.13
      */
     public static double mul(double v1, double v2) {
@@ -529,7 +526,6 @@ public class Maths {
      * @param v1 被乘数
      * @param v2 乘数
      * @return 积
-     *
      * @since 4.6.13
      */
     public static double mul(Double v1, Double v2) {
@@ -544,7 +540,6 @@ public class Maths {
      * @param v1 被乘数
      * @param v2 乘数
      * @return 积
-     *
      * @since 4.6.13
      */
     public static BigDecimal mul(Number v1, Number v2) {
@@ -579,7 +574,6 @@ public class Maths {
      * @param v1 被乘数
      * @param v2 乘数
      * @return 积
-     *
      * @since 4.6.13
      */
     public static BigDecimal mul(String v1, String v2) {
@@ -592,7 +586,6 @@ public class Maths {
      *
      * @param values 多个被乘值
      * @return 积
-     *
      * @since 4.6.13
      */
     public static BigDecimal mul(String... values) {
@@ -614,7 +607,6 @@ public class Maths {
      *
      * @param values 多个被乘值
      * @return 积
-     *
      * @since 4.6.13
      */
     public static BigDecimal mul(BigDecimal... values) {
@@ -631,6 +623,7 @@ public class Maths {
 
     /**
      * 默认除法运算精度
+     *
      * @since 4.6.13
      */
     private static final int DEFAULT_DIV_SCALE = 10;
@@ -641,7 +634,6 @@ public class Maths {
      * @param v1 被除数
      * @param v2 除数
      * @return 两个参数的商
-     *
      * @since 4.6.13
      */
     public static double div(float v1, float v2) {
@@ -654,7 +646,6 @@ public class Maths {
      * @param v1 被除数
      * @param v2 除数
      * @return 两个参数的商
-     *
      * @since 4.6.13
      */
     public static double div(float v1, double v2) {
@@ -667,7 +658,6 @@ public class Maths {
      * @param v1 被除数
      * @param v2 除数
      * @return 两个参数的商
-     *
      * @since 4.6.13
      */
     public static double div(double v1, float v2) {
@@ -680,7 +670,6 @@ public class Maths {
      * @param v1 被除数
      * @param v2 除数
      * @return 两个参数的商
-     *
      * @since 4.6.13
      */
     public static double div(double v1, double v2) {
@@ -693,7 +682,6 @@ public class Maths {
      * @param v1 被除数
      * @param v2 除数
      * @return 两个参数的商
-     *
      * @since 4.6.13
      */
     public static double div(Double v1, Double v2) {
@@ -706,8 +694,6 @@ public class Maths {
      * @param v1 被除数
      * @param v2 除数
      * @return 两个参数的商
-     *
-     *
      * @since 4.6.13
      */
     public static BigDecimal div(Number v1, Number v2) {
@@ -720,7 +706,6 @@ public class Maths {
      * @param v1 被除数
      * @param v2 除数
      * @return 两个参数的商
-     *
      * @since 4.6.13
      */
     public static BigDecimal div(String v1, String v2) {
@@ -734,7 +719,6 @@ public class Maths {
      * @param v2    除数
      * @param scale 精确度，如果为负值，取绝对值
      * @return 两个参数的商
-     *
      * @since 4.6.13
      */
     public static double div(float v1, float v2, int scale) {
@@ -748,7 +732,6 @@ public class Maths {
      * @param v2    除数
      * @param scale 精确度，如果为负值，取绝对值
      * @return 两个参数的商
-     *
      * @since 4.6.13
      */
     public static double div(float v1, double v2, int scale) {
@@ -762,7 +745,6 @@ public class Maths {
      * @param v2    除数
      * @param scale 精确度，如果为负值，取绝对值
      * @return 两个参数的商
-     *
      * @since 4.6.13
      */
     public static double div(double v1, float v2, int scale) {
@@ -776,7 +758,6 @@ public class Maths {
      * @param v2    除数
      * @param scale 精确度，如果为负值，取绝对值
      * @return 两个参数的商
-     *
      * @since 4.6.13
      */
     public static double div(double v1, double v2, int scale) {
@@ -790,7 +771,6 @@ public class Maths {
      * @param v2    除数
      * @param scale 精确度，如果为负值，取绝对值
      * @return 两个参数的商
-     *
      * @since 4.6.13
      */
     public static double div(Double v1, Double v2, int scale) {
@@ -804,7 +784,6 @@ public class Maths {
      * @param v2    除数
      * @param scale 精确度，如果为负值，取绝对值
      * @return 两个参数的商
-     *
      * @since 4.6.13
      */
     public static BigDecimal div(Number v1, Number v2, int scale) {
@@ -818,7 +797,6 @@ public class Maths {
      * @param v2    除数
      * @param scale 精确度，如果为负值，取绝对值
      * @return 两个参数的商
-     *
      * @since 4.6.13
      */
     public static BigDecimal div(String v1, String v2, int scale) {
@@ -833,7 +811,6 @@ public class Maths {
      * @param scale        精确度，如果为负值，取绝对值
      * @param roundingMode 保留小数的模式 {@link RoundingMode}
      * @return 两个参数的商
-     *
      * @since 4.6.13
      */
     public static double div(float v1, float v2, int scale, RoundingMode roundingMode) {
@@ -848,7 +825,6 @@ public class Maths {
      * @param scale        精确度，如果为负值，取绝对值
      * @param roundingMode 保留小数的模式 {@link RoundingMode}
      * @return 两个参数的商
-     *
      * @since 4.6.13
      */
     public static double div(float v1, double v2, int scale, RoundingMode roundingMode) {
@@ -863,7 +839,6 @@ public class Maths {
      * @param scale        精确度，如果为负值，取绝对值
      * @param roundingMode 保留小数的模式 {@link RoundingMode}
      * @return 两个参数的商
-     *
      * @since 4.6.13
      */
     public static double div(double v1, float v2, int scale, RoundingMode roundingMode) {
@@ -878,7 +853,6 @@ public class Maths {
      * @param scale        精确度，如果为负值，取绝对值
      * @param roundingMode 保留小数的模式 {@link RoundingMode}
      * @return 两个参数的商
-     *
      * @since 4.6.13
      */
     public static double div(double v1, double v2, int scale, RoundingMode roundingMode) {
@@ -893,7 +867,6 @@ public class Maths {
      * @param scale        精确度，如果为负值，取绝对值
      * @param roundingMode 保留小数的模式 {@link RoundingMode}
      * @return 两个参数的商
-     *
      * @since 4.6.13
      */
     public static double div(Double v1, Double v2, int scale, RoundingMode roundingMode) {
@@ -909,7 +882,6 @@ public class Maths {
      * @param scale        精确度，如果为负值，取绝对值
      * @param roundingMode 保留小数的模式 {@link RoundingMode}
      * @return 两个参数的商
-     *
      * @since 4.6.13
      */
     public static BigDecimal div(Number v1, Number v2, int scale, RoundingMode roundingMode) {
@@ -927,7 +899,6 @@ public class Maths {
      * @param scale        精确度，如果为负值，取绝对值
      * @param roundingMode 保留小数的模式 {@link RoundingMode}
      * @return 两个参数的商
-     *
      * @since 4.6.13
      */
     public static BigDecimal div(String v1, String v2, int scale, RoundingMode roundingMode) {
@@ -942,8 +913,6 @@ public class Maths {
      * @param scale        精确度，如果为负值，取绝对值
      * @param roundingMode 保留小数的模式 {@link RoundingMode}
      * @return 两个参数的商
-     *
-     *
      * @since 4.6.13
      */
     public static BigDecimal div(BigDecimal v1, BigDecimal v2, int scale, RoundingMode roundingMode) {
@@ -955,5 +924,80 @@ public class Maths {
             scale = -scale;
         }
         return v1.divide(v2, scale, roundingMode);
+    }
+
+    private final static List<Integer> primes = Lists.newArrayList(2, 3, 5, 7);
+
+    /**
+     * 判断一个数是不为 质数
+     * <p>
+     * 不，1不是质数。质数是指大于1且只能被1和本身整除的自然数。而1只能被1整除，不能被其他任何自然数整除，因此不符合质数的定义。按照常规的定义，质数应该是大于1的自然数。
+     */
+    public static boolean isPrimeNumber(int num) {
+        if (num < 2) {
+            return false;
+        }
+        if (num > 7) {
+            // 能够被 2,3,5,7 中任何一个整除的数，一定不是质数
+            // 不能够被 2,3,5,7 中任何一个整除的数，一定是质数
+            for (Integer p : primes) {
+                if (num % p == 0) {
+                    return false;
+                }
+            }
+        } else {
+            return primes.contains(num);
+        }
+        return true;
+    }
+
+    /**
+     * 找出指定范围内的所有质数 [start, end)
+     */
+    public static List<Integer> findPrimes(int start, int end) {
+        if (start < 2) {
+            start = 2;
+        }
+        if (end < 2) {
+            end = 2;
+        }
+        if (start > end) {
+            int tmp = start;
+            start = end;
+            end = tmp;
+        }
+        List<Integer> ret = Lists.newArrayList();
+        int prime = -1;
+        // 找到第一个prime
+        while (prime < 2 && start < end) {
+            if (isPrimeNumber(start)) {
+                prime = start;
+                ret.add(prime);
+                break;
+            } else {
+                start++;
+            }
+        }
+        if (prime < 2 || start >= end) {
+            return ret;
+        }
+        if (prime == 2 && end > 3) {
+            prime = 3;
+            ret.add(prime);
+        }
+        int next = prime;
+        out:
+        while (next < end) {
+            for (int i = 2; i <= 7; i++) {
+                int candidate = next + i;
+                if (candidate < end && isPrimeNumber(candidate)) {
+                    next = candidate;
+                    ret.add(next);
+                    continue out;
+                }
+            }
+            next = next + 2;
+        }
+        return ret;
     }
 }
--- langx-java/src/test/java/com/jn/langx/test/util/MathsTests.java
+++ langx-java/src/test/java/com/jn/langx/test/util/MathsTests.java
@@ -1,7 +1,12 @@
 package com.jn.langx.test.util;
 
+import com.jn.langx.text.StringTemplates;
+import com.jn.langx.util.Dates;
 import com.jn.langx.util.Maths;
+import com.jn.langx.util.collection.Lists;
 import org.junit.Test;
+
+import java.util.List;
 
 public class MathsTests {
     @Test
@@ -21,5 +26,29 @@ public class MathsTests {
         System.out.println(Maths.formatPrecision(123123.2352343234234d, 2));
         System.out.println(Maths.formatPrecision(123123.2362343234234d, 2));
         System.out.println(Maths.formatPrecision(123123.2392343234234d, 2));
+    }
+
+    @Test
+    public void testIsPrime() {
+        long startTime = Dates.now().getTime();
+        List<Integer> primes = Lists.newArrayList();
+        for (int i = 1; i <= 100; i++) {
+            boolean isPrime = Maths.isPrimeNumber(i);
+            if (isPrime) {
+                primes.add(i);
+            }
+        }
+        long endTime = Dates.now().getTime();
+        System.out.println(primes);
+        System.out.println(StringTemplates.formatWithPlaceholder("cost: {} ms", endTime - startTime));
+    }
+
+    @Test
+    public void testFindPrimes() {
+        long startTime = Dates.now().getTime();
+        List<Integer> primes = Maths.findPrimes(0, 101);
+        long endTime = Dates.now().getTime();
+        System.out.println(primes);
+        System.out.println(StringTemplates.formatWithPlaceholder("cost: {} ms", endTime - startTime));
     }
 }
