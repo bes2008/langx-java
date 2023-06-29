e3e9d9824c65364f2040f17a0c923ccbdbef8dc4
fjn1194361820 <fs1194361820@163.com>
2023-06-29 15:25:08 +0800

    * 代码简化

--- langx-java/src/main/java/com/jn/langx/util/timing/cron/CronExpression.java
+++ langx-java/src/main/java/com/jn/langx/util/timing/cron/CronExpression.java
@@ -1,23 +1,31 @@
 package com.jn.langx.util.timing.cron;
 
-
 import com.jn.langx.text.StrTokenizer;
 import com.jn.langx.util.Objs;
 
 import java.io.Serializable;
 import java.text.ParseException;
-import java.util.*;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Locale;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TimeZone;
+import java.util.TreeSet;
 
 /**
  * Provides a parser and evaluator for unix-like cron expressions. Cron
  * expressions provide the ability to specify complex time combinations such as
  * &quot;At 8:00am every Monday through Friday&quot; or &quot;At 1:30am every
  * last Friday of the month&quot;.
- * <P>
+ * <p>
  * Cron expressions are comprised of 6 required fields and one optional field
  * separated by white space. The fields respectively are described as follows:
- *
- * <table cellspacing="8">
+ * </p>
+ * <table>
+ * <caption>Examples of cron expressions and their meanings.</caption>
  * <tr>
  * <th align="left">Field Name</th>
  * <th align="left">&nbsp;</th>
@@ -75,21 +83,23 @@ import java.util.*;
  * <td align="left"><code>, - * /</code></td>
  * </tr>
  * </table>
- * <P>
+ * <p>
  * The '*' character is used to specify all values. For example, &quot;*&quot;
  * in the minute field means &quot;every minute&quot;.
- * <P>
+ * </p>
+ * <p>
  * The '?' character is allowed for the day-of-month and day-of-week fields. It
  * is used to specify 'no specific value'. This is useful when you need to
  * specify something in one of the two fields, but not the other.
- * <P>
+ * <p>
  * The '-' character is used to specify ranges For example &quot;10-12&quot; in
  * the hour field means &quot;the hours 10, 11 and 12&quot;.
- * <P>
+ * <p>
  * The ',' character is used to specify additional values. For example
  * &quot;MON,WED,FRI&quot; in the day-of-week field means &quot;the days Monday,
  * Wednesday, and Friday&quot;.
- * <P>
+ * </p>
+ * <p>
  * The '/' character is used to specify increments. For example &quot;0/15&quot;
  * in the seconds field means &quot;the seconds 0, 15, 30, and 45&quot;. And
  * &quot;5/15&quot; in the seconds field means &quot;the seconds 5, 20, 35, and
@@ -101,7 +111,8 @@ import java.util.*;
  * on every &quot;nth&quot; value in the given set. Thus &quot;7/6&quot; in the
  * month field only turns on month &quot;7&quot;, it does NOT mean every 6th
  * month, please note that subtlety.
- * <P>
+ * </p>
+ * <p>
  * The 'L' character is allowed for the day-of-month and day-of-week fields.
  * This character is short-hand for &quot;last&quot;, but it has different
  * meaning in each of the two fields. For example, the value &quot;L&quot; in
@@ -114,7 +125,8 @@ import java.util.*;
  * from the last day of the month, such as "L-3" which would mean the third-to-last
  * day of the calendar month. <i>When using the 'L' option, it is important not to
  * specify lists, or ranges of values, as you'll get confusing/unexpected results.</i>
- * <P>
+ * </p>
+ * <p>
  * The 'W' character is allowed for the day-of-month field.  This character
  * is used to specify the weekday (Monday-Friday) nearest the given day.  As an
  * example, if you were to specify &quot;15W&quot; as the value for the
@@ -126,11 +138,13 @@ import java.util.*;
  * 1st is a Saturday, the trigger will fire on Monday the 3rd, as it will not
  * 'jump' over the boundary of a month's days.  The 'W' character can only be
  * specified when the day-of-month is a single day, not a range or list of days.
- * <P>
+ * </p>
+ * <p>
  * The 'L' and 'W' characters can also be combined for the day-of-month
  * expression to yield 'LW', which translates to &quot;last weekday of the
  * month&quot;.
- * <P>
+ * </p>
+ * <p>
  * The '#' character is allowed for the day-of-week field. This character is
  * used to specify &quot;the nth&quot; XXX day of the month. For example, the
  * value of &quot;6#3&quot; in the day-of-week field means the third Friday of
@@ -141,7 +155,7 @@ import java.util.*;
  * no firing will occur that month.  If the '#' character is used, there can
  * only be one expression in the day-of-week field (&quot;3#1,6#3&quot; is
  * not valid, since there are two expressions).
- * <P>
+ * </p>
  * <!--The 'C' character is allowed for the day-of-month and day-of-week fields.
  * This character is short-hand for "calendar". This means values are
  * calculated against the associated calendar, if any. If no calendar is
@@ -149,12 +163,13 @@ import java.util.*;
  * value of "5C" in the day-of-month field means "the first day included by the
  * calendar on or after the 5th". A value of "1C" in the day-of-week field
  * means "the first day included by the calendar on or after Sunday".-->
- * <P>
+ * <p>
  * The legal characters and the names of months and days of the week are not
  * case sensitive.
  *
  * <p>
  * <b>NOTES:</b>
+ * </p>
  * <ul>
  * <li>Support for specifying both a day-of-week and a day-of-month value is
  * not complete (you'll need to use the '?' character in one of these fields).
@@ -186,34 +201,30 @@ public final class CronExpression implements Serializable, Cloneable {
     private static final Integer ALL_SPEC = ALL_SPEC_INT;
     private static final Integer NO_SPEC = NO_SPEC_INT;
 
-    private static final Map<String, Integer> monthMap;
-    private static final Map<String, Integer> dayMap;
+    private static final Map<String, Integer> monthMap = new HashMap<String, Integer>(20);
+    private static final Map<String, Integer> dayMap = new HashMap<String, Integer>(60);
 
     static {
-        Map<String, Integer> monthmap = new HashMap<String, Integer>(12);
-        monthmap.put("JAN", 0);
-        monthmap.put("FEB", 1);
-        monthmap.put("MAR", 2);
-        monthmap.put("APR", 3);
-        monthmap.put("MAY", 4);
-        monthmap.put("JUN", 5);
-        monthmap.put("JUL", 6);
-        monthmap.put("AUG", 7);
-        monthmap.put("SEP", 8);
-        monthmap.put("OCT", 9);
-        monthmap.put("NOV", 10);
-        monthmap.put("DEC", 11);
-        monthMap = Collections.unmodifiableMap(monthmap);
+        monthMap.put("JAN", 0);
+        monthMap.put("FEB", 1);
+        monthMap.put("MAR", 2);
+        monthMap.put("APR", 3);
+        monthMap.put("MAY", 4);
+        monthMap.put("JUN", 5);
+        monthMap.put("JUL", 6);
+        monthMap.put("AUG", 7);
+        monthMap.put("SEP", 8);
+        monthMap.put("OCT", 9);
+        monthMap.put("NOV", 10);
+        monthMap.put("DEC", 11);
 
-        Map<String, Integer> daymap = new HashMap<String, Integer>(7);
-        daymap.put("SUN", 1);
-        daymap.put("MON", 2);
-        daymap.put("TUE", 3);
-        daymap.put("WED", 4);
-        daymap.put("THU", 5);
-        daymap.put("FRI", 6);
-        daymap.put("SAT", 7);
-        dayMap = Collections.unmodifiableMap(daymap);
+        dayMap.put("SUN", 1);
+        dayMap.put("MON", 2);
+        dayMap.put("TUE", 3);
+        dayMap.put("WED", 4);
+        dayMap.put("THU", 5);
+        dayMap.put("FRI", 6);
+        dayMap.put("SAT", 7);
     }
 
     private final String cronExpression;
@@ -270,7 +281,7 @@ public final class CronExpression implements Serializable, Cloneable {
         try {
             buildExpression(cronExpression);
         } catch (ParseException ex) {
-            throw new AssertionError();
+            throw new AssertionError(ex);
         }
         if (expression.getTimeZone() != null) {
             setTimeZone((TimeZone) expression.getTimeZone().clone());
@@ -837,42 +848,43 @@ public final class CronExpression implements Serializable, Cloneable {
     }
 
     public String getExpressionSummary() {
+        StringBuilder buf = new StringBuilder();
 
-        String buf = "seconds: " +
-                getExpressionSetSummary(seconds) +
-                "\n" +
-                "minutes: " +
-                getExpressionSetSummary(minutes) +
-                "\n" +
-                "hours: " +
-                getExpressionSetSummary(hours) +
-                "\n" +
-                "daysOfMonth: " +
-                getExpressionSetSummary(daysOfMonth) +
-                "\n" +
-                "months: " +
-                getExpressionSetSummary(months) +
-                "\n" +
-                "daysOfWeek: " +
-                getExpressionSetSummary(daysOfWeek) +
-                "\n" +
-                "lastdayOfWeek: " +
-                lastdayOfWeek +
-                "\n" +
-                "nearestWeekday: " +
-                nearestWeekday +
-                "\n" +
-                "NthDayOfWeek: " +
-                nthdayOfWeek +
-                "\n" +
-                "lastdayOfMonth: " +
-                lastdayOfMonth +
-                "\n" +
-                "years: " +
-                getExpressionSetSummary(years) +
-                "\n";
+        buf.append("seconds: ");
+        buf.append(getExpressionSetSummary(seconds));
+        buf.append("\n");
+        buf.append("minutes: ");
+        buf.append(getExpressionSetSummary(minutes));
+        buf.append("\n");
+        buf.append("hours: ");
+        buf.append(getExpressionSetSummary(hours));
+        buf.append("\n");
+        buf.append("daysOfMonth: ");
+        buf.append(getExpressionSetSummary(daysOfMonth));
+        buf.append("\n");
+        buf.append("months: ");
+        buf.append(getExpressionSetSummary(months));
+        buf.append("\n");
+        buf.append("daysOfWeek: ");
+        buf.append(getExpressionSetSummary(daysOfWeek));
+        buf.append("\n");
+        buf.append("lastdayOfWeek: ");
+        buf.append(lastdayOfWeek);
+        buf.append("\n");
+        buf.append("nearestWeekday: ");
+        buf.append(nearestWeekday);
+        buf.append("\n");
+        buf.append("NthDayOfWeek: ");
+        buf.append(nthdayOfWeek);
+        buf.append("\n");
+        buf.append("lastdayOfMonth: ");
+        buf.append(lastdayOfMonth);
+        buf.append("\n");
+        buf.append("years: ");
+        buf.append(getExpressionSetSummary(years));
+        buf.append("\n");
 
-        return buf;
+        return buf.toString();
     }
 
     String getExpressionSetSummary(java.util.Set<Integer> set) {
@@ -1581,7 +1593,7 @@ public final class CronExpression implements Serializable, Cloneable {
      * that the <code>CronExpression</code> matches.
      */
     public Date getTimeBefore(Date endTime) {
-        //  implement QUARTZ-423
+        // implement QUARTZ-423
         return null;
     }
 
@@ -1590,7 +1602,7 @@ public final class CronExpression implements Serializable, Cloneable {
      * <code>CronExpression</code> will match.
      */
     public Date getFinalFireTime() {
-        //  implement QUARTZ-423
+        // implement QUARTZ-423
         return null;
     }
 
