9950875d49aeab3b35dc97ca94e73de93cbfc1ff
fjn1194361820 <fs1194361820@163.com>
2024-03-04 15:42:24 +0800

    * 代码整理

--- langx-java-security-gm/langx-java-security-gm-jca-bouncycastle/src/main/java/com/jn/langx/security/gm/crypto/bc/asymmetric/sm2/SM2KeyPairGeneratorSpi.java
+++ langx-java-security-gm/langx-java-security-gm-jca-bouncycastle/src/main/java/com/jn/langx/security/gm/crypto/bc/asymmetric/sm2/SM2KeyPairGeneratorSpi.java
@@ -2,8 +2,19 @@ package com.jn.langx.security.gm.crypto.bc.asymmetric.sm2;
 
 import org.bouncycastle.jcajce.provider.asymmetric.ec.KeyPairGeneratorSpi;
 
+import java.security.spec.ECGenParameterSpec;
+
 /**
  * SM2 用的就是 EC的 key pair
  */
 public class SM2KeyPairGeneratorSpi extends KeyPairGeneratorSpi.EC {
+
+    SM2KeyPairGeneratorSpi(){
+        super();
+        try {
+            initialize(new ECGenParameterSpec("sm2p256v1"));
+        }catch (Exception e){
+            // ignore it
+        }
+    }
 }
