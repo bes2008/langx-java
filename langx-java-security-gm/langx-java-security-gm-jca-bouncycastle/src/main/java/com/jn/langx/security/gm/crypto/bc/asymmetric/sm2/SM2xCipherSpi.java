6d3077d28b56242356b85eb7c2f03971427631e3
fjn1194361820 <fs1194361820@163.com>
2024-03-04 18:32:40 +0800

    * 代码整理

--- langx-java-security-gm/langx-java-security-gm-jca-bouncycastle/src/main/java/com/jn/langx/security/gm/crypto/bc/asymmetric/sm2/SM2xCipherSpi.java
+++ langx-java-security-gm/langx-java-security-gm-jca-bouncycastle/src/main/java/com/jn/langx/security/gm/crypto/bc/asymmetric/sm2/SM2xCipherSpi.java
@@ -18,67 +18,67 @@ public class SM2xCipherSpi extends GMCipherSpi {
     /**
      * Classes that inherit from us
      */
-    static public class SM2withSm3 extends SM2xCipherSpi {
+    public static class SM2withSm3 extends SM2xCipherSpi {
         public SM2withSm3() {
             super(new SM3Digest());
         }
     }
 
-    static public class SM2withBlake2b extends SM2xCipherSpi {
+    public static class SM2withBlake2b extends SM2xCipherSpi {
         public SM2withBlake2b() {
             super(new Blake2bDigest(512));
         }
     }
 
-    static public class SM2withBlake2s extends SM2xCipherSpi {
+    public static class SM2withBlake2s extends SM2xCipherSpi {
         public SM2withBlake2s() {
             super(new Blake2sDigest(256));
         }
     }
 
-    static public class SM2withWhirlpool extends SM2xCipherSpi {
+    public static class SM2withWhirlpool extends SM2xCipherSpi {
         public SM2withWhirlpool() {
             super(new WhirlpoolDigest());
         }
     }
 
-    static public class SM2withMD5 extends SM2xCipherSpi {
+    public static class SM2withMD5 extends SM2xCipherSpi {
         public SM2withMD5() {
             super(new MD5Digest());
         }
     }
 
-    static public class SM2withRMD extends SM2xCipherSpi {
+    public static class SM2withRMD extends SM2xCipherSpi {
         public SM2withRMD() {
             super(new RIPEMD160Digest());
         }
     }
 
-    static public class SM2withSha1 extends SM2xCipherSpi {
+    public static class SM2withSha1 extends SM2xCipherSpi {
         public SM2withSha1() {
             super(new SHA1Digest());
         }
     }
 
-    static public class SM2withSha224 extends SM2xCipherSpi {
+    public static class SM2withSha224 extends SM2xCipherSpi {
         public SM2withSha224() {
             super(new SHA224Digest());
         }
     }
 
-    static public class SM2withSha256 extends SM2xCipherSpi {
+    public static class SM2withSha256 extends SM2xCipherSpi {
         public SM2withSha256() {
             super(new SHA256Digest());
         }
     }
 
-    static public class SM2withSha384 extends SM2xCipherSpi {
+    public static class SM2withSha384 extends SM2xCipherSpi {
         public SM2withSha384() {
             super(new SHA384Digest());
         }
     }
 
-    static public class SM2withSha512 extends SM2xCipherSpi {
+    public static class SM2withSha512 extends SM2xCipherSpi {
         public SM2withSha512() {
             super(new SHA512Digest());
         }
