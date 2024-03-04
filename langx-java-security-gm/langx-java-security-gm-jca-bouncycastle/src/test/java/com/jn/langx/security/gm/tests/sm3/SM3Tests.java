e4c046ef0945068b580e3adf93bcb5c7b64b1757
fjn1194361820 <fs1194361820@163.com>
2024-03-04 15:58:57 +0800

    * 测试用例整理

--- langx-java-security-gm/langx-java-security-gm-jca-bouncycastle/src/test/java/com/jn/langx/security/gm/tests/sm3/SM3Tests.java
+++
@@ -1,8 +0,0 @@
-package com.jn.langx.security.gm.tests.sm3;
-
-public class SM3Tests {
-    public void test(){
-
-    }
-
-}
--- langx-java-security-gm/langx-java-security-gm-jca-tests/src/test/java/com/jn/langx/security/gm/tests/vshutool/SM3Tests.java
+++ langx-java-security-gm/langx-java-security-gm-jca-tests/src/test/java/com/jn/langx/security/gm/tests/vshutool/SM3Tests.java
@@ -1,6 +1,8 @@
 package com.jn.langx.security.gm.tests.vshutool;
 
+import cn.hutool.crypto.SmUtil;
 import com.jn.langx.security.crypto.digest.MessageDigests;
+import com.jn.langx.util.Objs;
 import org.junit.Test;
 
 public class SM3Tests {
@@ -10,7 +12,13 @@ public class SM3Tests {
         String content ="hello, 对比 langx-java 的 sm3, 与 hutool 的SM3结果是否一致";
 
         String digest1 = MessageDigests.getDigestHexString("SM3",content);
+
+        String digest2 = SmUtil.sm3(content);
+
         System.out.println(digest1);
+        System.out.println(digest2);
+
+        System.out.println(Objs.equals(digest1, digest2));
     }
 
 
