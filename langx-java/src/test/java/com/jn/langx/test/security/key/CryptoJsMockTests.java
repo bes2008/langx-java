d2b59efe0f1cf14b85c5e7e5dbaf2187dfe8a59d
fjn1194361820 <fs1194361820@163.com>
2024-04-26 15:07:51 +0800

    * 校正hash算法 crypto-js 中的AES算法

--- langx-java/src/test/java/com/jn/langx/test/security/key/CryptoJsMockTests.java
+++ langx-java/src/test/java/com/jn/langx/test/security/key/CryptoJsMockTests.java
@@ -2,7 +2,10 @@ package com.jn.langx.test.security.key;
 
 import com.jn.langx.security.ext.cryptojs.CryptoJS;
 import com.jn.langx.util.Objs;
+import com.jn.langx.util.collection.Lists;
 import org.junit.Test;
+
+import java.util.List;
 
 public class CryptoJsMockTests {
     @Test
@@ -17,9 +20,15 @@ public class CryptoJsMockTests {
         System.out.println(decryptedText);
         System.out.println(Objs.equals(message,decryptedText));
 
-        encryptedText="U2FsdGVkX18vE/xKFEjExOkwa5LGi23wj5ZXXR5zNJs=";
-        decryptedText = CryptoJS.AES.decrypt(encryptedText, passphrase,null);
-        System.out.println(decryptedText);
-        System.out.println(Objs.equals(message,decryptedText));
+        System.out.println("==========接下来的测试是用 node.js 使用 crypto-js 生成的加密数据 ==========");
+        List<String> encryptedTexts= Lists.newArrayList(
+                "U2FsdGVkX18vE/xKFEjExOkwa5LGi23wj5ZXXR5zNJs=",
+                "U2FsdGVkX1+EkiwP7zttFvziuAOM0MDoPYvKcIxvz3w="
+        );
+        for (String cipherText: encryptedTexts ){
+            decryptedText = CryptoJS.AES.decrypt(encryptedText, passphrase,null);
+            System.out.println(decryptedText);
+            System.out.println(Objs.equals(message,decryptedText));
+        }
     }
 }
