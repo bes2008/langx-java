f8ffebcaca1f3125f749477ab201c06d0d6c1a86
fjn1194361820 <fs1194361820@163.com>
2024-05-06 20:49:33 +0800

    * 模拟django的CSRF算法

---
+++ langx-java/src/test/java/com/jn/langx/test/security/pbe/DjangoCsrfTokenTests.java
@@ -0,0 +1,14 @@
+package com.jn.langx.test.security.pbe;
+
+import com.jn.langx.security.crypto.pbe.pswdenc.DjangoCsrfTokenEncryptor;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DjangoCsrfTokenTests {
+    @Test
+    public void test(){
+       DjangoCsrfTokenEncryptor encryptor= new DjangoCsrfTokenEncryptor();
+       Assert.assertTrue(encryptor.check("EaOKub9Cbshwpsi8OwWFaWCRumqDuh2bU7NrCJqUPc60psgBi9fbuev6s4ZyY2EN",
+               "LGilu6JUAYPsB3KZw1KwODQIehJcoH5M1Dh2CE0ceIEWB3Is0E328VJXcZi7SsHo"));
+    }
+}
