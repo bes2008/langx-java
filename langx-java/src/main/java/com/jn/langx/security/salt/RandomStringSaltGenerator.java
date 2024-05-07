29552b7323f4088d967a9d6143b4aefbe463c9a1
fjn1194361820 <fs1194361820@163.com>
2024-05-07 15:20:59 +0800

    * 支持 salt

--- langx-java/src/main/java/com/jn/langx/security/salt/RandomStringSaltGenerator.java
+++ langx-java/src/main/java/com/jn/langx/security/salt/RandomStringSaltGenerator.java
@@ -1,10 +1,9 @@
 package com.jn.langx.security.salt;
 
-import com.jn.langx.util.Strings;
 import com.jn.langx.util.id.Nanoids;
 import com.jn.langx.util.random.Randoms;
 
-public class RandomStringSaltGenerator implements BytesSaltGenerator {
+public class RandomStringSaltGenerator implements StringSaltGenerator {
     private String alphabet = Nanoids.DEFAULT_ALPHABET;
     public RandomStringSaltGenerator(){
     }
@@ -14,8 +13,7 @@ public class RandomStringSaltGenerator implements BytesSaltGenerator {
     }
 
     @Override
-    public byte[] get(Integer bytesLength) {
-        String str= Randoms.randomString(alphabet,(bytesLength+1)/2);
-        return Strings.getBytesUtf8(str);
+    public String get(Integer bytesLength) {
+        return Randoms.randomString(alphabet,(bytesLength+1)/2);
     }
 }
