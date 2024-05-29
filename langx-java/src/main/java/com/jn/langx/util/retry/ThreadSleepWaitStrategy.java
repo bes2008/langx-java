094c078418cb50cea9e7e0baa0a1d6d8111b8cf1
fjn1194361820 <fs1194361820@163.com>
2024-05-29 20:49:14 +0800

    * 代码整理

--- langx-java/src/main/java/com/jn/langx/util/retry/ThreadSleepWaitStrategy.java
+++ langx-java/src/main/java/com/jn/langx/util/retry/ThreadSleepWaitStrategy.java
@@ -1,8 +1,19 @@
 package com.jn.langx.util.retry;
 
+import com.jn.langx.util.Maths;
+import com.jn.langx.util.Preconditions;
+
 public class ThreadSleepWaitStrategy implements WaitStrategy{
+    private long maxMills;
+    public ThreadSleepWaitStrategy(){
+        this(5*60*1000L);
+    }
+    public ThreadSleepWaitStrategy(long maxMills){
+        Preconditions.checkArgument(maxMills>=0);
+        this.maxMills = maxMills;
+    }
     @Override
     public void await(long mills) throws InterruptedException{
-        Thread.sleep(mills);
+        Thread.sleep(Maths.maxLong(0, Maths.minLong(mills, this.maxMills)));
     }
 }
