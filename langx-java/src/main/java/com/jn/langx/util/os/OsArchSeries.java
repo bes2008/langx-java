3e4ccaa45c11efd6eeb5cea1e089832839e9fd41
fjn1194361820 <fs1194361820@163.com>
2024-08-19 14:13:24 +0800

    * 更新 os-arch-serial

--- langx-java/src/main/java/com/jn/langx/util/os/OsArchSeries.java
+++ langx-java/src/main/java/com/jn/langx/util/os/OsArchSeries.java
@@ -1,17 +1,42 @@
 package com.jn.langx.util.os;
 
+/**
+ *  <pre>
+ *  CPU指令集：
+ *    RISC:
+ *      ARM
+ *      PowerPC
+ *      RISC-V
+ *      MIPS
+ *      SPARC
+ *      PA_RISC
+ *      SuperH （SH）
+ *      Alpha
+ *    CISC:
+ *      x86
+ *      Z
+ *      VAX
+ *    EPIC:
+ *      IA (Intel Itanium )
+ *
+ *  </pre>
+ */
 public enum OsArchSeries {
 
-    X86,
+    // 属于RISC的：
     ARM,
     POWER,
-    RISC,
+    RISC_V,
     MIPS,
-    Z,
-    SH,
-    IA,
+    SH, //SuperH
     SPARC,
     PA_RISC,
     ALPHA,
-    VAX;
+    // 属于 CISC的：
+    X86,
+    Z,
+    VAX,
+    // 属于 EPIC的：
+    IA,
+    ;
 }
