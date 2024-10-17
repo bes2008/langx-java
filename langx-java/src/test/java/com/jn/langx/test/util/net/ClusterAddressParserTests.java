7481416138dbd7d2822fe5ee8c1b0d82aa628b8b
fjn1194361820 <fs1194361820@163.com>
2024-10-17 14:52:05 +0800

    * 修改测试用例

--- langx-java/src/test/java/com/jn/langx/test/util/net/ClusterAddressParserTests.java
+++ langx-java/src/test/java/com/jn/langx/test/util/net/ClusterAddressParserTests.java
@@ -121,9 +121,11 @@ public class ClusterAddressParserTests {
 
     @Test
     public void zkClusterAddressTests(){
-        System.out.println(Regexps.match("^(\\w+)(-\\w+)*(\\.\\w+(-\\w+)*)*(:\\d{1,5})?","comp-zookeeper-0-0.comp-zookeeper-headless.besinsight.svc.cluster.local:2181"));
         String str="comp-zookeeper-0-0.comp-zookeeper-headless.besinsight.svc.cluster.local:2181,comp-zookeeper-1-0.comp-zookeeper-headless.besinsight.svc.cluster.local:2181,comp-zookeeper-2-0.comp-zookeeper-headless.besinsight.svc.cluster.local:2181";
-        String serverLists = new ClusterAddressParser(2181).normalize(str);
+        ClusterAddressParser parser = new ClusterAddressParser(2181);
+        List<NetworkAddress> addresses = parser.parse(str);
+        System.out.println(addresses);
+        String serverLists = parser.normalize(str);
         System.out.println(serverLists);
     }
 
