0fc7d2eec46de747ddc2f048fe353f44c41320b1
fjn1194361820 <fs1194361820@163.com>
2024-10-21 20:03:37 +0800

    * 整理代码

--- langx-java/src/main/java/com/jn/langx/management/connector/remote/JMXRemoteConnection.java
+++ langx-java/src/main/java/com/jn/langx/management/connector/remote/JMXRemoteConnection.java
@@ -1,6 +1,7 @@
 package com.jn.langx.management.connector.remote;
 
 import com.jn.langx.management.JMXConnection;
+import com.jn.langx.util.logging.Loggers;
 
 import javax.management.MBeanServerConnection;
 import javax.management.remote.JMXConnector;
@@ -21,7 +22,7 @@ public class JMXRemoteConnection extends JMXConnection {
             try {
                 this.connector.close();
             } catch (IOException e) {
-                e.printStackTrace();
+                Loggers.getLogger(JMXRemoteConnection.class).warn(e.getMessage(),e);
             }
         }
     }
