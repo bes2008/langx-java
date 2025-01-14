ecb03dfad0c83241182d207ab148c4a419449f11
fjn1194361820 <fs1194361820@163.com>
2025-01-14 16:07:16 +0800

    * 完全委托给其它来进行实现

--- langx-java/src/main/java/com/jn/langx/io/resource/PatternResourceLoader.java
+++ langx-java/src/main/java/com/jn/langx/io/resource/PatternResourceLoader.java
@@ -1,7 +1,11 @@
 package com.jn.langx.io.resource;
 
+import com.jn.langx.Ordered;
+
 import java.util.List;
 
-public interface PatternResourceLoader extends ResourceLoader {
+public interface PatternResourceLoader extends ResourceLoader , Ordered {
     List<Resource> getResources(String locationPattern);
+
+    boolean isSupportedPatttern(String locaionPattern);
 }
