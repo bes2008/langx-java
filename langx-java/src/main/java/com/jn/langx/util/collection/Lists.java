7d7a64139e4c6e2cf1007e9883bdf3a4602cb116
fjn1194361820 <fs1194361820@163.com>
2025-01-23 10:08:59 +0800

    * 支持初始数据的list

--- langx-java/src/main/java/com/jn/langx/util/collection/Lists.java
+++ langx-java/src/main/java/com/jn/langx/util/collection/Lists.java
@@ -26,6 +26,14 @@ public class Lists {
         return Collects.newArrayListWithCapacity(initialArraySize);
     }
 
+    public static <E> ArrayList<E> newArrayListWithFill(int initialArraySize, E initFillElement) {
+        ArrayList<E> list = Collects.newArrayListWithCapacity(initialArraySize);
+        for (int i =0; i< initialArraySize;i++){
+            list.add(initFillElement);
+        }
+        return list;
+    }
+
     public static <E> ArrayList<E> newArrayListWithExpectedSize(int estimatedSize) {
         return Collects.newArrayListWithExpectedSize(estimatedSize);
     }
@@ -36,6 +44,13 @@ public class Lists {
 
     public static <E> LinkedList<E> newLinkedList(Iterable<E> elements) {
        return Collects.newLinkedList(elements);
+    }
+    public static <E> LinkedList<E> newLinkedListWithFill(int initialArraySize, E initFillElement) {
+        LinkedList<E> list = Collects.newLinkedList();
+        for (int i =0; i< initialArraySize;i++){
+            list.add(initFillElement);
+        }
+        return list;
     }
 
     public static <E> CopyOnWriteArrayList<E> newCopyOnWriteArrayList() {
