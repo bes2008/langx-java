ea34a54da9c2ddebe3e6db437e7f72838f0dae2f
fjn1194361820 <fs1194361820@163.com>
2025-02-06 16:49:48 +0800

    refactor(langx-java): 优化 XML 相关接口文档
    - 为 TransformerFactoryCustomizer 接口添加详细注释，说明其用途和使用方法
    - 为 XmlDocumentHandler 接口添加泛型参数说明和方法注释
    - 为 XPathHandler 接口添加接口功能和方法参数说明

--- langx-java/src/main/java/com/jn/langx/text/xml/XPathHandler.java
+++ langx-java/src/main/java/com/jn/langx/text/xml/XPathHandler.java
@@ -3,9 +3,21 @@ package com.jn.langx.text.xml;
 import com.jn.langx.Named;
 import com.jn.langx.Transformer;
 /**
+ * <pre>
+ * XPathHandler接口定义了处理XPath表达式的标准方法。
+ * 它继承了Transformer和Named接口，以实现名称绑定和通用转换功能。
+ * 主要用于在提供XPath参数的情况下执行数据转换。
+ * </pre>
+ *
  * @since 5.0.2
  */
 public interface XPathHandler extends Transformer<String, String>, Named {
+    /**
+     * 根据提供的XPath参数执行转换操作。
+     *
+     * @param xpathParameter XPath表达式参数，用于指导转换过程。
+     * @return 转换后的字符串结果。
+     */
     @Override
     String transform(String xpathParameter);
 
