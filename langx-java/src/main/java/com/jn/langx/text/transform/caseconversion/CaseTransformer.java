051b1db646f1ac09c09ab09d2e8d754f4713d3cf
coderjn <fs1194361820@163.com>
2025-02-09 11:44:08 +0800

    * 更新注释

--- langx-java/src/main/java/com/jn/langx/text/transform/caseconversion/CaseTransformer.java
+++ langx-java/src/main/java/com/jn/langx/text/transform/caseconversion/CaseTransformer.java
@@ -2,6 +2,9 @@ package com.jn.langx.text.transform.caseconversion;
 
 import com.jn.langx.Transformer;
 
+/**
+ * @since 5.4.7
+ */
 public interface CaseTransformer extends Transformer<String, String> {
     @Override
     String transform(String text);
