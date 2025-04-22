b92b45b29d19bc5165a3da64ed00e7fc4b45233d
coderjn <fs1194361820@163.com>
2025-04-22 08:33:28 +0800

    * 简化代码

--- langx-java/src/main/java/com/jn/langx/security/crypto/pbe/pbkdf/OpenSSLEvpKeyGenerator.java
+++ langx-java/src/main/java/com/jn/langx/security/crypto/pbe/pbkdf/OpenSSLEvpKeyGenerator.java
@@ -2,6 +2,7 @@ package com.jn.langx.security.crypto.pbe.pbkdf;
 
 import com.jn.langx.security.SecurityException;
 import com.jn.langx.security.crypto.key.PKIs;
+import com.jn.langx.util.io.bytes.Bytes;
 
 import java.security.MessageDigest;
 
@@ -26,42 +27,25 @@ public class OpenSSLEvpKeyGenerator extends DerivedKeyGenerator {
         try {
             int keyBytesLength = PKIs.getBytesLength(keyBitSize);
             int ivBytesLength = PKIs.getBytesLength(ivBitSize);
-            byte[] key = new byte[keyBytesLength];
-            byte[] iv = new byte[ivBytesLength];
 
-            // 4个字符作为一个 word
-            int keySizeInWord = keyBytesLength / 4;
-            int ivSizeInWord = ivBytesLength / 4;
-            int targetKeySizeInWorld = keySizeInWord + ivSizeInWord;
+            int bytesLength = keyBytesLength + ivBytesLength;
+
             // 由 key ,iv进行拼接的
-            byte[] derivedBytes = new byte[targetKeySizeInWorld * 4];
-            int numberOfDerivedWords = 0;
-            byte[] block = null;
-
             MessageDigest hasher = MessageDigest.getInstance(digestAlgorithm);
-            while (numberOfDerivedWords < targetKeySizeInWorld) {
-                if (block != null) {
-                    hasher.update(block);
-                }
+            hasher.update(password);
+            byte[] block = hasher.digest(salt);
+            hasher.reset();
+
+
+            while (block.length < bytesLength) {
+                hasher.update(block);
                 hasher.update(password);
                 block = hasher.digest(salt);
                 hasher.reset();
-
-                // Iterations
-                for (int i = 1; i < iterationCount; i++) {
-                    block = hasher.digest(block);
-                    hasher.reset();
-                }
-
-                System.arraycopy(block, 0, derivedBytes, numberOfDerivedWords * 4,
-                        Math.min(block.length, (targetKeySizeInWorld - numberOfDerivedWords) * 4));
-
-                numberOfDerivedWords += block.length / 4;
             }
 
-            System.arraycopy(derivedBytes, 0, key, 0, keySizeInWord * 4);
-            System.arraycopy(derivedBytes, keySizeInWord * 4, iv, 0, ivSizeInWord * 4);
-
+            byte[] key = Bytes.subBytes(block, 0, keyBytesLength);
+            byte[] iv = Bytes.subBytes(block, keyBytesLength, ivBytesLength);
             return new SimpleDerivedKey(key, iv);
         } catch (Throwable e) {
             throw new SecurityException(e);
