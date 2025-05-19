db9ef02abdf9ae8f6af619c2cacd976c3126f0f2
coderjn <fs1194361820@163.com>
2025-05-20 06:53:23 +0800

    * 提供全局的WheelTimer

---
+++ langx-java/src/main/java/com/jn/langx/util/Globals.java
@@ -0,0 +1,27 @@
+package com.jn.langx.util;
+
+import com.jn.langx.util.concurrent.CommonThreadFactory;
+import com.jn.langx.util.timing.timer.HashedWheelTimer;
+import com.jn.langx.util.timing.timer.WheelTimers;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class Globals {
+    private static final Object lock = new Object();
+    private static HashedWheelTimer wheelTimer;
+
+    public static HashedWheelTimer getWheelTimer() {
+        if (wheelTimer == null) {
+            synchronized (lock) {
+                if (wheelTimer == null) {
+                    Executor executor = new ThreadPoolExecutor(0, 100, 60L, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>(), new CommonThreadFactory("Global-WheelTimer-TimeoutTask", true));
+                    wheelTimer = WheelTimers.newHashedWheelTimer(new CommonThreadFactory("Global-WheelTimer-Worker", true), 10, TimeUnit.MILLISECONDS, 100, false, executor);
+                }
+            }
+        }
+        return wheelTimer;
+    }
+}
--- langx-java/src/main/java/com/jn/langx/util/timing/timer/WheelTimers.java
+++ langx-java/src/main/java/com/jn/langx/util/timing/timer/WheelTimers.java
@@ -2,6 +2,7 @@ package com.jn.langx.util.timing.timer;
 
 import com.jn.langx.util.concurrent.CommonThreadFactory;
 
+import java.util.concurrent.Executor;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 
@@ -27,5 +28,9 @@ public class WheelTimers {
 
     public static HashedWheelTimer newHashedWheelTimer(ThreadFactory threadFactory, long tickDuration, TimeUnit unit, int ticksPerWheel, boolean leakDetection) {
         return new HashedWheelTimer(threadFactory == null ? new CommonThreadFactory() : threadFactory, tickDuration, unit, ticksPerWheel, leakDetection, -1);
+    }
+
+    public static HashedWheelTimer newHashedWheelTimer(ThreadFactory threadFactory, long tickDuration, TimeUnit unit, int ticksPerWheel, boolean leakDetection, Executor executor) {
+        return new HashedWheelTimer(threadFactory == null ? new CommonThreadFactory() : threadFactory, tickDuration, unit, ticksPerWheel, leakDetection, -1, executor);
     }
 }
